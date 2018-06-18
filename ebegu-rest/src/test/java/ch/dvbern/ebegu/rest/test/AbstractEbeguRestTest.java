29e55c9087143e2813860dcaa7f6cf3446e9df4f
HEFR <franziska.herger@dvbern.ch>
2018-06-18 17:23:23 +0200

    KIBON-43: Tests flicken

--- ebegu-rest/src/test/java/ch/dvbern/ebegu/rest/test/AbstractEbeguRestTest.java
+++ ebegu-rest/src/test/java/ch/dvbern/ebegu/rest/test/AbstractEbeguRestTest.java
@@ -16,9 +16,15 @@
 package ch.dvbern.ebegu.rest.test;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Locale;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.UriInfo;
 
@@ -63,15 +69,204 @@ public abstract class AbstractEbeguRestTest {
 	@Inject
 	private GesuchsperiodeResource gesuchsperiodeResource;
 
-	public static HttpServletResponse DUMMY_RESPONSE;
-	public static UriInfo DUMMY_URIINFO;
+	public HttpServletResponse DUMMY_RESPONSE;
+	public UriInfo DUMMY_URIINFO;
+
+	public AbstractEbeguRestTest() {
+		HttpServletResponse response = new HttpServletResponse() {
+			@Override
+			public void addCookie(Cookie cookie) {
+
+			}
+
+			@Override
+			public boolean containsHeader(String name) {
+				return false;
+			}
+
+			@Override
+			public String encodeURL(String url) {
+				return "";
+			}
+
+			@Override
+			public String encodeRedirectURL(String url) {
+				return "";
+			}
+
+			@Override
+			public String encodeUrl(String url) {
+				return "";
+			}
+
+			@Override
+			public String encodeRedirectUrl(String url) {
+				return "";
+			}
+
+			@Override
+			public void sendError(int sc, String msg) {
+
+			}
+
+			@Override
+			public void sendError(int sc) {
+
+			}
+
+			@Override
+			public void sendRedirect(String location) {
+
+			}
+
+			@Override
+			public void setDateHeader(String name, long date) {
+
+			}
+
+			@Override
+			public void addDateHeader(String name, long date) {
+
+			}
+
+			@Override
+			public void setHeader(String name, String value) {
+
+			}
+
+			@Override
+			public void addHeader(String name, String value) {
+
+			}
+
+			@Override
+			public void setIntHeader(String name, int value) {
+
+			}
+
+			@Override
+			public void addIntHeader(String name, int value) {
+
+			}
+
+			@Override
+			public void setStatus(int sc) {
+
+			}
+
+			@Override
+			public void setStatus(int sc, String sm) {
+
+			}
+
+			@Override
+			public int getStatus() {
+				return 0;
+			}
+
+			@Override
+			public String getHeader(String name) {
+				return "";
+			}
+
+			@Override
+			public Collection<String> getHeaders(String name) {
+				return Collections.emptyList();
+			}
+
+			@Override
+			public Collection<String> getHeaderNames() {
+				return Collections.emptyList();
+			}
+
+			@Override
+			public String getCharacterEncoding() {
+				return "";
+			}
+
+			@Override
+			public String getContentType() {
+				return "";
+			}
+
+			@Override
+			public ServletOutputStream getOutputStream() {
+				return null;
+			}
+
+			@Override
+			public PrintWriter getWriter() {
+				return null;
+			}
+
+			@Override
+			public void setCharacterEncoding(String charset) {
+
+			}
+
+			@Override
+			public void setContentLength(int len) {
+
+			}
+
+			@Override
+			public void setContentLengthLong(long len) {
+
+			}
+
+			@Override
+			public void setContentType(String type) {
+
+			}
+
+			@Override
+			public void setBufferSize(int size) {
+
+			}
+
+			@Override
+			public int getBufferSize() {
+				return 0;
+			}
+
+			@Override
+			public void flushBuffer() {
+
+			}
+
+			@Override
+			public void resetBuffer() {
+
+			}
+
+			@Override
+			public boolean isCommitted() {
+				return false;
+			}
+
+			@Override
+			public void reset() {
+
+			}
+
+			@Override
+			public void setLocale(Locale loc) {
+
+			}
+
+			@Override
+			public Locale getLocale() {
+				return Locale.GERMAN;
+			}
+		};
+		DUMMY_RESPONSE = new ResteasyHttpServletResponseWrapper(response, null) {};
+		DUMMY_URIINFO = new ResteasyUriInfo("test", "test", "test");
+	}
 
 	@Deployment
 	@OverProtocol("Servlet 3.0")
 	public static Archive<?> createTestArchive() {
 		//noinspection ConstantConditions
-		DUMMY_RESPONSE = new ResteasyHttpServletResponseWrapper(null, null) {};
-		DUMMY_URIINFO = new ResteasyUriInfo("test", "test", "test");
 		return createTestArchive(null);
 	}
 
