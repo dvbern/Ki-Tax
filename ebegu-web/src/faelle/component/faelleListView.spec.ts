598327058acc5c44cc4979a3831ffbd8433d69e9
HEFR <franziska.herger@dvbern.ch>
2018-06-25 12:41:38 +0200

    KIBON-79: Navigation Dashboard GS auf vorhandenes Gesuch geht nicht

--- ebegu-web/src/faelle/component/faelleListView.spec.ts
+++ ebegu-web/src/faelle/component/faelleListView.spec.ts
@@ -83,19 +83,19 @@ describe('faelleListView', function () {
             it('should call findGesuch and open the view gesuch.fallcreation with it for normal user', function () {
                 let tsGesuch = callEditFall();
 
-                expect($state.go).toHaveBeenCalledWith('gesuch.fallcreation', {createNew: false, gesuchId: '66345345'});
+                expect($state.go).toHaveBeenCalledWith('gesuch.fallcreation', {createNew: false, gesuchId: '66345345', dossierId: '11111111'});
 
             });
             it('should call findGesuch and open the view gesuch.betreuungen with it for INS/TRAEGER user if gesuch not verfuegt', function () {
                 spyOn(authServiceRS, 'isOneOfRoles').and.returnValue(true);
                 let tsGesuch = callEditFall();
-                expect($state.go).toHaveBeenCalledWith('gesuch.betreuungen', {createNew: false, gesuchId: '66345345'});
+                expect($state.go).toHaveBeenCalledWith('gesuch.betreuungen', {createNew: false, gesuchId: '66345345', dossierId: '11111111'});
             });
             it('should call findGesuch and open the view gesuch.verfuegen with it for INS/TRAEGER user if gesuch verfuegt', function () {
                 spyOn(authServiceRS, 'isOneOfRoles').and.returnValue(true);
                 mockAntrag.status = TSAntragStatus.VERFUEGT;
                 let tsGesuch = callEditFall();
-                expect($state.go).toHaveBeenCalledWith('gesuch.verfuegen', {createNew: false, gesuchId: '66345345'});
+                expect($state.go).toHaveBeenCalledWith('gesuch.verfuegen', {createNew: false, gesuchId: '66345345', dossierId: '11111111'});
             });
         });
     });
@@ -104,6 +104,7 @@ describe('faelleListView', function () {
         let mockPendenz: TSAntragDTO = new TSAntragDTO('66345345', 123, 'name', TSAntragTyp.ERSTGESUCH,
             undefined, undefined, undefined, [TSBetreuungsangebotTyp.KITA], ['Inst1, Inst2'], 'Juan Arbolado', 'Juan Arbolado',
             undefined, undefined, undefined);
+        mockPendenz.dossierId = '11111111';
         let dtoList: Array<TSAntragDTO> = [mockPendenz];
         let totalSize: number = 1;
         let searchresult: TSAntragSearchresultDTO = new TSAntragSearchresultDTO(dtoList, totalSize);
