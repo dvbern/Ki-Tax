860d1576ab8afdf0fd8bbbacb3b1f0f4872f2a49
Tobias Feller <tobias.feller@dvbern.ch>
2018-07-18 16:03:53 +0200

    KIBON-127 improve onboarding component styling

--- ebegu-web/src/core/component/dv-onboarding/dv-onboarding.component.ts
+++ ebegu-web/src/core/component/dv-onboarding/dv-onboarding.component.ts
@@ -2,11 +2,11 @@ import {Component} from '@angular/core';
 import {IComponentOptions} from 'angular';
 
 const template = require('./dv-onboarding.component.html');
-require('./dv-onboarding.component.scss');
+require('./dv-onboarding.component.less');
 @Component({
   selector: 'dv-onboarding',
   templateUrl: './dv-onboarding.component.html',
-  styleUrls: ['./dv-onboarding.component.scss']
+  styleUrls: ['./dv-onboarding.component.less']
 })
 export class DvOnboardingComponent {
 
