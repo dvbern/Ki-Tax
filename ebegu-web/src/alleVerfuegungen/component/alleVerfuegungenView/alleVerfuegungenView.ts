0b118f0f00c1dd2cfa4dea940e35f91f386fc858
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2018-08-02 16:57:13 +0200

    KIBON-25 - fallToolbar must to be shown in tab Alle Verfuegungen too

--- ebegu-web/src/alleVerfuegungen/component/alleVerfuegungenView/alleVerfuegungenView.ts
+++ ebegu-web/src/alleVerfuegungen/component/alleVerfuegungenView/alleVerfuegungenView.ts
@@ -76,6 +76,13 @@ export class AlleVerfuegungenViewController {
         }
     }
 
+    public getFallId(): string {
+        if (this.dossier && this.dossier.fall) {
+            return this.dossier.fall.id;
+        }
+        return '';
+    }
+
     public getAlleVerfuegungen(): Array<TSAntragStatusHistory> {
         return this.alleVerfuegungen;
     }
