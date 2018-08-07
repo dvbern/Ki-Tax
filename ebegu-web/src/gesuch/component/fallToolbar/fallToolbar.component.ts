2d0d5f1e83d567c87138d80d57b252f46ae31fe7
Fabio Heer <fabio.heer@dvbern.ch>
2018-08-07 13:02:57 +0200

    KIBON-25 properly annotate undefined

--- ebegu-web/src/gesuch/component/fallToolbar/fallToolbar.component.ts
+++ ebegu-web/src/gesuch/component/fallToolbar/fallToolbar.component.ts
@@ -31,7 +31,7 @@ export class FallToolbarComponent implements OnInit, OnChanges {
     @Input() defaultGemeindeName: string;
     // fall: TSFall;
     dossierList: TSDossier[] = [];
-    selectedDossier: TSDossier;
+    selectedDossier?: TSDossier;
 
     constructor(private dossierRS: DossierRS) {
     }
