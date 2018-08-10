7575820611bc6bac7948225f854a69eda4b84a96
HEFR <franziska.herger@dvbern.ch>
2018-08-10 19:04:20 +0200

    KIBON-130: UI: Maske "Registrierung abschliessen" umsetzen

--- ebegu-web/src/app/onboarding/dv-onboarding-gs-abschliessen/onboarding-gs-abschliessen.component.ts
+++ ebegu-web/src/app/onboarding/dv-onboarding-gs-abschliessen/onboarding-gs-abschliessen.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {Observable} from 'rxjs/index';
 import AuthServiceRS from '../../../authentication/service/AuthServiceRS.rest';
+import GemeindeRS from '../../../gesuch/service/gemeindeRS.rest';
 import TSGemeinde from '../../../models/TSGemeinde';
 import TSUser from '../../../models/TSUser';
 
@@ -15,13 +16,17 @@ export class OnboardingGsAbschliessenComponent {
     public user$: Observable<TSUser>;
     public gemeinde: TSGemeinde;
 
-    constructor(public readonly authServiceRS: AuthServiceRS) {
+    constructor(
+        public readonly authServiceRS: AuthServiceRS,
+        public readonly gemeindeRS: GemeindeRS) {
     }
 
     ngOnInit() {
         this.user$ = this.authServiceRS.principal$;
         //TODO: Das Dossier muss zu diesem Zeitpunkt schon bestehen! Muss (wenn principal da ist) vom Server gelesen werden
-        this.gemeinde = new TSGemeinde();
-        this.gemeinde.name = 'Bern';
+        //TODO: Vorerst verwenden wir fix die Gemeinde Ostermundigen. Wir hoffen dass wir die Gemeinde dann erst auf dieser Maske selektieren lassen können
+        this.gemeindeRS.findGemeinde('80a8e496-b73c-4a4a-a163-a0b2caf76487').then(foundGemeinde => {
+            this.gemeinde = foundGemeinde;
+        });
     }
 }
