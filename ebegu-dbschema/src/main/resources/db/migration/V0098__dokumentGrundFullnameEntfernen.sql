c97fc3186936f2c7b4e16f773721615a9e1709ef
HEFR <franziska.herger@dvbern.ch>
2018-08-24 16:52:23 +0200

    KIBON-174: DokumentGrund.fullname entfernen

--- ebegu-dbschema/src/main/resources/db/migration/V0098__dokumentGrundFullnameEntfernen.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0098__dokumentGrundFullnameEntfernen.sql
@@ -3,7 +3,13 @@ ALTER TABLE dokument_grund
 ALTER TABLE dokument_grund_aud
 	DROP full_name;
 
+DELETE FROM dokument
+WHERE dokument.dokument_grund_id IN (
+	SELECT id
+	FROM dokument_grund
+	WHERE dokument_grund.person_type = 'FREETEXT');
+
 DELETE FROM dokument_grund
 WHERE dokument_grund.person_type = 'FREETEXT';
 DELETE FROM dokument_grund_aud
-WHERE dokument_grund.person_type = 'FREETEXT';
+WHERE dokument_grund_aud.person_type = 'FREETEXT';
