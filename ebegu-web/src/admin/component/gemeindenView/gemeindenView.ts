0e44c08c559d236341cb34073d6f6d9ce49c6a8d
Martin Riem <martin.riem@pbroker.ch>
2018-09-04 16:49:40 +0200

    enhance asynchronous handling

--- ebegu-web/src/admin/component/gemeindenView/gemeindenView.ts
+++ ebegu-web/src/admin/component/gemeindenView/gemeindenView.ts
@@ -1,31 +1,41 @@
 /*
- * Ki-Tax: System for the management of external childcare subsidies
- * Copyright (C) 2017 City of Bern Switzerland
+ * AGPL File-Header
+ *
+ * Copyright (C) 2018 DV Bern AG, Switzerland
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
+ *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import * as angular from 'angular';
-import {AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    OnDestroy,
+    OnInit,
+    ViewChild
+} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {MatDialog, MatDialogConfig, MatSort, MatSortable, MatTableDataSource} from '@angular/material';
-import {Subject} from 'rxjs/index';
-import {takeUntil} from 'rxjs/operators';
-import {DvNgRemoveDialogComponent} from '../../../app/core/component/dv-ng-remove-dialog/dv-ng-remove-dialog.component';
+import {MatDialog, MatSort, MatSortable, MatTableDataSource} from '@angular/material';
+import * as angular from 'angular';
+import {Subject} from 'rxjs';
+import {map, takeUntil} from 'rxjs/operators';
 import ErrorService from '../../../app/core/errors/service/ErrorService';
 import {LogFactory} from '../../../app/core/logging/LogFactory';
-import TSGemeinde from '../../../models/TSGemeinde';
-import GemeindeRS from '../../../gesuch/service/gemeindeRS.rest';
 import AuthServiceRS from '../../../authentication/service/AuthServiceRS.rest';
-import EbeguUtil from '../../../utils/EbeguUtil';
+import GemeindeRS from '../../../gesuch/service/gemeindeRS.rest';
+import TSGemeinde from '../../../models/TSGemeinde';
 import AbstractAdminViewController from '../../abstractAdminView';
 
 const LOG = LogFactory.createLog('GemeindenViewComponent');
@@ -33,14 +43,14 @@ const LOG = LogFactory.createLog('GemeindenViewComponent');
 @Component({
     selector: 'dv-gemeinden-view',
     templateUrl: './gemeindenView.html',
-    styleUrls: ['./gemeindenView.less']
+    styleUrls: ['./gemeindenView.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class GemeindenViewComponent extends AbstractAdminViewController implements OnInit, OnDestroy, AfterViewInit {
 
     displayedColumns: string[] = ['name', 'status'];
     gemeinde: TSGemeinde = undefined;
-    gemeindenList: Array<TSGemeinde>;
     dataSource: MatTableDataSource<TSGemeinde>;
     private readonly unsubscribe$ = new Subject<void>();
 
@@ -50,14 +60,14 @@ export class GemeindenViewComponent extends AbstractAdminViewController implemen
     constructor(private readonly gemeindeRS: GemeindeRS,
                 private readonly errorService: ErrorService,
                 private readonly dialog: MatDialog,
+                private readonly changeDetectorRef: ChangeDetectorRef,
                 authServiceRS: AuthServiceRS) {
 
         super(authServiceRS);
-
-        this.updateGemeindenList();
     }
 
     public ngOnInit(): void {
+        this.updateGemeindenList();
         this.sortTable();
     }
 
@@ -72,12 +82,16 @@ export class GemeindenViewComponent extends AbstractAdminViewController implemen
 
     private updateGemeindenList(): void {
         this.gemeindeRS.getGemeindenForPrincipal$()
-            .pipe(takeUntil(this.unsubscribe$))
-            .subscribe(gemeinden => {
-                    this.dataSource = new MatTableDataSource(gemeinden);
-                },
-                err => LOG.error(err)
-            );
+            .pipe(map(gemeinden => {
+                    const dataSource = new MatTableDataSource(gemeinden);
+                    return dataSource;
+                }),
+                takeUntil(this.unsubscribe$)
+            )
+            .subscribe(dataSource => {
+                this.dataSource = dataSource;
+                this.changeDetectorRef.markForCheck();
+            });
     }
 
     /**
