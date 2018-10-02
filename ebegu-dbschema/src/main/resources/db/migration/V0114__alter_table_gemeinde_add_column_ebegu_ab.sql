926cb1ede2c76d238f86a64401e4e3c2ad685f92
Tobias Feller <tobias.feller@dvbern.ch>
2018-10-02 17:50:02 +0200

    KIBON-216 fix migration by adding audit table

--- ebegu-dbschema/src/main/resources/db/migration/V0114__alter_table_gemeinde_add_column_ebegu_ab.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0114__alter_table_gemeinde_add_column_ebegu_ab.sql
@@ -1,5 +1,7 @@
 ALTER TABLE gemeinde ADD COLUMN betreuungsgutscheine_startdatum DATE;
 
+ALTER TABLE gemeinde_aud ADD COLUMN betreuungsgutscheine_startdatum DATE;
+
 UPDATE gemeinde g
 JOIN einstellung e
 ON g.id = e.gemeinde_id AND e.einstellung_key = 'BEGU_ANBIETEN_AB'
