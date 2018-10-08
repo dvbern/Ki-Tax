db0a44519185c186ffb5fe12bc48ddb068a2d17b
Fabio Heer <fabio.heer@dvbern.ch>
2018-10-08 17:35:29 +0200

    KIBON-221 write default values to DB

--- ebegu-dbschema/src/main/resources/db/migration/V0117__unitsForBetreuungspensum.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0117__unitsForBetreuungspensum.sql
@@ -1,9 +1,13 @@
 ALTER TABLE betreuungspensum
-	ADD COLUMN unit_for_display VARCHAR(255) NOT NULL;
+	ADD COLUMN unit_for_display VARCHAR(255) NOT NULL DEFAULT 'PERCENTAGE';
 ALTER TABLE betreuungspensum_aud
 	ADD COLUMN unit_for_display VARCHAR(255);
+ALTER TABLE betreuungspensum
+	ALTER unit_for_display DROP DEFAULT;
 
 ALTER TABLE betreuungsmitteilung_pensum
-	ADD COLUMN unit_for_display VARCHAR(255) NOT NULL;
+	ADD COLUMN unit_for_display VARCHAR(255) NOT NULL DEFAULT 'PERCENTAGE';
 ALTER TABLE betreuungsmitteilung_pensum_aud
 	ADD COLUMN unit_for_display VARCHAR(255);
+ALTER TABLE betreuungsmitteilung_pensum
+	ALTER unit_for_display DROP DEFAULT;
