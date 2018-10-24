f565d4104a428cafe11f32af128e530f1864c35d
Martin Riem <martin.riem@pbroker.ch>
2018-10-24 11:26:31 +0200

    /KIBON-245: flyaway...

--- ebegu-dbschema/src/main/resources/db/migration/V0130__renameGemeindeEinstellungen.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0130__renameGemeindeEinstellungen.sql
@@ -3,4 +3,5 @@ UPDATE einstellung SET einstellung_key = 'GEMEINDE_KONTINGENTIERUNG_ENABLED' WHE
 UPDATE einstellung SET einstellung_key = 'GEMEINDE_BG_BIS_UND_MIT_SCHULSTUFE' WHERE einstellung_key = 'BG_BIS_UND_MIT_SCHULSTUFE';
 # Boolean Spalte ist nicht nötig, kann über das Existieren der Beschwerdeadresse ermittelt werden
 ALTER TABLE gemeinde_stammdaten	DROP COLUMN keine_beschwerde_adresse;
+ALTER TABLE gemeinde_stammdaten_aud	DROP COLUMN keine_beschwerde_adresse;
 
