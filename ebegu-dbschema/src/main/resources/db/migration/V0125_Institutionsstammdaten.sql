83b6cd71b137e325e5f18a5fb55936d4e6330b4d
HEFR <franziska.herger@dvbern.ch>
2018-10-25 11:21:25 +0200

    KIBON-250: InstitutionStammdatenTagesschule und InstitutionStammdatenFerieninsel neu zeitabhängig

---
+++ ebegu-dbschema/src/main/resources/db/migration/V0125_Institutionsstammdaten.sql
@@ -0,0 +1,25 @@
+# InstitutionStammdatenTagesschule / InstitutionStammdatenFerieninsel neu zeitabhängig:
+
+ALTER TABLE institution_stammdaten_tagesschule ADD gueltig_ab DATE;
+ALTER TABLE institution_stammdaten_tagesschule ADD gueltig_bis DATE;
+
+ALTER TABLE institution_stammdaten_tagesschule_aud ADD gueltig_ab DATE;
+ALTER TABLE institution_stammdaten_tagesschule_aud ADD gueltig_bis DATE;
+
+ALTER TABLE institution_stammdaten_ferieninsel ADD gueltig_ab DATE;
+ALTER TABLE institution_stammdaten_ferieninsel ADD gueltig_bis DATE;
+
+ALTER TABLE institution_stammdaten_ferieninsel_aud ADD gueltig_ab DATE;
+ALTER TABLE institution_stammdaten_ferieninsel_aud ADD gueltig_bis DATE;
+
+UPDATE institution_stammdaten_tagesschule SET gueltig_ab = '1000-01-01';
+UPDATE institution_stammdaten_tagesschule SET gueltig_bis = '9999-12-31';
+
+UPDATE institution_stammdaten_ferieninsel SET gueltig_ab = '1000-01-01';
+UPDATE institution_stammdaten_ferieninsel SET gueltig_bis = '9999-12-31';
+
+ALTER TABLE institution_stammdaten_tagesschule MODIFY gueltig_ab DATE NOT NULL;
+ALTER TABLE institution_stammdaten_tagesschule MODIFY gueltig_bis DATE NOT NULL;
+
+ALTER TABLE institution_stammdaten_ferieninsel MODIFY gueltig_ab DATE NOT NULL;
+ALTER TABLE institution_stammdaten_ferieninsel MODIFY gueltig_bis DATE NOT NULL;
