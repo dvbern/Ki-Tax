3f148828901403e5880d42ba6b6f584673db0306
Christian Egli <christian.egli@dvbern.ch>
2018-10-31 14:59:31 +0100

    KIBON-229: fixing test

--- ebegu-web/src/app/einladung/einladung-abschliessen/einladung-abschliessen.component.spec.ts
+++ ebegu-web/src/app/einladung/einladung-abschliessen/einladung-abschliessen.component.spec.ts
@@ -16,21 +16,59 @@
  */
 
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Transition} from '@uirouter/core';
+import {of} from 'rxjs';
+import AuthServiceRS from '../../../authentication/service/AuthServiceRS.rest';
+import TestDataUtil from '../../../utils/TestDataUtil.spec';
+import {InstitutionRS} from '../../core/service/institutionRS.rest';
+import {TraegerschaftRS} from '../../core/service/traegerschaftRS.rest';
+import {SharedModule} from '../../shared/shared.module';
 
 import {EinladungAbschliessenComponent} from './einladung-abschliessen.component';
 
-describe('EinladungAbschliessenComponent', () => {
+fdescribe('EinladungAbschliessenComponent', () => {
+    let component: EinladungAbschliessenComponent;
     let fixture: ComponentFixture<EinladungAbschliessenComponent>;
 
     beforeEach(async(() => {
+        const transitionSpy = jasmine.createSpyObj<Transition>(Transition.name, ['params']);
+        const insitutionSpy = jasmine.createSpyObj<InstitutionRS>(InstitutionRS.name,
+            ['getInstitutionenForCurrentBenutzer']);
+        const traegerschaftSpy = jasmine.createSpyObj<TraegerschaftRS>(TraegerschaftRS.name, ['getAllTraegerschaften']);
+        const authServiceSpy = jasmine.createSpyObj<AuthServiceRS>(AuthServiceRS.name,
+            ['principal$', 'getVisibleRolesForPrincipal']);
+
         TestBed.configureTestingModule({
-            declarations: [EinladungAbschliessenComponent],
+            imports: [
+                SharedModule,
+            ],
+            declarations: [
+                EinladungAbschliessenComponent
+            ],
+            providers: [
+                {provide: Transition, useValue: transitionSpy},
+                {provide: InstitutionRS, useValue: insitutionSpy},
+                {provide: TraegerschaftRS, useValue: traegerschaftSpy},
+                {provide: AuthServiceRS, useValue: authServiceSpy},
+            ]
         })
             .compileComponents();
+
+        const superadmin = TestDataUtil.createSuperadmin();
+        authServiceSpy.principal$ = of(superadmin) as any;
+        authServiceSpy.getVisibleRolesForPrincipal.and.returnValue([]);
+        insitutionSpy.getInstitutionenForCurrentBenutzer.and.returnValue(Promise.resolve([]));
+        traegerschaftSpy.getAllTraegerschaften.and.returnValue(Promise.resolve([]));
+        transitionSpy.params.and.returnValue({inputId: undefined});
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(EinladungAbschliessenComponent);
+        component = fixture.componentInstance;
         fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
     });
 });
