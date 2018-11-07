27351c2c65217590772d9b02dd7339e4da9c7776
HEFR <franziska.herger@dvbern.ch>
2018-11-07 17:26:58 +0100

    KIBON-227: Copyright Header

--- ebegu-web/src/app/traegerschaft/traegerschaft.module.ts
+++ ebegu-web/src/app/traegerschaft/traegerschaft.module.ts
@@ -1,9 +1,27 @@
+/*
+ * Copyright (C) 2018 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {MaterialModule} from '../shared/material.module';
 import {SharedModule} from '../shared/shared.module';
 import {TraegerschaftEditComponent} from './traegerschaft-edit/traegerschaft-edit.component';
 import {TraegerschaftListComponent} from './traegerschaft-list/traegerschaft-list.component';
 import {TraegerschaftRoutingModule} from './traegerschaft-routing/traegerschaft-routing.module';
+import { TraegerschaftAddComponent } from './traegerschaft-add/traegerschaft-add.component';
 
 @NgModule({
     imports: [
@@ -18,10 +36,12 @@ import {TraegerschaftRoutingModule} from './traegerschaft-routing/traegerschaft-
     declarations: [
         TraegerschaftEditComponent,
         TraegerschaftListComponent,
+        TraegerschaftAddComponent,
     ],
     entryComponents: [
         TraegerschaftEditComponent,
         TraegerschaftListComponent,
+        TraegerschaftAddComponent,
     ],
     providers: [],
 })
