ceb8bbce4677c240e2050015c7da81e0b665a58c
Christian Egli <christian.egli@dvbern.ch>
2018-11-10 16:37:04 +0100

    KIBON-260: sql script fix

--- ebegu-dbschema/src/main/resources/db/migration/V0134__increase_pensum_decimal_place.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0134__increase_pensum_decimal_place.sql
@@ -1,6 +1,6 @@
 -- increase pensum decimal places to 10
 ALTER TABLE betreuungspensum ALTER COLUMN pensum DECIMAL(19, 10)
-ALTER TABLE betreuungspensum_aud DECIMAL(19, 10)
+ALTER TABLE betreuungspensum_aud ALTER COLUMN pensum DECIMAL(19, 10)
 
-ALTER TABLE betreuungsmitteilung_pensum DECIMAL(19, 10)
-ALTER TABLE betreuungsmitteilung_pensum_aud DECIMAL(19, 10)
+ALTER TABLE betreuungsmitteilung_pensum ALTER COLUMN pensum DECIMAL(19, 10)
+ALTER TABLE betreuungsmitteilung_pensum_aud ALTER COLUMN pensum DECIMAL(19, 10)
