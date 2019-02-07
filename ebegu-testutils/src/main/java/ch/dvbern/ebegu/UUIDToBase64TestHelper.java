bdb543e1e34643754c25e6fdc02b6676d5d391d0
Markus Hofstetter <markus.hofstetter@dvbern.ch>
2019-02-07 18:18:45 +0100

    EBEGU-1511 > fixed tests for dbunit by replacing uuid with base64 equivalent

--- ebegu-testutils/src/main/java/ch/dvbern/ebegu/UUIDToBase64TestHelper.java
+++ ebegu-testutils/src/main/java/ch/dvbern/ebegu/UUIDToBase64TestHelper.java
@@ -18,20 +18,33 @@ import org.apache.xmlbeans.impl.util.Base64;
  * This is a helper that is able to replace an uuid in a db unit script with its equivalent base 64 encoded binary value
  * Usefull for migrating dbunit datasets to a binary uuid id column
  * */
+@SuppressWarnings("PMD.SystemPrintln")
 public class UUIDToBase64TestHelper {
+
+	private UUIDToBase64TestHelper() {
+		//util
+	}
 
 	private static List<String> lineList;
 
 	public static void main(String[] args) throws IOException {
+
+		String path = "/home/homa/java/ideaprojects/jugendamt/ebegu-test/ebegu/ebegu-testutils/src/main/resources/datasets/massenversand-dataset.xml";
+		if(args.length > 0){
+			path = args[0];
+		}
+		replaceUUIDSInfile(path);
+	}
+
+	private static void replaceUUIDSInfile(String path) throws IOException {
 		String testsimple = "0621fb5d-a187-5a91-abaf-8a813c4d2aaa";
-		String test = "<gesuchsperiode id=\"0621fb5d-a187-5a91-abaf-8a813c4d2aaa\"";
+		String testLine = "<gesuchsperiode id=\"0621fb5d-a187-5a91-abaf-8a813c4d2aaa\"";
 		final boolean simpleMatch = testsimple.matches(Constants.REGEX_UUID);
 		System.out.println("simple match " + simpleMatch);
-		final boolean complexMatch = test.matches(Constants.REGEX_UUID);
+		final boolean complexMatch = testLine.matches(Constants.REGEX_UUID);
 		System.out.println("complex match " + complexMatch);
 
-		lineList = Files.lines(Paths.get("/home/homa/java/ideaprojects/jugendamt/ebegu-test/ebegu/ebegu-testutils/src/main/resources"
-			+ "/datasets/massenversand-dataset.xml")).collect(Collectors.toList());
+		lineList = Files.lines(Paths.get(path)).collect(Collectors.toList());
 
 		StringBuffer sb = new StringBuffer();
 		Pattern p = Pattern.compile(Constants.REGEX_UUID);
@@ -49,9 +62,7 @@ public class UUIDToBase64TestHelper {
 			sb.append("\n");
 
 		}
-		System.out.println(sb.toString());
-
-
+		System.out.println(sb);
 	}
 
 	@Nonnull
