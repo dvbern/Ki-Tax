ab0f8c2d7bf57a65cc9693e58622accbe7e34727
HEFR <franziska.herger@dvbern.ch>
2019-05-07 18:08:50 +0200

    KIBON-112: Attribut Gemeinde auf Entität hinzugefügt, Flyway-Skript

---
+++ ebegu-dbschema/src/main/resources/db/migration/V0011__zahlungslaufProGemeinde.sql
@@ -0,0 +1,13 @@
+# Wir muessen allenfalls bestehende Zahlungsauftraege loeschen
+DELETE FROM zahlungsposition;
+DELETE FROM zahlung;
+DELETE FROM zahlungsauftrag;
+UPDATE verfuegung_zeitabschnitt SET zahlungsstatus = 'NEU';
+
+ALTER TABLE zahlungsauftrag ADD gemeinde_id BINARY(16) NOT NULL;
+ALTER TABLE zahlungsauftrag_aud ADD gemeinde_id BINARY(16);
+
+ALTER TABLE zahlungsauftrag
+	ADD CONSTRAINT FK_zahlungsauftrag_gemeinde_id
+FOREIGN KEY (gemeinde_id)
+REFERENCES gemeinde (id);
