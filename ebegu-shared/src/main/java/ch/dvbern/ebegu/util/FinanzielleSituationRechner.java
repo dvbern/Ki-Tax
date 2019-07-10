edad941ee1089c90387d58faae91e2eacd33b3c2
HEFR <franziska.herger@dvbern.ch>
2019-07-10 15:15:14 +0200

    KIBON-646: Einkommensverschlechterung wird bei Verbesserung falsch interpretiert

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/util/FinanzielleSituationRechner.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/util/FinanzielleSituationRechner.java
@@ -281,7 +281,8 @@ public class FinanzielleSituationRechner {
 		boolean result = massgebendesEinkommenBasisjahr.compareTo(BigDecimal.ZERO) > 0;
 		if (result) {
 			BigDecimal differenz = calculateProzentualeDifferenz(massgebendesEinkommenBasisjahr, massgebendesEinkommenJahr);
-			return differenz.abs().compareTo(minimumEKV) > 0;
+			boolean accept = differenz.compareTo(minimumEKV.negate()) < 0;
+			return accept;
 		}
 		return false;
 	}
