97512dc4f2800c24ce103998668cf2202153f09f
Imanol Bernabeu <imanol.bernabeu@dvbern.ch>
2019-07-24 14:47:54 +0200

    KIBON-652: log info improved

--- ebegu-rest/src/main/java/ch/dvbern/ebegu/api/resource/util/ResourceHelper.java
+++ ebegu-rest/src/main/java/ch/dvbern/ebegu/api/resource/util/ResourceHelper.java
@@ -134,7 +134,8 @@ public class ResourceHelper {
 			new EbeguEntityNotFoundException(ASSERT_BETREUUNG_STATUS_EQUAL, ErrorCodeEnum.ERROR_ENTITY_NOT_FOUND, betreuungId));
 		// Der Status des Client-Objektes darf nicht weniger weit sein als der des Server-Objektes
 		if (Arrays.stream(betreuungsstatusFromClient).noneMatch(status -> betreuungFromDB.getBetreuungsstatus() == status)) {
-			String msg = "Expected BetreuungStatus to be " + betreuungsstatusFromClient + " but was " + betreuungFromDB.getBetreuungsstatus();
+			String msg = "Expected BetreuungStatus to be " + Arrays.toString(betreuungsstatusFromClient)
+				+ " but was " + betreuungFromDB.getBetreuungsstatus();
 			throw new EbeguRuntimeException(ASSERT_BETREUUNG_STATUS_EQUAL, msg, ErrorCodeEnum.ERROR_INVALID_EBEGUSTATE, betreuungId);
 		}
 	}
