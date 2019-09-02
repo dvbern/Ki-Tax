6b08ad1b1aab641de71c7056216361343d997163
HEFR <franziska.herger@dvbern.ch>
2019-09-02 16:53:02 +0200

    KIBON-719: Module pro Gesuchsperiode: Service zum Kopieren der Module auf die neue Gesuchsperiode

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/ModulTagesschuleServiceBean.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/ModulTagesschuleServiceBean.java
@@ -15,6 +15,7 @@
 
 package ch.dvbern.ebegu.services;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -24,9 +25,12 @@ import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import ch.dvbern.ebegu.entities.Gesuchsperiode;
 import ch.dvbern.ebegu.entities.ModulTagesschule;
+import ch.dvbern.ebegu.entities.ModulTagesschule_;
 import ch.dvbern.ebegu.enums.ErrorCodeEnum;
 import ch.dvbern.ebegu.errors.EbeguEntityNotFoundException;
+import ch.dvbern.ebegu.persistence.CriteriaQueryHelper;
 import ch.dvbern.lib.cdipersistence.Persistence;
 
 import static ch.dvbern.ebegu.enums.UserRoleName.ADMIN_GEMEINDE;
@@ -42,6 +46,9 @@ public class ModulTagesschuleServiceBean extends AbstractBaseService implements 
 
 	@Inject
 	private Persistence persistence;
+
+	@Inject
+	private CriteriaQueryHelper criteriaQueryHelper;
 
 	@Nonnull
 	@Override
@@ -70,6 +77,20 @@ public class ModulTagesschuleServiceBean extends AbstractBaseService implements 
 		persistence.remove(modulToRemove);
 	}
 
+	@Override
+	@RolesAllowed(SUPER_ADMIN)
+	public void copyModuleTagesschuleToNewGesuchsperiode(
+		@Nonnull Gesuchsperiode gesuchsperiodeToCreate,
+		@Nonnull Gesuchsperiode lastGesuchsperiode
+	) {
+		Collection<ModulTagesschule> moduleOfLastGP = criteriaQueryHelper.getEntitiesByAttribute(ModulTagesschule.class, lastGesuchsperiode,
+			ModulTagesschule_.gesuchsperiode);
+		moduleOfLastGP
+			.forEach(lastGPModul -> {
+				ModulTagesschule modulOfNewGP = lastGPModul.copyForGesuchsperiode(gesuchsperiodeToCreate);
+				saveModul(modulOfNewGP);
+			});
+	}
 }
 
 
