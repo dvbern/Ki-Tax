12fddae27cd52f02641c9e03c5bf665d22561d5c
HEFR <franziska.herger@dvbern.ch>
2019-12-02 12:57:05 +0100

    KIBON-882: Tageschule Tarifrechner: Interface angepasst, Test als normaler JUnit Test

---
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rechner/TagesschuleRechner.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2019 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.dvbern.ebegu.rechner;
+
+import java.math.BigDecimal;
+
+import javax.annotation.Nonnull;
+
+import ch.dvbern.ebegu.entities.BelegungTagesschuleModul;
+import ch.dvbern.ebegu.entities.VerfuegungZeitabschnitt;
+import ch.dvbern.ebegu.util.MathUtil;
+
+public class TagesschuleRechner {
+
+
+	public TagesschuleRechner() {
+
+	}
+
+	/**
+	 * TODO Spaeter wuerde ich hier ein TagesschuleCalculationResult zurueckgeben mit TarifProStunde und Tarif fuer das ganze Modul (inkl. eventl.
+	 * Verpflegungskosten und beruecksichtigung des intervalls). Daher auch das modul als parameter anstat nur das flag paedagogisch betreut
+	 *
+	 * Berechnet den Tarif pro Stunde für einen gegebenen Zeitabschnit und für ein Modul. Es werden diverse Parameter benoetigt
+	 * diese werden in einem DTO uebergeben.
+	 */
+	public BigDecimal calculateTarif(
+		@Nonnull VerfuegungZeitabschnitt zeitabschnitt,
+		@Nonnull TagesschuleRechnerParameterDTO parameterDTO,
+		@Nonnull BelegungTagesschuleModul modul
+	) {
+		// Massgebendes Einkommen der Familie. Mit Maximal und Minimalwerten "verrechnen"
+		BigDecimal massgebendesEinkommen = zeitabschnitt.getMassgebendesEinkommen();
+		massgebendesEinkommen = MathUtil.minimum(massgebendesEinkommen, parameterDTO.getMinMassgebendesEinkommen());
+		massgebendesEinkommen = MathUtil.maximum(massgebendesEinkommen, parameterDTO.getMaxMassgebendesEinkommen());
+
+		// Falls der Gesuchsteller die Finanziellen Daten nicht angeben will, rechnen wir mit dem Maximalen Einkommen.
+		// TODO: Sobald im GUI die Frage nach den Vollkosten vorhanden ist, muss sichergestellt werden, dass eine Rule das "isBezahltVollkosten"-Flag setzt
+		if (zeitabschnitt.isBezahltVollkosten()) {
+			massgebendesEinkommen = parameterDTO.getMaxMassgebendesEinkommen();
+		}
+
+		BigDecimal mataMinusMita = null;
+		if (modul.getModulTagesschule().getModulTagesschuleGroup().isWirdPaedagogischBetreut()) {
+			mataMinusMita = MathUtil.EXACT.subtract(parameterDTO.getMaxTarifMitPaedagogischerBetreuung(), parameterDTO.getMinTarif());
+		} else {
+			mataMinusMita = MathUtil.EXACT.subtract(parameterDTO.getMaxTarifOhnePaedagogischeBetreuung(), parameterDTO.getMinTarif());
+		}
+		BigDecimal maxmEMinusMinmE = MathUtil.EXACT.subtract(parameterDTO.getMaxMassgebendesEinkommen(),
+			parameterDTO.getMinMassgebendesEinkommen());
+		BigDecimal divided = MathUtil.EXACT.divide(mataMinusMita, maxmEMinusMinmE);
+
+
+		BigDecimal meMinusMinmE = MathUtil.EXACT.subtract(massgebendesEinkommen,
+			parameterDTO.getMinMassgebendesEinkommen());
+
+		BigDecimal multiplyDividedMeMinusMinmE = MathUtil.EXACT.multiply(divided, meMinusMinmE);
+
+		return MathUtil.DEFAULT.addNullSafe(multiplyDividedMeMinusMinmE, parameterDTO.getMinTarif());
+	}
+}
---
+++ ebegu-server/src/test/java/ch/dvbern/ebegu/rechner/TagesschuleRechnerTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2019 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.dvbern.ebegu.rechner;
+
+import java.math.BigDecimal;
+
+import ch.dvbern.ebegu.entities.BelegungTagesschuleModul;
+import ch.dvbern.ebegu.entities.ModulTagesschule;
+import ch.dvbern.ebegu.entities.ModulTagesschuleGroup;
+import ch.dvbern.ebegu.entities.VerfuegungZeitabschnitt;
+import ch.dvbern.ebegu.util.MathUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test fuer TagesschuleTarifRechner
+ */
+public class TagesschuleRechnerTest {
+
+	private final BigDecimal MATA_MIT_PEDAGOGISCHE_BETREUUNG = MathUtil.DEFAULT.fromNullSafe(12.24);
+	private final BigDecimal MATA_OHNE_PEDAGOGISCHE_BETREUUNG = MathUtil.DEFAULT.fromNullSafe(6.11);
+	private final BigDecimal MITA = MathUtil.DEFAULT.fromNullSafe(0.78);
+	private final BigDecimal MAXIMAL_MASSGEGEBENES_EINKOMMEN = MathUtil.DEFAULT.fromNullSafe(160000.00);
+	private final BigDecimal MINIMAL_MASSGEGEBENES_EINKOMMEN = MathUtil.DEFAULT.fromNullSafe(43000.00);
+
+	private TagesschuleRechner tarifRechner = new TagesschuleRechner();
+	private TagesschuleRechnerParameterDTO parameterDTO;
+
+
+	@Before
+	public void setUp() {
+		parameterDTO = getTagesschuleTarifRechnerParameterDTO();
+	}
+
+	@Test
+	public void minimalTarif() {
+		doTest(10000, true, 0.78);
+		doTest(37000, true, 0.78);
+		doTest(10000, false, 0.78);
+		doTest(37000, false, 0.78);
+	}
+
+	@Test
+	public void grenzeMittendrinn() {
+		doTest(67000, true, 3.13);
+		doTest(67100, true, 3.14);
+		doTest(67000, false, 1.87);
+		doTest(67100, false, 1.88);
+	}
+
+	@Test
+	public void maximalTarif() {
+		doTest(150000, true, 11.26);
+		doTest(160000, true, 12.24);
+		doTest(1600000, true, 12.24);
+		doTest(150000, false, 5.65);
+		doTest(160000, false, 6.11);
+		doTest(1600000, false, 6.11);
+	}
+
+	private void doTest(double einkommen, boolean paedagogischBetreut, double expectedTarif) {
+		VerfuegungZeitabschnitt verfuegungZeitabschnitt = new VerfuegungZeitabschnitt();
+		verfuegungZeitabschnitt.setMassgebendesEinkommenVorAbzugFamgr(MathUtil.DEFAULT.fromNullSafe(einkommen));
+		verfuegungZeitabschnitt.setAbzugFamGroesse(BigDecimal.ZERO);
+		BelegungTagesschuleModul modul = new BelegungTagesschuleModul();
+		modul.setModulTagesschule(new ModulTagesschule());
+		modul.getModulTagesschule().setModulTagesschuleGroup(new ModulTagesschuleGroup());
+		modul.getModulTagesschule().getModulTagesschuleGroup().setWirdPaedagogischBetreut(paedagogischBetreut);
+		BigDecimal calculatedTarif = tarifRechner.calculateTarif(verfuegungZeitabschnitt, parameterDTO, modul);
+
+		Assert.assertEquals(MathUtil.DEFAULT.fromNullSafe(expectedTarif), calculatedTarif);
+	}
+
+	private TagesschuleRechnerParameterDTO getTagesschuleTarifRechnerParameterDTO() {
+		TagesschuleRechnerParameterDTO dto = new TagesschuleRechnerParameterDTO();
+		dto.setMaxMassgebendesEinkommen(MAXIMAL_MASSGEGEBENES_EINKOMMEN);
+		dto.setMinMassgebendesEinkommen(MINIMAL_MASSGEGEBENES_EINKOMMEN);
+		dto.setMaxTarifMitPaedagogischerBetreuung(MATA_MIT_PEDAGOGISCHE_BETREUUNG);
+		dto.setMaxTarifOhnePaedagogischeBetreuung(MATA_OHNE_PEDAGOGISCHE_BETREUUNG);
+		dto.setMinTarif(MITA);
+		return dto;
+	}
+}
