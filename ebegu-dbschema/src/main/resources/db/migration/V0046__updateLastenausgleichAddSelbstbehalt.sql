fcdbaf6058606a055136807959e0822a18734b27
egch <christian.egli@dvbern.ch>
2019-12-23 15:36:16 +0100

    direct2dev: flyway number

--- ebegu-dbschema/src/main/resources/db/migration/V0046__updateLastenausgleichAddSelbstbehalt.sql
+++
@@ -1,2 +0,0 @@
-alter table lastenausgleich_grundlagen add selbstbehalt_pro100prozent_platz decimal(19,2) not null;
-alter table lastenausgleich_grundlagen_aud add selbstbehalt_pro100prozent_platz decimal(19,2);
