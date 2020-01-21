8b92871158c977e4246f5a8af24894fef0841653
HEFR <franziska.herger@dvbern.ch>
2020-01-21 13:34:55 +0100

    KIBON-1032: Einstellungen für die Zusatzgutscheine der Gemeinden

---
+++ ebegu-dbschema/src/main/resources/db/migration/V0056__einstellungenZusatzgutscheineDefaultsMandant.sql
@@ -0,0 +1,171 @@
+-- Default-Einstellungen für den Mandanten (pro Gesuchsperiode)
+
+-- GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_ENABLED
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_ENABLED' as einstellung_key,
+			 FALSE 								as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_BETRAG_KITA
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_BETRAG_KITA' as einstellung_key,
+			 '0.00' 							as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_BETRAG_TFO
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_BETRAG_TFO' as einstellung_key,
+			 '0.00' 							as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_BIS_UND_MIT_SCHULSTUFE_KITA
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_BIS_UND_MIT_SCHULSTUFE_KITA' as einstellung_key,
+			 'VORSCHULALTER' 							as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_BIS_UND_MIT_SCHULSTUFE_TFO
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_BIS_UND_MIT_SCHULSTUFE_TFO' as einstellung_key,
+			 'VORSCHULALTER' 							as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_BABYBEITRAG_ENABLED
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_BABYBEITRAG_ENABLED' as einstellung_key,
+			 FALSE 								as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_BABYBEITRAG_BETRAG_KITA
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_BABYBEITRAG_BETRAG_KITA' as einstellung_key,
+			 '0.00' 							as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_BABYBEITRAG_BETRAG_TFO
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_BABYBEITRAG_BETRAG_TFO' as einstellung_key,
+			 '0.00' 							as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_ANSPRUCH_FREIWILLIGENARBEIT_ENABLED
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_ANSPRUCH_FREIWILLIGENARBEIT_ENABLED' as einstellung_key,
+			 FALSE 								as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
+
+-- GEMEINDE_ZUSAETZLICHER_ANSPRUCH_FREIWILLIGENARBEIT_MAXPROZENT
+INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
+						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
+SELECT *
+FROM (SELECT UNHEX(REPLACE(UUID() COLLATE utf8_unicode_ci, '-', ''))    as id,
+			 '2020-01-01 00:00:00'              as timestamp_erstellt,
+			 '2020-01-01 00:00:00'              as timestamp_mutiert,
+			 'flyway'                           as user_erstellt,
+			 'flyway'                           as user_mutiert,
+			 0                                  as version,
+			 'GEMEINDE_ZUSAETZLICHER_ANSPRUCH_FREIWILLIGENARBEIT_MAXPROZENT' as einstellung_key,
+			 '0' 								as value,
+			 NULL                               as gemeinde_id,
+			 gp.id                              as gesuchsperiode_id,
+			 UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-','')) as mandant_id
+	  from gesuchsperiode as gp) as tmp;
