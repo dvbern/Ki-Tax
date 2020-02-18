53dd8772006b52b14a60fa03ad03cef133f8b155
wexa <xaver.weibel@dvbern.ch>
2020-02-18 11:28:33 +0100

    KIBON-1001: Keine E-Mails bei TS Anmeldung / Ablehnung versenden

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/BetreuungServiceBean.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/BetreuungServiceBean.java
@@ -329,7 +329,11 @@ public class BetreuungServiceBean extends AbstractBaseService implements Betreuu
 		Betreuung persistedBetreuung = saveBetreuung(betreuung, false);
 		try {
 			// Bei Ablehnung einer Betreuung muss eine E-Mail geschickt werden
-			mailService.sendInfoBetreuungAbgelehnt(persistedBetreuung);
+			GemeindeStammdaten gemeindeStammdaten =
+				gemeindeService.getGemeindeStammdatenByGemeindeId(persistedBetreuung.extractGesuch().getDossier().getGemeinde().getId()).get();
+			if(gemeindeStammdaten.getBenachrichtigungTsEmailAuto()) {
+				mailService.sendInfoBetreuungAbgelehnt(persistedBetreuung);
+			}
 		} catch (MailException e) {
 			logExceptionAccordingToEnvironment(e,
 				"Mail InfoBetreuungAbgelehnt konnte nicht verschickt werden fuer Betreuung",
