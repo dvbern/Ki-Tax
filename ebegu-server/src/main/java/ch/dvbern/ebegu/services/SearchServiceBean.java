0d1051b19feb82145018b055a50c9b34a13e7b3e
HEFR <franziska.herger@dvbern.ch>
2020-03-24 17:11:03 +0100

    KIBON-1143: Ersten Teil des Tasks wieder rueckgaengig gemacht, siehe Jira

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/SearchServiceBean.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/SearchServiceBean.java
@@ -217,7 +217,7 @@ public class SearchServiceBean extends AbstractBaseService implements SearchServ
 		case SACHBEARBEITER_TS:
 		case ADMIN_TS:
 			if (searchForPendenzen) {
-				predicates.add(createPredicateSCHOrMischGesuche(cb, joinDossier));
+				predicates.add(createPredicateSCHOrMischGesuche(cb, root, joinDossier));
 			}
 			break;
 		default:
@@ -408,10 +408,12 @@ public class SearchServiceBean extends AbstractBaseService implements SearchServ
 	/**
 	 * Ob es ein BG- TS- oder Mischgesuch ist, wird anhand der Verantwortliche berechnet.
 	 * TS- oder Mischgesuche sind alle, die VerantwortlicherTS gesetzt haben, VerantwortlicherBG interessiert nicht.
+	 * Fuer TS sind sie nur so lange in der Pendenzenliste, wie das FinSit-Flag nicht gesetzt ist
 	 */
-	private Predicate createPredicateSCHOrMischGesuche(CriteriaBuilder cb, Join<Gesuch, Dossier> dossier) {
+	private Predicate createPredicateSCHOrMischGesuche(CriteriaBuilder cb, Root<Gesuch> root, Join<Gesuch, Dossier> dossier) {
 		final Predicate predicateIsVerantwortlicherTS = cb.isNotNull(dossier.get(Dossier_.verantwortlicherTS));
-		return cb.and(predicateIsVerantwortlicherTS);
+		final Predicate predicateIsFlagFinSitNotSet = cb.isNull(root.get(Gesuch_.finSitStatus));
+		return cb.and(predicateIsVerantwortlicherTS, predicateIsFlagFinSitNotSet);
 	}
 
 
