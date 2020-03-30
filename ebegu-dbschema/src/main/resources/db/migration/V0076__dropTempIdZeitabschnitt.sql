181e47e4446b20ce4d38a49651157d1a8f458932
HEFR <franziska.herger@dvbern.ch>
2020-03-30 09:40:23 +0200

    directToDev: DB Skript geflickt

--- ebegu-dbschema/src/main/resources/db/migration/V0076__dropTempIdZeitabschnitt.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0076__dropTempIdZeitabschnitt.sql
@@ -3,5 +3,5 @@ alter table bgcalculation_result_aud drop temp_id_zeitabschnitt;
 
 alter table bgcalculation_result modify column abzug_fam_groesse decimal(19,2) not null;
 alter table bgcalculation_result modify column fam_groesse decimal(19,2) not null;
-alter table bgcalculation_result_aud modify column abzug_fam_groesse decimal(19,2) not null;
-alter table bgcalculation_result_aud modify column fam_groesse decimal(19,2) not null;
+alter table bgcalculation_result_aud modify column abzug_fam_groesse decimal(19,2);
+alter table bgcalculation_result_aud modify column fam_groesse decimal(19,2);
