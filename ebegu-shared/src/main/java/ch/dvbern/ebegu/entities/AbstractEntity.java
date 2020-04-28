8767ba93d1499ccfbf7c5566a23fba2c636db3bb
NEFR <frederic.nell@dvbern.ch>
2020-04-28 15:55:52 +0200

    KIBON-577: add vergessene Filter

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/entities/AbstractEntity.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/entities/AbstractEntity.java
@@ -43,6 +43,7 @@ import ch.dvbern.ebegu.util.Constants;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
 import org.apache.lucene.analysis.de.GermanNormalizationFilterFactory;
+import org.apache.lucene.analysis.standard.StandardFilterFactory;
 import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.hibernate.Hibernate;
 import org.hibernate.annotations.Type;
@@ -109,6 +110,7 @@ import org.hibernate.search.annotations.TokenizerDef;
 	name = "EBEGUGermanAnalyzer",
 	tokenizer = @TokenizerDef(factory = StandardTokenizerFactory.class),
 	filters = {
+		@TokenFilterDef(factory = StandardFilterFactory.class),
 		@TokenFilterDef(factory = LowerCaseFilterFactory.class),
 		@TokenFilterDef(factory = GermanNormalizationFilterFactory.class)
 	}
