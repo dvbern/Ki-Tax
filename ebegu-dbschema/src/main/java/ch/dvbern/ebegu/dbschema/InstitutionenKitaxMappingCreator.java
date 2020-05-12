b2db506397a15b3eef2fb2dda5e56213f063f055
HEFR <franziska.herger@dvbern.ch>
2020-05-12 17:00:46 +0200

    KIBON-1255: SpotBugs

--- ebegu-dbschema/src/main/java/ch/dvbern/ebegu/dbschema/InstitutionenKitaxMappingCreator.java
+++ ebegu-dbschema/src/main/java/ch/dvbern/ebegu/dbschema/InstitutionenKitaxMappingCreator.java
@@ -21,6 +21,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -72,10 +73,11 @@ public class InstitutionenKitaxMappingCreator {
 				KitaxInstitution kitax = new KitaxInstitution(kitaArray);
 				println(kitax.toSqlInsert());
 			}
+		} catch (IOException e) {
+			LOG.error("Ki-Tax Daten koennen nicht gelesen werden", e);
+		} finally {
 			printWriter.flush();
 			printWriter.close();
-		} catch (Exception e) {
-			LOG.error("Ki-Tax Daten koennen nicht gelesen werden", e);
 		}
 	}
 
