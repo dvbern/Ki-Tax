4134120c1d8f05e648d5da0681017b7a1f56c5a2
egch <christian.egli@dvbern.ch>
2020-07-02 23:16:45 +0200

    KIBON-1199: show the new override question for superuser too

--- ebegu-web/src/gesuch/component/betreuungView/betreuungView.ts
+++ ebegu-web/src/gesuch/component/betreuungView/betreuungView.ts
@@ -1056,7 +1056,8 @@ export class BetreuungViewController extends AbstractGesuchViewController<TSBetr
             return;
         }
         if (!(!this.getBetreuungModel().isNew() &&
-            this.authServiceRS.isOneOfRoles(TSRoleUtil.getTraegerschaftInstitutionOnlyRoles()))) {
+            (this.authServiceRS.isOneOfRoles(TSRoleUtil.getTraegerschaftInstitutionOnlyRoles())
+            || this.authServiceRS.isOneOfRoles(TSRoleUtil.getSuperAdminRoles())))) {
             return;
         }
         this.mitteilungRS.getNewestBetreuungsmitteilung(this.getBetreuungModel().id)
