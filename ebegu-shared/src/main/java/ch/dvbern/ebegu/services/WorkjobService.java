29ad944c0849224fe1da96bf8e757945a7b244b2
HEFR <franziska.herger@dvbern.ch>
2020-07-15 09:35:13 +0200

    KIBON-1432: Review

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/services/WorkjobService.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/services/WorkjobService.java
@@ -81,7 +81,7 @@ public interface WorkjobService {
 	@Nonnull
 	Workjob createNewReporting(
 		@Nonnull Workjob workJob,
-		@Nonnull ReportVorlage vorlageReportTagesschuleAnmeldungen,
+		@Nonnull ReportVorlage vorlage,
 		@Nonnull String stammdatenId,
 		@Nonnull String gesuchsperiodeId,
 		@Nonnull Locale locale
