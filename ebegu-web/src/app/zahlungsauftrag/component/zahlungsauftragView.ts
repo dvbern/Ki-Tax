62871c1804228352e73df6131694297d1ed8a5c2
HEFR <franziska.herger@dvbern.ch>
2020-10-20 18:07:00 +0200

    KIBON-1625: Datum generiert bei (Test-)Zahlungen muss in der Zukunft liegen, damit gerade erstellte Verfuegungen auch getestet werden koennen.

--- ebegu-web/src/app/zahlungsauftrag/component/zahlungsauftragView.ts
+++ ebegu-web/src/app/zahlungsauftrag/component/zahlungsauftragView.ts
@@ -92,7 +92,7 @@ export class ZahlungsauftragViewController implements IController {
 
     public $onInit(): void {
         // Testlauf darf auch nur in die Zukunft gemacht werden!
-        this.minDateForTestlauf = moment(moment.now()).subtract(1, 'days');
+        this.minDateForTestlauf = moment(moment.now());
         this.updateZahlungsauftrag();
         this.updateGemeindenList();
         this.applicationPropertyRS.isZahlungenTestMode().then((response: any) => {
