01a110f2f868dd32f8e2115074a6ae662f5fde07
Simon Kafader <simon.kafader@dvbern.ch>
2020-11-23 08:53:20 +0100

    KIBON-1586: fix linting issues

--- ebegu-web/src/app/core/new-antrag-list/new-user-select.directive.spec.ts
+++ ebegu-web/src/app/core/new-antrag-list/new-user-select.directive.spec.ts
@@ -2,7 +2,7 @@ import { NewUserSelectDirective } from './new-user-select.directive';
 
 describe('NewUserSelectDirective', () => {
   it('should create an instance', () => {
-    const directive = new NewUserSelectDirective();
+    const directive = new NewUserSelectDirective(null, null);
     expect(directive).toBeTruthy();
   });
 });
