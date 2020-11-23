8fb5f14f36f43097b50b923d496206e5c5cb09d5
Kafader Simon <simon.kafader@dvbern.ch>
2020-11-23 11:23:39 +0100

    KIBON-1586: fix tests

--- ebegu-web/src/app/core/new-antrag-list/new-antrag-list.component.spec.ts
+++ ebegu-web/src/app/core/new-antrag-list/new-antrag-list.component.spec.ts
@@ -1,25 +1,39 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {UpgradeModule} from '@angular/upgrade/static';
+import {TranslateModule} from '@ngx-translate/core';
+import {MaterialModule} from '../../shared/material.module';
+import {GesuchsperiodeRS} from '../service/gesuchsperiodeRS.rest';
+import {InstitutionRS} from '../service/institutionRS.rest';
 
-import { NewAntragListComponent } from './new-antrag-list.component';
+import {NewAntragListComponent} from './new-antrag-list.component';
+import {NewUserSelectDirective} from './new-user-select.directive';
 
 describe('NewAntragListComponent', () => {
-  let component: NewAntragListComponent;
-  let fixture: ComponentFixture<NewAntragListComponent>;
+    let component: NewAntragListComponent;
+    let fixture: ComponentFixture<NewAntragListComponent>;
+    const insitutionSpy = jasmine.createSpyObj<InstitutionRS>(InstitutionRS.name,
+        []);
+    const gesuchPeriodeSpy = jasmine.createSpyObj<GesuchsperiodeRS>(GesuchsperiodeRS.name,
+        []);
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NewAntragListComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [NewAntragListComponent, NewUserSelectDirective],
+            imports: [MaterialModule, TranslateModule.forRoot(), UpgradeModule],
+            providers: [
+                {provide: InstitutionRS, useValue: insitutionSpy},
+                {provide: GesuchsperiodeRS, useValue: gesuchPeriodeSpy},
+            ]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NewAntragListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NewAntragListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
