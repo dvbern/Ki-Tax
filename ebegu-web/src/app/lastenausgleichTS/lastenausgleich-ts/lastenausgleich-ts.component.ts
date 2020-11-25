d385b2f3b5193814202f1f6c483ea373bea87116
ENJA <janik.endtner@dvbern.ch>
2020-11-25 11:39:31 +0100

    KIBON-1583 Anzeige der Components basierend auf Rollen

--- ebegu-web/src/app/lastenausgleichTS/lastenausgleich-ts/lastenausgleich-ts.component.ts
+++ ebegu-web/src/app/lastenausgleichTS/lastenausgleich-ts/lastenausgleich-ts.component.ts
@@ -1,16 +1,28 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {AuthServiceRS} from '../../../authentication/service/AuthServiceRS.rest';
+import {TSRoleUtil} from '../../../utils/TSRoleUtil';
 
 @Component({
-  selector: 'dv-lastenausgleich-ts',
-  templateUrl: './lastenausgleich-ts.component.html',
-  styleUrls: ['./lastenausgleich-ts.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'dv-lastenausgleich-ts',
+    templateUrl: './lastenausgleich-ts.component.html',
+    styleUrls: ['./lastenausgleich-ts.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LastenausgleichTSComponent implements OnInit {
 
-  constructor() { }
+    public constructor(
+        private authServiceRS: AuthServiceRS
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    public ngOnInit(): void {
+    }
 
+    public showToolbar(): boolean {
+        return this.authServiceRS.isOneOfRoles(TSRoleUtil.getMandantRoles());
+    }
+
+    public showKommentare(): boolean {
+        return this.authServiceRS.isOneOfRoles(TSRoleUtil.getMandantRoles());
+    }
 }
