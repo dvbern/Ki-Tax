da4f38a0fcc00ac72081708c28838c772ea2190c
ENJA <janik.endtner@dvbern.ch>
2021-01-14 08:40:53 +0100

    KIBON-1585 PMD

--- ebegu-rest/src/main/java/ch/dvbern/ebegu/api/resource/gemeindeantrag/GemeindeAntragResource.java
+++ ebegu-rest/src/main/java/ch/dvbern/ebegu/api/resource/gemeindeantrag/GemeindeAntragResource.java
@@ -39,13 +39,11 @@ import javax.ws.rs.core.UriInfo;
 import ch.dvbern.ebegu.api.converter.JaxBConverter;
 import ch.dvbern.ebegu.api.dtos.JaxId;
 import ch.dvbern.ebegu.api.dtos.gemeindeantrag.JaxGemeindeAntrag;
-import ch.dvbern.ebegu.entities.Gemeinde;
 import ch.dvbern.ebegu.entities.Gesuchsperiode;
 import ch.dvbern.ebegu.entities.gemeindeantrag.GemeindeAntrag;
 import ch.dvbern.ebegu.enums.ErrorCodeEnum;
 import ch.dvbern.ebegu.enums.gemeindeantrag.GemeindeAntragTyp;
 import ch.dvbern.ebegu.errors.EbeguEntityNotFoundException;
-import ch.dvbern.ebegu.services.GemeindeService;
 import ch.dvbern.ebegu.services.GesuchsperiodeService;
 import ch.dvbern.ebegu.services.gemeindeantrag.GemeindeAntragService;
 import io.swagger.annotations.Api;
@@ -69,9 +67,6 @@ public class GemeindeAntragResource {
 
 	@Inject
 	private GesuchsperiodeService gesuchsperiodeService;
-
-	@Inject
-	private GemeindeService gemeindeService;
 
 	@Inject
 	private JaxBConverter converter;
