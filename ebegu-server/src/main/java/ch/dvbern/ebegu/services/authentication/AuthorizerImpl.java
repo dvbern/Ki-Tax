7c268b410d5bad6909d089e5c0994a404e1a372c
ENJA <janik.endtner@dvbern.ch>
2021-01-19 16:33:31 +0100

    KIBON-1694 add read- and writeAuthorization checks

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/authentication/AuthorizerImpl.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/authentication/AuthorizerImpl.java
@@ -1590,11 +1590,38 @@ public class AuthorizerImpl implements Authorizer, BooleanAuthorizer {
 		if (principalBean.isCallerInAnyOfRole(SUPER_ADMIN, ADMIN_MANDANT, SACHBEARBEITER_MANDANT)) {
 			return;
 		}
-		if (principalBean.isCallerInAnyOfRole(ADMIN_GEMEINDE, SACHBEARBEITER_GEMEINDE)
+		if (principalBean.isCallerInAnyOfRole(UserRole.getTsAndGemeindeRoles())
 			&& principalBean.belongsToGemeinde(antrag.getGemeinde())) {
 			return;
 		}
 		throwViolation(antrag);
+	}
+
+	@Override
+	public void checkWriteAuthorizationLATSGemeindeAntrag(@Nonnull String gemeindeAntragId) {
+		Objects.requireNonNull(gemeindeAntragId);
+		LastenausgleichTagesschuleAngabenGemeindeContainer antrag =
+			(LastenausgleichTagesschuleAngabenGemeindeContainer) gemeindeAntragService.findGemeindeAntrag(
+				GemeindeAntragTyp.LASTENAUSGLEICH_TAGESSCHULEN,
+				gemeindeAntragId
+			).orElseThrow(() -> new EbeguEntityNotFoundException("checkReadAuthorizationLATSGemeindeAntrag", gemeindeAntragId));
+
+		if (principalBean.isCallerInRole(SUPER_ADMIN)) {
+			return;
+		}
+
+		switch (antrag.getStatus()) {
+		case NEU: {
+			if (principalBean.isCallerInAnyOfRole(UserRole.getTsAndGemeindeRoles())
+				&& principalBean.belongsToGemeinde(antrag.getGemeinde())) {
+				return;
+			} else {
+				throwViolation(antrag);
+			}
+		} default: {
+			throwViolation(antrag);
+		}
+		}
 	}
 
 	private boolean isNotSenderTypOrEmpfaengerTyp(@Nullable Mitteilung mitteilung, MitteilungTeilnehmerTyp typ) {
