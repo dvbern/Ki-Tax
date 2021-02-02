bf4ba874a2ed646af91a3721a9577e1b327612ad
ENJA <janik.endtner@dvbern.ch>
2021-02-02 16:00:47 +0100

    KIBON-1474 add proper error handling

--- ebegu-web/src/app/lastenausgleichTS/lastenausgleich-ts-kommentar/lastenausgleich-ts-kommentar.component.ts
+++ ebegu-web/src/app/lastenausgleichTS/lastenausgleich-ts-kommentar/lastenausgleich-ts-kommentar.component.ts
@@ -15,10 +15,13 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
+import {HttpErrorResponse} from '@angular/common/http';
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {TranslateService} from '@ngx-translate/core';
 import {ReplaySubject, Subscription} from 'rxjs';
 import {TSLastenausgleichTagesschuleAngabenGemeindeContainer} from '../../../models/gemeindeantrag/TSLastenausgleichTagesschuleAngabenGemeindeContainer';
+import {ErrorService} from '../../core/errors/service/ErrorService';
 import {LogFactory} from '../../core/logging/LogFactory';
 import {LastenausgleichTSService} from '../services/lastenausgleich-ts.service';
 
@@ -40,7 +43,9 @@ export class LastenausgleichTsKommentarComponent implements OnInit, OnDestroy {
 
     public constructor(
         private readonly lastenausgleichTSService: LastenausgleichTSService,
-        private readonly ref: ChangeDetectorRef
+        private readonly ref: ChangeDetectorRef,
+        private readonly errorService: ErrorService,
+        private readonly translate: TranslateService
     ) {
     }
 
@@ -67,7 +72,11 @@ export class LastenausgleichTsKommentarComponent implements OnInit, OnDestroy {
             this.kommentarControl.value
         ).subscribe(() => {
             this.saving$.next(false);
-        }, error => LOG.error(error));
+        }, (error: HttpErrorResponse) => {
+            LOG.error(error);
+            const translated = this.translate.instant('ERROR_LATS_KOMMENTAR_SAVE');
+            this.errorService.addMesageAsError(translated);
+        });
     }
 
     private initForm(): void {
