61550c53cff87d629a72e89a67de4136f04cb7a1
ENJA <janik.endtner@dvbern.ch>
2021-03-10 15:41:45 +0100

    KIBON-1808 add and remove some roles for Tagesschule Resources

--- ebegu-rest/src/main/java/ch/dvbern/ebegu/api/resource/gemeindeantrag/GemeindeAntragResource.java
+++ ebegu-rest/src/main/java/ch/dvbern/ebegu/api/resource/gemeindeantrag/GemeindeAntragResource.java
@@ -63,10 +63,12 @@ import static ch.dvbern.ebegu.enums.UserRoleName.ADMIN_FERIENBETREUUNG;
 import static ch.dvbern.ebegu.enums.UserRoleName.ADMIN_GEMEINDE;
 import static ch.dvbern.ebegu.enums.UserRoleName.ADMIN_INSTITUTION;
 import static ch.dvbern.ebegu.enums.UserRoleName.ADMIN_MANDANT;
+import static ch.dvbern.ebegu.enums.UserRoleName.ADMIN_TS;
 import static ch.dvbern.ebegu.enums.UserRoleName.SACHBEARBEITER_FERIENBETREUUNG;
 import static ch.dvbern.ebegu.enums.UserRoleName.SACHBEARBEITER_GEMEINDE;
 import static ch.dvbern.ebegu.enums.UserRoleName.SACHBEARBEITER_INSTITUTION;
 import static ch.dvbern.ebegu.enums.UserRoleName.SACHBEARBEITER_MANDANT;
+import static ch.dvbern.ebegu.enums.UserRoleName.SACHBEARBEITER_TS;
 import static ch.dvbern.ebegu.enums.UserRoleName.SUPER_ADMIN;
 
 /**
@@ -132,7 +134,7 @@ public class GemeindeAntragResource {
 	@ApiOperation("Gibt alle Gemeindeanträge zurück, die die Benutzerin sehen kann")
 	@GET
 	@Path("")
-	@RolesAllowed({ SUPER_ADMIN, ADMIN_MANDANT, SACHBEARBEITER_MANDANT, ADMIN_GEMEINDE, SACHBEARBEITER_GEMEINDE,
+	@RolesAllowed({ SUPER_ADMIN, ADMIN_MANDANT, SACHBEARBEITER_MANDANT, ADMIN_GEMEINDE, SACHBEARBEITER_GEMEINDE, ADMIN_TS, SACHBEARBEITER_TS,
 		SACHBEARBEITER_INSTITUTION, ADMIN_INSTITUTION, SACHBEARBEITER_FERIENBETREUUNG, ADMIN_FERIENBETREUUNG })
 	public List<JaxGemeindeAntrag> getAllGemeindeAntraege(
 		@Nullable @QueryParam("gemeinde") String gemeinde,
@@ -148,7 +150,7 @@ public class GemeindeAntragResource {
 	@GET
 	@Path("{gemeindeAntragId}/tagesschulenantraege")
 	@RolesAllowed({ SUPER_ADMIN, ADMIN_MANDANT, SACHBEARBEITER_MANDANT, ADMIN_INSTITUTION, SACHBEARBEITER_INSTITUTION, ADMIN_GEMEINDE,
-		SACHBEARBEITER_GEMEINDE })
+		ADMIN_TS, SACHBEARBEITER_TS, SACHBEARBEITER_GEMEINDE })
 	public List<JaxLastenausgleichTagesschuleAngabenInstitutionContainer> getTagesschuleAntraegeFuerGemeinedAntrag(
 		@Nonnull @Valid @PathParam("gemeindeAntragId") String gemeindeAntragId
 	) {
