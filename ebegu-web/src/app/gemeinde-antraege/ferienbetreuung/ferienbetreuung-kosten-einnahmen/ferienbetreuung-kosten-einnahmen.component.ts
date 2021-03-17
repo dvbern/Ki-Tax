729a2e99341f41386341004421d21818728eb356
ENJA <janik.endtner@dvbern.ch>
2021-03-17 15:21:44 +0100

    KIBON-1809 ferienbetreuung kosten-einahmen form

--- ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-kosten-einnahmen/ferienbetreuung-kosten-einnahmen.component.ts
+++ ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-kosten-einnahmen/ferienbetreuung-kosten-einnahmen.component.ts
@@ -15,7 +15,12 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {TSFerienbetreuungAngaben} from '../../../../models/gemeindeantrag/TSFerienbetreuungAngaben';
+import {TSFerienbetreuungAngabenContainer} from '../../../../models/gemeindeantrag/TSFerienbetreuungAngabenContainer';
+import {numberValidator, ValidationType} from '../../../shared/validators/number-validator.directive';
+import {FerienbetreuungService} from '../services/ferienbetreuung.service';
 
 @Component({
     selector: 'dv-ferienbetreuung-kosten-einnahmen',
@@ -25,10 +30,68 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 })
 export class FerienbetreuungKostenEinnahmenComponent implements OnInit {
 
-    public constructor() {
+    public form: FormGroup;
+    public container: TSFerienbetreuungAngabenContainer;
+
+    public constructor(
+        private readonly ferienbetreuungService: FerienbetreuungService,
+        private readonly fb: FormBuilder,
+        private readonly cd: ChangeDetectorRef
+    ) {
     }
 
     public ngOnInit(): void {
+        this.ferienbetreuungService.getFerienbetreuungContainer()
+            .subscribe(container => {
+                this.container = container;
+                const fbAngaben = container.angabenDeklaration;
+                this.setupForm(fbAngaben);
+                this.cd.markForCheck();
+            });
+    }
+
+    private setupForm(angaben: TSFerienbetreuungAngaben): void {
+        if (!angaben.kostenEinnahmen) {
+            return;
+        }
+        const nutzung = angaben.kostenEinnahmen;
+        this.form = this.fb.group({
+            personalkosten: [
+                nutzung.personalkosten,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            personalkostenLeitungAdmin: [
+                nutzung.personalkostenLeitungAdmin,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            sachkosten: [
+                nutzung.sachkosten,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            verpflegungskosten: [
+                nutzung.verpflegungskosten,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            weitereKosten: [
+                nutzung.weitereKosten,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            bemerkungenKosten: [
+                nutzung.bemerkungenKosten
+            ],
+            elterngebuehren: [
+                nutzung.elterngebuehren,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            weitereEinnahmen: [
+                nutzung.weitereEinnahmen,
+                numberValidator(ValidationType.INTEGER)
+            ],
+        });
+    }
+
+    public onFormSubmit(): void {
+        // TODO: implement
     }
 
 }
