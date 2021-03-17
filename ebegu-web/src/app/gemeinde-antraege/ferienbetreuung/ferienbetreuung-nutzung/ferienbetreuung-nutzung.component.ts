dd951b4b7baa1a73e04fc0c258b5d5dc80bfc223
ENJA <janik.endtner@dvbern.ch>
2021-03-17 10:50:21 +0100

    KIBON-1809 nutzung form GUI

--- ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-nutzung/ferienbetreuung-nutzung.component.ts
+++ ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-nutzung/ferienbetreuung-nutzung.component.ts
@@ -15,7 +15,11 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {TSFerienbetreuungAngaben} from '../../../../models/gemeindeantrag/TSFerienbetreuungAngaben';
+import {numberValidator, ValidationType} from '../../../shared/validators/number-validator.directive';
+import {FerienbetreuungService} from '../services/ferienbetreuung.service';
 
 @Component({
     selector: 'dv-ferienbetreuung-nutzung',
@@ -25,10 +29,74 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 })
 export class FerienbetreuungNutzungComponent implements OnInit {
 
-    public constructor() {
+    public form: FormGroup;
+
+    public constructor(
+        private readonly ferienbetreuungService: FerienbetreuungService,
+        private readonly fb: FormBuilder,
+        private readonly cd: ChangeDetectorRef
+    ) {
     }
 
     public ngOnInit(): void {
+        this.ferienbetreuungService.getFerienbetreuungContainer()
+            .subscribe(container => {
+                const fbAngaben = container.angabenDeklaration;
+                this.setupForm(fbAngaben);
+                this.cd.markForCheck();
+            });
     }
 
+    private setupForm(angaben: TSFerienbetreuungAngaben): void {
+        if (!angaben.nutzung) {
+            return;
+        }
+        const nutzung = angaben.nutzung;
+        this.form = this.fb.group({
+            anzahlBetreuungstageKinderBern: [
+                nutzung.anzahlBetreuungstageKinderBern,
+                numberValidator(ValidationType.HALF)
+            ],
+            betreuungstageKinderDieserGemeinde: [
+                nutzung.betreuungstageKinderDieserGemeinde,
+                numberValidator(ValidationType.HALF)
+            ],
+            betreuungstageKinderDieserGemeindeSonderschueler: [
+                nutzung.betreuungstageKinderDieserGemeindeSonderschueler,
+                numberValidator(ValidationType.HALF)
+            ],
+            davonBetreuungstageKinderAndererGemeinden: [
+                nutzung.davonBetreuungstageKinderAndererGemeinden,
+                numberValidator(ValidationType.HALF)
+            ],
+            davonBetreuungstageKinderAndererGemeindenSonderschueler: [
+                nutzung.davonBetreuungstageKinderAndererGemeindenSonderschueler,
+                numberValidator(ValidationType.HALF)
+            ],
+            anzahlBetreuteKinder: [
+                nutzung.anzahlBetreuteKinder,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            anzahlBetreuteKinderSonderschueler: [
+                nutzung.anzahlBetreuteKinderSonderschueler,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            anzahlBetreuteKinder1Zyklus: [
+                nutzung.anzahlBetreuteKinder1Zyklus,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            anzahlBetreuteKinder2Zyklus: [
+                nutzung.anzahlBetreuteKinder2Zyklus,
+                numberValidator(ValidationType.INTEGER)
+            ],
+            anzahlBetreuteKinder3Zyklus: [
+                nutzung.anzahlBetreuteKinder3Zyklus,
+                numberValidator(ValidationType.INTEGER)
+            ],
+        });
+    }
+
+    public onFormSubmit(): void {
+        // TODO: implement
+    }
 }
