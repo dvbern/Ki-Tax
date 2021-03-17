fa989f347d5c8d054b1a5b6e36f967897b9954fe
ENJA <janik.endtner@dvbern.ch>
2021-03-17 12:33:14 +0100

    KIBON-1809 add simple version of stammdaten-gemeinde form

--- ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-stammdaten-gemeinde/ferienbetreuung-stammdaten-gemeinde.component.ts
+++ ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-stammdaten-gemeinde/ferienbetreuung-stammdaten-gemeinde.component.ts
@@ -15,7 +15,12 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {GemeindeRS} from '../../../../gesuch/service/gemeindeRS.rest';
+import {TSFerienbetreuungAngaben} from '../../../../models/gemeindeantrag/TSFerienbetreuungAngaben';
+import {TSBfsGemeinde} from '../../../../models/TSBfsGemeinde';
+import {FerienbetreuungService} from '../services/ferienbetreuung.service';
 
 @Component({
     selector: 'dv-ferienbetreuung-stammdaten-gemeinde',
@@ -25,10 +30,95 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 })
 export class FerienbetreuungStammdatenGemeindeComponent implements OnInit {
 
-    public constructor() {
+    public form: FormGroup;
+    public formFreigebenTriggered: false;
+    public bfsGemeinden: TSBfsGemeinde[];
+
+    public constructor(
+        private readonly ferienbetreuungService: FerienbetreuungService,
+        private readonly fb: FormBuilder,
+        private readonly cd: ChangeDetectorRef,
+        private readonly gemeindeRS: GemeindeRS
+    ) {
     }
 
     public ngOnInit(): void {
+        this.ferienbetreuungService.getFerienbetreuungContainer()
+            .subscribe(container => {
+                const fbAngaben = container.angabenDeklaration;
+                this.setupForm(fbAngaben);
+                this.cd.markForCheck();
+            });
+        this.gemeindeRS.getAllBfsGemeinden().then(gemeinden => {
+            this.bfsGemeinden = gemeinden;
+            this.cd.markForCheck();
+        });
     }
 
+    private setupForm(angaben: TSFerienbetreuungAngaben): void {
+        if (!angaben.stammdaten) {
+            return;
+        }
+        const stammdaten = angaben.stammdaten;
+        this.form = this.fb.group({
+            traegerschaft: [
+                stammdaten?.traegerschaft
+            ],
+            amAngebotBeteiligteGemeinden: [
+                stammdaten?.amAngebotBeteiligteGemeinden
+            ],
+            seitWannFerienbetreuungen: [
+                stammdaten?.seitWannFerienbetreuungen
+            ],
+            stammdatenAdresseAnschrift: [
+                stammdaten?.stammdatenAdresse?.organisation
+            ],
+            stammdatenAdresseZusatz: [
+                stammdaten?.stammdatenAdresse?.zusatzzeile
+            ],
+            stammdatenAdresseStrasse: [
+                stammdaten?.stammdatenAdresse?.strasse
+            ],
+            stammdatenAdresseNr: [
+                stammdaten?.stammdatenAdresse?.hausnummer
+            ],
+            stammdatenAdressePlz: [
+                stammdaten?.stammdatenAdresse?.plz
+            ],
+            stammdatenAdresseOrt: [
+                stammdaten?.stammdatenAdresse?.ort
+            ],
+            stammdatenKontaktpersonVorname: [
+                stammdaten?.stammdatenKontaktpersonVorname
+            ],
+            stammdatenKontaktpersonNachname: [
+                stammdaten?.stammdatenKontaktpersonNachname
+            ],
+            stammdatenKontaktpersonFunktion: [
+                stammdaten?.stammdatenKontaktpersonFunktion
+            ],
+            stammdatenKontaktpersonTelefon: [
+                stammdaten?.stammdatenKontaktpersonTelefon
+            ],
+            stammdatenKontaktpersonEmail: [
+                stammdaten?.stammdatenKontaktpersonEmail
+            ],
+            iban: [
+                stammdaten?.iban
+            ],
+            kontoinhaber: [
+                stammdaten?.kontoinhaber
+            ],
+            adresseKontoinhaber: [
+                stammdaten?.adresseKontoinhaber
+            ],
+            vermerkAuszahlung: [
+                stammdaten?.vermerkAuszahlung
+            ],
+        });
+    }
+
+    public onFormSubmit(): void {
+        // TODO implement
+    }
 }
