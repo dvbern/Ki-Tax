52712103f3a910d94708f1cbaffeeadf0357594f
ENJA <janik.endtner@dvbern.ch>
2021-03-24 07:44:52 +0100

    KIBON-1810 basic UI and frontend for uplooading files

--- ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-upload/ferienbetreuung-upload.component.ts
+++ ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-upload/ferienbetreuung-upload.component.ts
@@ -16,6 +16,16 @@
  */
 
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {TSFerienbetreuungAngabenContainer} from '../../../../models/gemeindeantrag/TSFerienbetreuungAngabenContainer';
+import {TSFerienbetreuungDokument} from '../../../../models/gemeindeantrag/TSFerienbetreuungDokument';
+import {EbeguUtil} from '../../../../utils/EbeguUtil';
+import {MAX_FILE_SIZE} from '../../../core/constants/CONSTANTS';
+import {ErrorService} from '../../../core/errors/service/ErrorService';
+import {LogFactory} from '../../../core/logging/LogFactory';
+import {UploadRS} from '../../../core/service/uploadRS.rest';
+import {FerienbetreuungService} from '../services/ferienbetreuung.service';
+
+const LOG = LogFactory.createLog('FerienbetreuungUploadComponent');
 
 @Component({
     selector: 'dv-ferienbetreuung-upload',
@@ -25,10 +35,69 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 })
 export class FerienbetreuungUploadComponent implements OnInit {
 
-    public constructor() {
+    public dokumente: TSFerienbetreuungDokument[];
+
+    private container: TSFerienbetreuungAngabenContainer;
+    private filesTooBig: File[];
+
+    public constructor(
+        private ferienbetreuungService: FerienbetreuungService,
+        private uploadRS: UploadRS,
+        private errorService: ErrorService
+    ) {
     }
 
     public ngOnInit(): void {
+        this.ferienbetreuungService.getFerienbetreuungContainer()
+            .subscribe(container => {
+                this.container = container;
+            }, error => {
+                LOG.error(error);
+            });
     }
 
+    public download(dokument: TSFerienbetreuungDokument, attachment: boolean): void {
+        // TODO
+    }
+
+    public onDelete(dokument: TSFerienbetreuungDokument): void {
+        // TODO
+    }
+
+    public onUpload(event: any): void {
+        if (EbeguUtil.isNullOrUndefined(event?.target?.files?.length)) {
+            return;
+        }
+        const files = event.target.files;
+        if (this.checkFilesLength(files as File[])) {
+            return;
+        }
+        this.uploadRS.uploadFerienbetreuungDokumente(files, this.container.id)
+            .then(dokumente => {
+                this.dokumente = dokumente;
+            })
+            .catch(err => {
+                LOG.error(err);
+                this.errorService.addMesageAsError(err);
+            });
+
+    }
+
+    public isReadonly(): boolean {
+        // TODO
+        return false;
+    }
+
+    /**
+     * checks if some files are too big and stores them in filesTooBig variable
+     */
+    private checkFilesLength(files: File[]): boolean {
+        this.filesTooBig = [];
+        for (const file of files) {
+            if (file.size > MAX_FILE_SIZE) {
+                this.filesTooBig.push(file);
+            }
+        }
+        return this.filesTooBig.length > 0;
+    }
 }
