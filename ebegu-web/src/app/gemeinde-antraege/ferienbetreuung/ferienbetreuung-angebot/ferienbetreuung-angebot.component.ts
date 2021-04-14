794f3020a82f9c83e96251e2b4eef28a457a77a4
Simon Kafader <simon.kafader@dvbern.ch>
2021-04-14 08:09:59 +0200

    KIBON-1884: fb angebot adress validation always on

--- ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-angebot/ferienbetreuung-angebot.component.ts
+++ ebegu-web/src/app/gemeinde-antraege/ferienbetreuung/ferienbetreuung-angebot/ferienbetreuung-angebot.component.ts
@@ -205,6 +205,7 @@ export class FerienbetreuungAngebotComponent extends AbstractFerienbetreuungForm
         }, {
             updateOn: 'blur',
         });
+        this.enableAdressValidation();
     }
 
     // overwrite
@@ -244,7 +245,6 @@ export class FerienbetreuungAngebotComponent extends AbstractFerienbetreuungForm
         this.ferienbetreuungService.saveAngebot(this.container.id, this.formToObject())
             .subscribe(() => {
                 this.formValidationTriggered = false;
-                this.form.get('angebotAdresse').clearValidators();
                 this.ferienbetreuungService.updateFerienbetreuungContainerStore(this.container.id);
                 this.errorService.clearAll();
                 this.errorService.addMesageAsInfo(this.translate.instant('SPEICHERN_ERFOLGREICH'));
