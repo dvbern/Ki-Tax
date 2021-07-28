2d9e89ae755de600c3886ab87c68c2a6c8340931
Simon Kafader <simon.kafader@dvbern.ch>
2021-07-28 13:40:52 +0200

    KIBON-1998: don't count pendenzen if role does not allow it

--- ebegu-web/src/utils/TSRoleUtil.ts
+++ ebegu-web/src/utils/TSRoleUtil.ts
@@ -725,4 +725,18 @@ export class TSRoleUtil {
     public static getAllRolesForDossierMitteilungen(): ReadonlyArray<TSRole> {
         return this.getGesuchstellerJugendamtSchulamtOtherAmtRoles().concat(this.getTraegerschaftInstitutionRoles());
     }
+
+    public static getGemeindeBgTSMandantRoles(): ReadonlyArray<TSRole> {
+        return [
+            TSRole.SUPER_ADMIN,
+            TSRole.ADMIN_MANDANT,
+            TSRole.SACHBEARBEITER_MANDANT,
+            TSRole.ADMIN_GEMEINDE,
+            TSRole.SACHBEARBEITER_GEMEINDE,
+            TSRole.ADMIN_BG,
+            TSRole.SACHBEARBEITER_BG,
+            TSRole.ADMIN_TS,
+            TSRole.SACHBEARBEITER_TS
+        ];
+    }
 }
