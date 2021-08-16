7f14a4484c1db8152e262918be77c88d51f9ecd2
Simon Kafader <simon.kafader@dvbern.ch>
2021-08-16 15:16:50 +0200

    KIBON-1998: restore negative warnung error, fix nullpointer

--- ebegu-web/src/app/zahlung/zahlungsauftrag-view-x/zahlungsauftrag-view-x.component.ts
+++ ebegu-web/src/app/zahlung/zahlungsauftrag-view-x/zahlungsauftrag-view-x.component.ts
@@ -108,7 +108,6 @@ export class ZahlungsauftragViewXComponent implements OnInit, AfterViewInit, OnD
         private readonly uiRouterGlobals: UIRouterGlobals,
         private readonly cd: ChangeDetectorRef,
         private readonly dialog: MatDialog,
-        private readonly currency: CurrencyPipe,
         private readonly transition: TransitionService,
         private readonly stateStore: StateStoreService,
         private readonly errorService: ErrorService,
@@ -462,11 +461,6 @@ export class ZahlungsauftragViewXComponent implements OnInit, AfterViewInit, OnD
                 displayFunction: (gemeinde: TSGemeinde) => gemeinde.name,
             },
             {
-                displayedName: this.translate.instant('ZAHLUNG_TOTAL'),
-                attributeName: 'betragTotalAuftrag',
-                displayFunction: (betrag: number) => this.currency.transform(betrag, '', ''),
-            },
-            {
                 displayedName: this.translate.instant('ZAHLUNG_STATUS'),
                 attributeName: 'status',
                 displayFunction: (
@@ -483,6 +477,7 @@ export class ZahlungsauftragViewXComponent implements OnInit, AfterViewInit, OnD
         allColumnNames.splice(0, 0, 'datumFaellig');
         allColumnNames.splice(3, 0, `zahlungPain`, 'zahlungPainExcel');
         allColumnNames.splice(5, 0, `beschrieb`);
+        allColumnNames.splice(6, 0, `betragTotalAuftrag`);
         if (this.principal?.hasOneOfRoles(TSRoleUtil.getAdministratorBgGemeindeRoles())) {
             allColumnNames.push('editSave');
             allColumnNames.push('ausloesen');
