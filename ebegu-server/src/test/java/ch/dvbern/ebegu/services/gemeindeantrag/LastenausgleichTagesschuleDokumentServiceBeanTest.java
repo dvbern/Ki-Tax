03f8c219b105aa17fd9ac689cd22312b5820d0c1
ENJA <janik.endtner@dvbern.ch>
2021-08-23 14:09:59 +0200

    KIBON-1986 finish tests

--- ebegu-server/src/test/java/ch/dvbern/ebegu/services/gemeindeantrag/LastenausgleichTagesschuleDokumentServiceBeanTest.java
+++ ebegu-server/src/test/java/ch/dvbern/ebegu/services/gemeindeantrag/LastenausgleichTagesschuleDokumentServiceBeanTest.java
@@ -18,24 +18,26 @@
 package ch.dvbern.ebegu.services.gemeindeantrag;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Optional;
 
-import ch.dvbern.ebegu.authentication.PrincipalBean;
 import ch.dvbern.ebegu.docxmerger.lats.LatsDocxDTO;
 import ch.dvbern.ebegu.entities.Adresse;
-import ch.dvbern.ebegu.entities.Benutzer;
 import ch.dvbern.ebegu.entities.Gemeinde;
 import ch.dvbern.ebegu.entities.GemeindeStammdaten;
 import ch.dvbern.ebegu.entities.gemeindeantrag.LastenausgleichTagesschuleAngabenGemeinde;
 import ch.dvbern.ebegu.entities.gemeindeantrag.LastenausgleichTagesschuleAngabenGemeindeContainer;
 import ch.dvbern.ebegu.enums.Sprache;
+import ch.dvbern.ebegu.mocks.PrincipalBeanMock;
 import ch.dvbern.ebegu.services.GemeindeService;
 import org.easymock.EasyMockExtension;
 import org.easymock.EasyMockSupport;
 import org.easymock.Mock;
 import org.easymock.TestSubject;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.easymock.EasyMock.expect;
@@ -46,42 +48,54 @@ public class LastenausgleichTagesschuleDokumentServiceBeanTest extends EasyMockS
 	@TestSubject
 	private final LastenausgleichTagesschuleDokumentServiceBean serviceBean = new LastenausgleichTagesschuleDokumentServiceBean();
 
-	@Mock
+	@Mock()
 	private GemeindeService gemeindeServiceMock;
 
-	@Mock
-	private PrincipalBean principalMock;
+	private static LastenausgleichTagesschuleAngabenGemeindeContainer container;
+
+	@BeforeAll()
+	public static void beforeAll() {
+		container = createContainer();
+	}
+
+	@BeforeEach()
+	public void beforeEach() {
+		createGemeindeServiceMock();
+		replayAll();
+		serviceBean.principalBean = new PrincipalBeanMock();
+	}
 
 	@Test
 	public void testCalculations() {
-		createGemeindeServiceMock();
-		createPrincipalMock();
-		replayAll();
+		LatsDocxDTO dto = serviceBean.toLatsDocxDTO(container, new BigDecimal("2000"), Sprache.DEUTSCH);
 
-		LatsDocxDTO dto = serviceBean.toLatsDocxDTO(createContainer(), new BigDecimal("2000"), Sprache.DEUTSCH);
-
-		Assert.assertEquals(new BigDecimal("1000"), dto.getElterngebuehrenProg());
+		Assertions.assertEquals(new BigDecimal("1000"), dto.getElterngebuehrenProg().setScale(0, RoundingMode.CEILING));
+		Assertions.assertEquals(new BigDecimal("20000"), dto.getNormlohnkostenTotalProg().setScale(0, RoundingMode.CEILING));
+		Assertions.assertEquals(new BigDecimal("19000"), dto.getLastenausgleichsberechtigterBetragProg().setScale(0 , RoundingMode.CEILING));
+		Assertions.assertEquals(new BigDecimal("9500"), dto.getErsteRateProg().setScale(0, RoundingMode.CEILING));
+		Assertions.assertEquals(new BigDecimal("7000"), dto.getZweiteRate().setScale(0, RoundingMode.CEILING));
+		Assertions.assertEquals(new BigDecimal("16500"), dto.getAuszahlungTotal().setScale(0, RoundingMode.CEILING));
 	}
 
 	private void createGemeindeServiceMock() {
 		expect(gemeindeServiceMock.getGemeindeStammdatenByGemeindeId(
-			"abcd"
-		)).andReturn(Optional.of(createGemeindeStammdaten()));
+			container.getGemeinde().getId()
+		))
+			.andReturn(Optional.of(createGemeindeStammdaten()))
+			.anyTimes();
 
 	}
 
-	private void createPrincipalMock() {
-		expect(principalMock.getBenutzer()).andReturn(createBenutzer());
-	}
-
-	private LastenausgleichTagesschuleAngabenGemeindeContainer createContainer() {
+	private static LastenausgleichTagesschuleAngabenGemeindeContainer createContainer() {
 		LastenausgleichTagesschuleAngabenGemeindeContainer container = new LastenausgleichTagesschuleAngabenGemeindeContainer();
 		LastenausgleichTagesschuleAngabenGemeinde korrektur = new LastenausgleichTagesschuleAngabenGemeinde();
 		Gemeinde gemeinde = new Gemeinde();
 
 		korrektur.setLastenausgleichberechtigteBetreuungsstunden(new BigDecimal("1000"));
-		korrektur.setNormlohnkostenBetreuungBerechnet(new BigDecimal("10"));
+		korrektur.setNormlohnkostenBetreuungBerechnet(new BigDecimal("10000"));
 		korrektur.setEinnahmenElterngebuehren(new BigDecimal("500"));
+		korrektur.setLastenausgleichsberechtigerBetrag(new BigDecimal("10000"));
+		korrektur.setErsteRateAusbezahlt(new BigDecimal("3000"));
 
 		container.setAngabenKorrektur(korrektur);
 		container.setGemeinde(gemeinde);
@@ -94,9 +108,5 @@ public class LastenausgleichTagesschuleDokumentServiceBeanTest extends EasyMockS
 		Adresse adresse = new Adresse();
 		stammdaten.setAdresse(adresse);
 		return stammdaten;
-	}
-
-	private Benutzer createBenutzer() {
-		return new Benutzer();
 	}
 }
