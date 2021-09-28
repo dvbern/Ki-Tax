b6fc12f46aa184bee9bd7160779535b9753a11ca
ENJA <janik.endtner@dvbern.ch>
2021-09-28 16:41:34 +0200

    KIBON-2012 show input fields red in date picker

--- ebegu-web/src/app/shared/component/dv-date-picker/dv-date-picker-x.component.ts
+++ ebegu-web/src/app/shared/component/dv-date-picker/dv-date-picker-x.component.ts
@@ -16,13 +16,15 @@
  */
 
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ControlContainer, NgForm} from '@angular/forms';
 import * as moment from 'moment';
 
 @Component({
     selector: 'dv-date-picker-x',
     templateUrl: './dv-date-picker-x.component.html',
     changeDetection: ChangeDetectionStrategy.Default,
-    styleUrls: ['dv-date-picker-x.component.less']
+    styleUrls: ['dv-date-picker-x.component.less'],
+    viewProviders: [ { provide: ControlContainer, useExisting: NgForm } ]
 })
 export class DvDatePickerXComponent implements OnInit {
 
