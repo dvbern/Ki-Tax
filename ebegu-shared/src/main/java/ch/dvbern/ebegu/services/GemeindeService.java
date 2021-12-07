b87521f6debf5d8bf20f26eff026995229bc4b62
Simon Kafader <simon.kafader@dvbern.ch>
2021-12-07 16:22:00 +0100

    KIBON-2160: new endpoint to get next bfsnummer for besondere volksschulen

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/services/GemeindeService.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/services/GemeindeService.java
@@ -135,6 +135,9 @@ public interface GemeindeService {
 	@Nonnull
 	Optional<BfsGemeinde> findBfsGemeinde(@Nonnull Long bfsNummer);
 
+	@Nonnull
+	Long getNextBesondereVolksschuleBfsNummer();
+
 	/**
 	 * aktiviert oder deaktiviert das BG Angebot
 	 */
