9f23563bdec071bcae403d11b052e98581973a21
Kafader Simon <simon.kafader@dvbern.ch>
2021-12-23 08:38:18 +0100

    KIBON-2158: use visitor pattern to get mandant related classes and data without switches

---
+++ ebegu-dbschema/src/main/resources/db/migration/V0216__AddMandantIdentifier.sql
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2021 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ALTER TABLE mandant
+ADD COLUMN IF NOT EXISTS mandant_identifier VARCHAR(255) NOT NULL DEFAULT 'BERN';
+
+ALTER TABLE mandant_aud
+ADD COLUMN IF NOT EXISTS mandant_identifier VARCHAR(255);
+
+UPDATE mandant SET mandant_identifier = 'LUZERN' WHERE id = '485d7483-30a2-11ec-a86f-b89a2ae4a038';
+UPDATE mandant SET mandant_identifier = 'SOLOTHURN' WHERE id = '7781a6bb-5374-11ec-98e8-f4390979fa3e';
