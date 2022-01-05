32e5a0e7f35866a82e0b98ede06ec007e98b99af
Kafader Simon <simon.kafader@dvbern.ch>
2022-01-05 14:58:21 +0100

    KIBON-2199: migrate enable-element directive

---
+++ ebegu-web/src/app/shared/directive/enable-element.directive.ts
@@ -0,0 +1,38 @@
+import {
+    Directive,
+    ElementRef,
+    Input,
+    OnChanges,
+    Optional,
+    Self,
+} from '@angular/core';
+import {NgControl} from '@angular/forms';
+import {AuthServiceRS} from '../../../authentication/service/AuthServiceRS.rest';
+import {TSRole} from '../../../models/enums/TSRole';
+import {TSRoleUtil} from '../../../utils/TSRoleUtil';
+
+@Directive({
+    selector: '[dvEnableElement]',
+})
+export class EnableElementDirective implements OnChanges {
+
+    @Input() public allowedRoles: ReadonlyArray<TSRole> = TSRoleUtil.getAllRoles();
+    @Input() public enableExpression: boolean = true;
+
+    public constructor(
+        private el: ElementRef,
+        private authService: AuthServiceRS,
+        @Optional() @Self() public ngControl: NgControl,
+    ) {
+    }
+
+    private setElementDisabled(): void {
+        this.ngControl?.valueAccessor.setDisabledState(
+            !this.authService.isOneOfRoles(this.allowedRoles) || !this.enableExpression);
+    }
+
+    public ngOnChanges(): void {
+        this.setElementDisabled();
+    }
+
+}
