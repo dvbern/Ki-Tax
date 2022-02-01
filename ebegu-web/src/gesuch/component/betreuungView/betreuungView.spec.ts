80d4fe6e57835ce5b55e28f7c5b434c8e35b5d41
BUAN <anita.brunner@dvbern.ch>
2022-02-01 16:01:10 +0100

    KIBON-2227: ts-lint

--- ebegu-web/src/gesuch/component/betreuungView/betreuungView.spec.ts
+++ ebegu-web/src/gesuch/component/betreuungView/betreuungView.spec.ts
@@ -16,7 +16,6 @@
 import {StateService} from '@uirouter/core';
 import {EinstellungRS} from '../../../admin/service/einstellungRS.rest';
 import {CORE_JS_MODULE} from '../../../app/core/core.angularjs.module';
-import {ApplicationPropertyRS} from '../../../app/core/rest-services/applicationPropertyRS.rest';
 import {InstitutionStammdatenRS} from '../../../app/core/service/institutionStammdatenRS.rest';
 import {AuthServiceRS} from '../../../authentication/service/AuthServiceRS.rest';
 import {ngServicesMock} from '../../../hybridTools/ngServicesMocks';
