203005b42d016c3ba3843fa5cb115cb6132faf67
NEFR <frederic.nell@dvbern.ch>
2022-03-07 09:27:05 +0100

    KIBON-2259: Komish, die ganze Prüfung würde basiert auf einer Frage die niemals bei Solo kommt. Dazu würden Luzern Methode auch im Solo FinSit gefunden. Das ist ja sehr komish. Ich habe alles auf das FamSit Frage basiert, wie bei Bern und die LU Fragen die würden niemals beantwortet habe ich weggenommen. Sonst micro Korrektur im Skript, Kolumn Name hatte nicht gestimmt.

--- ebegu-web/src/gesuch/component/finanzielleSituation/solothurn/AbstractFinSitsolothurnView.ts
+++ ebegu-web/src/gesuch/component/finanzielleSituation/solothurn/AbstractFinSitsolothurnView.ts
@@ -57,14 +57,12 @@ export abstract class AbstractFinSitsolothurnView extends AbstractGesuchViewX<TS
             return;
         }
         this.getModel().finanzielleSituationJA.quellenbesteuert = undefined;
-        this.getModel().finanzielleSituationJA.gemeinsameStekVorjahr = undefined;
-        this.getModel().finanzielleSituationJA.alleinigeStekVorjahr = undefined;
         this.getModel().finanzielleSituationJA.veranlagt = undefined;
     }
 
     public showSelbstdeklaration(): boolean {
         return EbeguUtil.isNotNullAndTrue(this.getModel().finanzielleSituationJA.quellenbesteuert)
-            || EbeguUtil.isNotNullAndFalse(this.getModel().finanzielleSituationJA.gemeinsameStekVorjahr)
+            || EbeguUtil.isNotNullAndFalse(this.model.gemeinsameSteuererklaerung)
             || EbeguUtil.isNotNullAndFalse(this.getModel().finanzielleSituationJA.alleinigeStekVorjahr)
             || EbeguUtil.isNotNullAndFalse(this.getModel().finanzielleSituationJA.veranlagt);
     }
@@ -77,26 +75,12 @@ export abstract class AbstractFinSitsolothurnView extends AbstractGesuchViewX<TS
         return !this.gesuchModelManager.isGesuchsteller2Required();
     }
 
-    public quellenBesteuertChange(newQuellenBesteuert: MatRadioChange): void {
-        if (newQuellenBesteuert.value === false) {
-            return;
-        }
-        this.getModel().finanzielleSituationJA.gemeinsameStekVorjahr = undefined;
-        this.getModel().finanzielleSituationJA.alleinigeStekVorjahr = undefined;
-        this.getModel().finanzielleSituationJA.veranlagt = undefined;
-    }
-
     public gemeinsameStekVisible(): boolean {
         return this.isGemeinsam() && EbeguUtil.isNotNullAndFalse(this.getModel().finanzielleSituationJA.quellenbesteuert);
     }
 
     public alleinigeStekVisible(): boolean {
         return !this.isGemeinsam() && EbeguUtil.isNotNullAndFalse(this.getModel().finanzielleSituationJA.quellenbesteuert);
-    }
-
-    public veranlagtVisible(): boolean {
-        return EbeguUtil.isNotNullAndTrue(this.getModel().finanzielleSituationJA.gemeinsameStekVorjahr)
-            || EbeguUtil.isNotNullAndTrue(this.getModel().finanzielleSituationJA.alleinigeStekVorjahr);
     }
 
     public gemeinsameStekChange(newGemeinsameStek: MatRadioChange): void {
@@ -106,7 +90,7 @@ export abstract class AbstractFinSitsolothurnView extends AbstractGesuchViewX<TS
     }
 
     public alleinigeStekVorjahrChange(newAlleinigeStekVorjahr: MatRadioChange): void {
-        if (newAlleinigeStekVorjahr.value === false && EbeguUtil.isNullOrFalse(this.getModel().finanzielleSituationJA.gemeinsameStekVorjahr)) {
+        if (newAlleinigeStekVorjahr.value === false && EbeguUtil.isNullOrFalse(this.model.gemeinsameSteuererklaerung)) {
             this.getModel().finanzielleSituationJA.veranlagt = undefined;
         }
     }
@@ -114,7 +98,7 @@ export abstract class AbstractFinSitsolothurnView extends AbstractGesuchViewX<TS
     public getYearForDeklaration(): number | string {
         const currentYear = this.getBasisjahrPlus1();
         const previousYear = this.getBasisjahr();
-        if (this.getModel().finanzielleSituationJA.gemeinsameStekVorjahr) {
+        if (this.model.gemeinsameSteuererklaerung) {
             return previousYear;
         }
         return currentYear;
@@ -229,15 +213,15 @@ export abstract class AbstractFinSitsolothurnView extends AbstractGesuchViewX<TS
     }
 
     public hasSteuerveranlagungErhalten(): boolean {
+        if (this.gesuchstellerNumber === 2 && this.isSteuerveranlagungGemeinsam()) {
+            // this is only saved on the primary GS for Solothurn
+            return this.getGesuch().gesuchsteller1.finanzielleSituationContainer.finanzielleSituationJA.steuerveranlagungErhalten;
+        }
         return this.getModel().finanzielleSituationJA.steuerveranlagungErhalten;
     }
 
     public isSteuerveranlagungGemeinsam(): boolean {
-        if (this.gesuchstellerNumber === 2) {
-            // this is only saved on the primary GS for Solothurn
-            return this.getGesuch().gesuchsteller1.finanzielleSituationContainer.finanzielleSituationJA.gemeinsameStekVorjahr;
-        }
-        return this.getModel().finanzielleSituationJA.gemeinsameStekVorjahr;
+        return this.model.gemeinsameSteuererklaerung;
     }
 
     protected resetVeranlagungSolothurn(): void {
