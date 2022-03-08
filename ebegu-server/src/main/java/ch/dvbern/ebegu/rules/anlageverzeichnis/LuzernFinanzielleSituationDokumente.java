e606dd6b1b3a0ad56f0e1f44f0d228c81e6e9cb4
BUAN <anita.brunner@dvbern.ch>
2022-03-08 16:34:16 +0100

    KIBON-2236: Add FinanzielleSituation Dokumente Luzern

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/anlageverzeichnis/LuzernFinanzielleSituationDokumente.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/anlageverzeichnis/LuzernFinanzielleSituationDokumente.java
@@ -26,7 +26,11 @@ import javax.annotation.Nullable;
 import ch.dvbern.ebegu.entities.AbstractFinanzielleSituation;
 import ch.dvbern.ebegu.entities.DokumentGrund;
 import ch.dvbern.ebegu.entities.Familiensituation;
+import ch.dvbern.ebegu.entities.FinanzielleSituation;
 import ch.dvbern.ebegu.entities.Gesuch;
+import ch.dvbern.ebegu.entities.GesuchstellerContainer;
+import ch.dvbern.ebegu.enums.DokumentGrundPersonType;
+import ch.dvbern.ebegu.enums.DokumentGrundTyp;
 import ch.dvbern.ebegu.enums.DokumentTyp;
 
 public class LuzernFinanzielleSituationDokumente extends AbstractDokumente<AbstractFinanzielleSituation, Familiensituation> {
@@ -37,12 +41,90 @@ public class LuzernFinanzielleSituationDokumente extends AbstractDokumente<Abstr
 		@Nonnull Set<DokumentGrund> anlageVerzeichnis,
 		@Nonnull Locale locale) {
 
+		final Familiensituation familiensituation = gesuch.extractFamiliensituation();
+		final boolean gemeinsam = familiensituation != null &&
+			familiensituation.getGemeinsameSteuererklaerung() != null &&
+			familiensituation.getGemeinsameSteuererklaerung();
+
+		if(gemeinsam) {
+			getAllDokumenteGemeinsam(gesuch, anlageVerzeichnis);
+		} else {
+			getAllDokumenteGS1(gesuch, anlageVerzeichnis);
+			getAllDokumenteGS2(gesuch, anlageVerzeichnis);
+		}
+	}
+
+	private void getAllDokumenteGS1(Gesuch gesuch, Set<DokumentGrund> anlageVerzeichnis) {
+		final GesuchstellerContainer gesuchsteller = gesuch.getGesuchsteller1();
+		getAllDokumenteGesuchsteller(gesuchsteller, 1, gesuch, anlageVerzeichnis);
+	}
+
+	private void getAllDokumenteGS2(Gesuch gesuch, Set<DokumentGrund> anlageVerzeichnis) {
+		final GesuchstellerContainer gesuchsteller = gesuch.getGesuchsteller2();
+		getAllDokumenteGesuchsteller(gesuchsteller, 2, gesuch, anlageVerzeichnis);
+	}
+
+	private void getAllDokumenteGemeinsam(Gesuch gesuch, Set<DokumentGrund> anlageVerzeichnis) {
+		GesuchstellerContainer gesuchsteller1 = gesuch.getGesuchsteller1();
+
+		if (gesuchsteller1 == null || gesuchsteller1.getFinanzielleSituationContainer() == null) {
+			return;
+		}
+
+		final FinanzielleSituation finanzielleSituationGS1JA = gesuchsteller1.getFinanzielleSituationContainer().getFinanzielleSituationJA();
+
+		if (finanzielleSituationGS1JA.getVeranlagt() != null && finanzielleSituationGS1JA.getVeranlagt()) {
+			addDokument(DokumentTyp.STEUERVERANLAGUNG, gesuch, anlageVerzeichnis, 0, finanzielleSituationGS1JA);
+		} else {
+			getAllDokumenteGS1(gesuch, anlageVerzeichnis);
+			getAllDokumenteGS2(gesuch, anlageVerzeichnis);
+		}
+	}
+
+	private void getAllDokumenteGesuchsteller(
+		GesuchstellerContainer gesuchsteller,
+		int gesuchstellerNumber,
+		Gesuch gesuch,
+		Set<DokumentGrund> anlageVerzeichnis) {
+
+		if (gesuchsteller == null || gesuchsteller.getFinanzielleSituationContainer() == null) {
+			return;
+		}
+
+		final FinanzielleSituation finanzielleSituationJA = gesuchsteller.getFinanzielleSituationContainer().getFinanzielleSituationJA();
+
+		if (finanzielleSituationJA.getVeranlagt() != null && finanzielleSituationJA.getVeranlagt()) {
+			addDokument(DokumentTyp.STEUERVERANLAGUNG, gesuch, anlageVerzeichnis, gesuchstellerNumber, finanzielleSituationJA);
+		} else {
+			//Selbstdeklaration
+			addDokument(DokumentTyp.JAHRESLOHNAUSWEISE, gesuch, anlageVerzeichnis, gesuchstellerNumber, finanzielleSituationJA);
+			addDokument(DokumentTyp.NACHWEIS_VERMOEGEN, gesuch, anlageVerzeichnis, gesuchstellerNumber, finanzielleSituationJA);
+		}
+	}
+
+	private void addDokument(
+		DokumentTyp dokumentTyp,
+		Gesuch gesuch,
+		Set<DokumentGrund> anlageVerzeichnis,
+		int gesuchstellerNumber,
+		FinanzielleSituation finanzielleSituationJA) {
+
+		final int basisJahr = gesuch.getGesuchsperiode().getGueltigkeit().calculateEndOfPreviousYear().getYear();
+
+		add(getDokument(
+				dokumentTyp,
+				finanzielleSituationJA,
+				String.valueOf(basisJahr),
+				DokumentGrundPersonType.GESUCHSTELLER,
+				gesuchstellerNumber,
+				DokumentGrundTyp.FINANZIELLESITUATION),
+			anlageVerzeichnis);
 	}
 
 	@Override
 	public boolean isDokumentNeeded(
 		@Nonnull DokumentTyp dokumentTyp,
 		@Nullable AbstractFinanzielleSituation dataForDocument) {
-		return false;
+		return true;
 	}
 }
