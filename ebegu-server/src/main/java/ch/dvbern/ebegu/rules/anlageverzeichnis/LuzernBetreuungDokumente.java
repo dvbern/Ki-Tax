bfb8faa6050d7378efb04ab66d6623f8d78c5234
BUAN <anita.brunner@dvbern.ch>
2022-05-04 11:26:02 +0200

    KIBON-2370: Add Betreuungsdokumente for Luzern

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/anlageverzeichnis/LuzernBetreuungDokumente.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/anlageverzeichnis/LuzernBetreuungDokumente.java
@@ -15,6 +15,7 @@
 
 package ch.dvbern.ebegu.rules.anlageverzeichnis;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
@@ -24,6 +25,8 @@ import javax.annotation.Nullable;
 import ch.dvbern.ebegu.entities.Betreuung;
 import ch.dvbern.ebegu.entities.DokumentGrund;
 import ch.dvbern.ebegu.entities.Gesuch;
+import ch.dvbern.ebegu.enums.DokumentGrundPersonType;
+import ch.dvbern.ebegu.enums.DokumentGrundTyp;
 import ch.dvbern.ebegu.enums.DokumentTyp;
 
 public class LuzernBetreuungDokumente extends AbstractDokumente<Betreuung, Object> {
@@ -34,10 +37,48 @@ public class LuzernBetreuungDokumente extends AbstractDokumente<Betreuung, Objec
 		@Nonnull Set<DokumentGrund> anlageVerzeichnis,
 		@Nonnull Locale locale
 	) {
+		final List<Betreuung> allBetreuungen = gesuch.extractAllBetreuungen();
+
+		if (allBetreuungen.isEmpty()) {
+			return;
+		}
+
+		allBetreuungen
+			.forEach(betreuung -> {
+				add(getDokument(DokumentTyp.BESTAETIGUNG_AUSSERORDENTLICHER_BETREUUNGSAUFWAND, betreuung, gesuch), anlageVerzeichnis);
+				add(getDokument(DokumentTyp.BESTAETIGUNG_KITA_PLUS, betreuung, gesuch), anlageVerzeichnis);
+			}
+		);
+	}
+
+	@Nullable
+	private DokumentGrund getDokument(DokumentTyp dokumentTyp, @Nonnull Betreuung betreuung, Gesuch gesuch) {
+		String gpString = gesuch.getGesuchsperiode().getGesuchsperiodeString();
+
+		return getDokument(
+			dokumentTyp,
+			betreuung,
+			gpString,
+			DokumentGrundPersonType.KIND,
+			betreuung.getKind().getKindNummer(),
+			DokumentGrundTyp.ERWEITERTE_BETREUUNG);
 	}
 
 	@Override
 	public boolean isDokumentNeeded(@Nonnull DokumentTyp dokumentTyp, @Nullable Betreuung betreuung) {
-		return false;
+		if (betreuung == null ||
+			betreuung.getErweiterteBetreuungContainer().getErweiterteBetreuungJA() == null) {
+			return false;
+		}
+
+		switch (dokumentTyp) {
+		case BESTAETIGUNG_KITA_PLUS:
+			return betreuung.getErweiterteBetreuungContainer().getErweiterteBetreuungJA().getKitaPlusZuschlag() != null &&
+				betreuung.getErweiterteBetreuungContainer().getErweiterteBetreuungJA().getKitaPlusZuschlag();
+		case BESTAETIGUNG_AUSSERORDENTLICHER_BETREUUNGSAUFWAND:
+			return betreuung.getErweiterteBetreuungContainer().getErweiterteBetreuungJA().getErweiterteBeduerfnisse();
+		default:
+			return false;
+		}
 	}
 }
