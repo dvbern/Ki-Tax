444da039aa9dc99902cb1c7e0c0ea710070dccaf
Franziska Herger <franziska.herger@dvbern.ch>
2022-05-19 17:58:02 +0200

    KIBON-2288: Einkommensänderung Paramenter in Bemerkungen * Parameter müssen als String mitgegeben werden, letzten Param bei EINKOMMENSVERSCHLECHTERUNG_NOT_ACCEPT umbenannt

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/EinkommenCalcRule.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/EinkommenCalcRule.java
@@ -197,7 +197,7 @@ public class EinkommenCalcRule extends AbstractCalcRule {
 						MsgKey.EINKOMMENSVERSCHLECHTERUNG_ACCEPT_MSG,
 						locale,
 						String.valueOf(basisjahrPlus1),
-						finanzDatenDTO.getMinEKV(),
+						String.valueOf(finanzDatenDTO.getMinEKV()),
 						String.valueOf(basisjahr));
 				}
 			} else {
@@ -214,7 +214,7 @@ public class EinkommenCalcRule extends AbstractCalcRule {
 						MsgKey.EINKOMMENSVERSCHLECHTERUNG_NOT_ACCEPT_MSG,
 						locale,
 						String.valueOf(basisjahrPlus1),
-						finanzDatenDTO.getMinEKV(),
+						String.valueOf(finanzDatenDTO.getMinEKV()),
 						String.valueOf(basisjahr));
 				}
 			}
@@ -230,7 +230,7 @@ public class EinkommenCalcRule extends AbstractCalcRule {
 						MsgKey.EINKOMMENSVERSCHLECHTERUNG_ACCEPT_MSG,
 						locale,
 						String.valueOf(basisjahrPlus2),
-						finanzDatenDTO.getMinEKV(),
+						String.valueOf(finanzDatenDTO.getMinEKV()),
 						String.valueOf(basisjahr));
 				}
 			} else {
@@ -246,7 +246,7 @@ public class EinkommenCalcRule extends AbstractCalcRule {
 						MsgKey.EINKOMMENSVERSCHLECHTERUNG_NOT_ACCEPT_MSG,
 						locale,
 						String.valueOf(basisjahrPlus2),
-						finanzDatenDTO.getMinEKV(),
+						String.valueOf(finanzDatenDTO.getMinEKV()),
 						String.valueOf(basisjahr));
 				}
 			}
