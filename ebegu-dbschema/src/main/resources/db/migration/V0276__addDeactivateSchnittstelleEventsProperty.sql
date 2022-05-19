1fb35d2602fe7bc9e5aa2f892e2f390d93de5aa3
NEFR <frederic.nell@dvbern.ch>
2022-05-19 12:07:20 +0200

    Direkt-to-dev: UUID schon verwendet

--- ebegu-dbschema/src/main/resources/db/migration/V0276__addDeactivateSchnittstelleEventsProperty.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0276__addDeactivateSchnittstelleEventsProperty.sql
@@ -18,20 +18,20 @@
 # bern
 INSERT INTO application_property (id, mandant_id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert,
 								  version, vorgaenger_id, name, value)
-VALUES (UNHEX(REPLACE('d3a81288-5cc7-11ec-93ed-f4390979fa3e', '-', '')),
+VALUES (UNHEX(REPLACE('d4a81288-5cc7-11ec-93ed-f4390979fa3e', '-', '')),
 		UNHEX(REPLACE('e3736eb8-6eef-40ef-9e52-96ab48d8f220', '-', '')), '2021-12-14 12:00:00', '2021-12-14 12:00:00',
 		'flyway', 'flyway', 0, NULL, 'SCHNITTSTELLE_EVENTS_AKTIVIERT', 'true');
 
 # solothurn
 INSERT INTO application_property (id, mandant_id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert,
 								  version, vorgaenger_id, name, value)
-VALUES (UNHEX(REPLACE('4d759f85-5cc8-11ec-93ed-f4390979fa3e', '-', '')),
+VALUES (UNHEX(REPLACE('5d759f85-5cc8-11ec-93ed-f4390979fa3e', '-', '')),
 		UNHEX(REPLACE('7781a6bb-5374-11ec-98e8-f4390979fa3e', '-', '')), '2021-12-14 12:00:00', '2021-12-14 12:00:00',
 		'flyway', 'flyway', 0, NULL, 'SCHNITTSTELLE_EVENTS_AKTIVIERT', 'false');
 
 # luzern
 INSERT INTO application_property (id, mandant_id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert,
 								  version, vorgaenger_id, name, value)
-VALUES (UNHEX(REPLACE('522c58af-5cc8-11ec-93ed-f4390979fa3e', '-', '')),
+VALUES (UNHEX(REPLACE('532c58af-5cc8-11ec-93ed-f4390979fa3e', '-', '')),
 		UNHEX(REPLACE('485d7483-30a2-11ec-a86f-b89a2ae4a038', '-', '')), '2021-12-14 12:00:00', '2021-12-14 12:00:00',
 		'flyway', 'flyway', 0, NULL, 'SCHNITTSTELLE_EVENTS_AKTIVIERT', 'true');
