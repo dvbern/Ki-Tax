0921c96018a71f19af71ec06dbf470bcc0f80d82
BUAN <anita.brunner@dvbern.ch>
2022-05-20 09:13:32 +0200

    KIBON-2271: stuendliche vollkosten bei abweichungsmeldung übernehmen

--- ebegu-dbschema/src/main/resources/db/migration/V0272__addEinstellungAndInputStuendlicheVollkostenBeiTFO.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0272__addEinstellungAndInputStuendlicheVollkostenBeiTFO.sql
@@ -1,2 +1,8 @@
 ALTER table betreuungspensum add stuendliche_vollkosten DECIMAL(19, 2);
 ALTER table betreuungspensum_aud add stuendliche_vollkosten DECIMAL(19, 2);
+
+ALTER table betreuungspensum_abweichung add stuendliche_vollkosten DECIMAL(19, 2);
+ALTER table betreuungspensum_abweichung_aud add stuendliche_vollkosten DECIMAL(19, 2);
+
+ALTER table betreuungsmitteilung_pensum add stuendliche_vollkosten DECIMAL(19, 2);
+ALTER table betreuungsmitteilung_pensum_aud add stuendliche_vollkosten DECIMAL(19, 2);
