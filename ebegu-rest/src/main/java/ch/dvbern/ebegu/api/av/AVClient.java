db683888b258300074107e9114154e3fff707335
Franziska Herger <franziska.herger@dvbern.ch>
2022-05-23 09:57:11 +0200

    KIBON-2200: DEV eingemergt, Flyway-Skripts umbenannt

--- ebegu-rest/src/main/java/ch/dvbern/ebegu/api/av/AVClient.java
+++ ebegu-rest/src/main/java/ch/dvbern/ebegu/api/av/AVClient.java
@@ -20,7 +20,6 @@ package ch.dvbern.ebegu.api.av;
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 import java.util.Map.Entry;
