10065909571046a89f329ec310e972e5b0316749
NEFR <frederic.nell@dvbern.ch>
2022-05-30 14:58:08 +0200

    KIBON-2442: klini Korrektur wegen is oder nicht Gemeinsam, fuer Veranlagung muss man auch die Frage stellen so UD oder nicht spielt keine Rolle

--- ebegu-web/src/gesuch/component/finanzielleSituation/bern/finanzielleSituationView/finanzielleSituationView.ts
+++ ebegu-web/src/gesuch/component/finanzielleSituation/bern/finanzielleSituationView/finanzielleSituationView.ts
@@ -155,8 +155,8 @@ export class FinanzielleSituationViewController extends AbstractFinSitBernView {
         if (this.model.gemeinsameSteuererklaerung) {
             return false;
         }
-        // wenn die Steuerdatenzugriff Frage nicht anezeigt wird, muss die veranlagungsfrage angezeigt werden
-        if (!this.showZugriffAufSteuerdaten()) {
+        // bei einem Papiergesuch muss man es anzeigen, die Steuerdatenzugriff Frage ist nicht gestellt
+        if (!this.gesuchModelManager.getGesuch().isOnlineGesuch() || (!this.authServiceRS.isRole(TSRole.GESUCHSTELLER) && EbeguUtil.isNullOrUndefined(this.model.getFiSiConToWorkWith().finanzielleSituationGS))) {
             return true;
         }
         // falls steuerschnittstelle aktiv, aber zugriffserlaubnis noch nicht beantwortet, dann zeigen wir die Frage
