38109e52f4143025abba9d6750727df28bbb3177
Kafader Simon <simon.kafader@dvbern.ch>
2022-06-22 09:21:16 +0200

    direct-to-dev: increase timeout for pendenzen batchjob

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/DailyBatchBean.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/DailyBatchBean.java
@@ -228,6 +228,8 @@ public class DailyBatchBean implements DailyBatch {
 	}
 
 	@Override
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	@TransactionTimeout(unit = TimeUnit.MINUTES, value = 30)
 	public void runBatchInfoOffenePendenzenNeueMitteilungInstitution() {
 		try {
 			LOGGER.info("Starting Job InfoOffenePendenzenNeueMitteilungInstitution...");
