65a2bba42004d28bb836bb46e241cf95d4ffd4f7
Franziska Herger <franziska.herger@dvbern.ch>
2022-07-06 12:40:35 +0200

    KIBON-2461: Test kann normaler JUnit Test sein

--- ebegu-server/src/test/java/ch/dvbern/ebegu/services/zahlungen/ZahlungsfileGeneratorPainTest.java
+++ ebegu-server/src/test/java/ch/dvbern/ebegu/services/zahlungen/ZahlungsfileGeneratorPainTest.java
@@ -1,24 +1,17 @@
 package ch.dvbern.ebegu.services.zahlungen;
 
 import ch.dvbern.oss.lib.beanvalidation.embeddables.IBAN;
-import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.needle4j.annotation.ObjectUnderTest;
-import org.needle4j.junit.NeedleRule;
 
 @SuppressWarnings("unused")
 class ZahlungsfileGeneratorPainTest {
 
-	@Rule
-	public NeedleRule needleRule = new NeedleRule();
-
-	@ObjectUnderTest
-	private ZahlungsfileGeneratorPain zahlungsfileGeneratorBern;
+	private final ZahlungsfileGeneratorPain zahlungsfileGeneratorPain = new ZahlungsfileGeneratorPain();
 
 	@Test
 	void ibanToUnformattedString() {
-		Assertions.assertEquals("CH3909000000306638172", zahlungsfileGeneratorBern.ibanToUnformattedString(new IBAN("CH39 0900 0000 3066 3817 2")));
-		Assertions.assertEquals("CH3909000000306638172", zahlungsfileGeneratorBern.ibanToUnformattedString(new IBAN("CH3909000000306638172")));
+		Assertions.assertEquals("CH3909000000306638172", zahlungsfileGeneratorPain.ibanToUnformattedString(new IBAN("CH39 0900 0000 3066 3817 2")));
+		Assertions.assertEquals("CH3909000000306638172", zahlungsfileGeneratorPain.ibanToUnformattedString(new IBAN("CH3909000000306638172")));
 	}
 }
