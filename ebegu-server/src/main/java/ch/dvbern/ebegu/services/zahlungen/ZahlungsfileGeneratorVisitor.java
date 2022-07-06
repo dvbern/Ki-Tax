0d12a3bc9ed02f2081f5cba42a06961394a97c1e
Franziska Herger <franziska.herger@dvbern.ch>
2022-07-06 14:17:27 +0200

    KIBON-2461: Infoma oder Pain ist nicht fix, sondern wird über ein Property ermittelt

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/zahlungen/ZahlungsfileGeneratorVisitor.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/zahlungen/ZahlungsfileGeneratorVisitor.java
@@ -4,11 +4,24 @@ import javax.annotation.Nonnull;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
+import ch.dvbern.ebegu.entities.ApplicationProperty;
 import ch.dvbern.ebegu.entities.Mandant;
+import ch.dvbern.ebegu.enums.ApplicationPropertyKey;
+import ch.dvbern.ebegu.enums.ErrorCodeEnum;
+import ch.dvbern.ebegu.errors.EbeguEntityNotFoundException;
+import ch.dvbern.ebegu.services.ApplicationPropertyService;
+import ch.dvbern.ebegu.services.MandantService;
+import ch.dvbern.ebegu.util.mandant.MandantIdentifier;
 import ch.dvbern.ebegu.util.mandant.MandantVisitor;
 
 @Dependent
 public class ZahlungsfileGeneratorVisitor implements MandantVisitor<IZahlungsfileGenerator> {
+
+	@Inject
+	private ApplicationPropertyService applicationPropertyService;
+
+	@Inject
+	private MandantService mandantService;
 
 	@Inject
 	private ZahlungsfileGeneratorPain painGenerator;
@@ -24,16 +37,36 @@ public class ZahlungsfileGeneratorVisitor implements MandantVisitor<IZahlungsfil
 
 	@Override
 	public IZahlungsfileGenerator visitBern() {
-		return painGenerator;
+		return getZahlungsfileGeneratorForMandant(MandantIdentifier.BERN);
 	}
 
 	@Override
 	public IZahlungsfileGenerator visitLuzern() {
-		return infomaGenerator;
+		return getZahlungsfileGeneratorForMandant(MandantIdentifier.LUZERN);
 	}
 
 	@Override
 	public IZahlungsfileGenerator visitSolothurn() {
+		return getZahlungsfileGeneratorForMandant(MandantIdentifier.SOLOTHURN);
+	}
+
+	@Nonnull
+	private IZahlungsfileGenerator getZahlungsfileGeneratorForMandant(@Nonnull MandantIdentifier mandantIdentifier) {
+		if (isInfomaZahlungenActivatedForMandant(mandantIdentifier)) {
+			return infomaGenerator;
+		}
 		return painGenerator;
+	}
+
+	private boolean isInfomaZahlungenActivatedForMandant(@Nonnull MandantIdentifier mandantIdentifier) {
+		final Mandant mandant = mandantService
+			.findMandantByIdentifier(mandantIdentifier)
+			.orElseThrow(() -> new EbeguEntityNotFoundException("findMandantByIdentifier", ErrorCodeEnum.ERROR_ENTITY_NOT_FOUND));
+		ApplicationProperty infomaZahlungen  =
+			this.applicationPropertyService.readApplicationProperty(
+					ApplicationPropertyKey.INFOMA_ZAHLUNGEN,
+					mandant)
+				.orElse(null);
+		return infomaZahlungen != null && Boolean.getBoolean(infomaZahlungen.getValue());
 	}
 }
