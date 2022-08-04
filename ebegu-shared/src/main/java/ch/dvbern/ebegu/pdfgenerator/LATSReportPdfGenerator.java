46c180a567679ffaffb1d3a3ecc7ef0f74a7a5f1
Simon Kafader <simon.kafader@dvbern.ch>
2022-08-04 11:52:14 +0200

    KIBON-2486: lats pdf content

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/pdfgenerator/LATSReportPdfGenerator.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/pdfgenerator/LATSReportPdfGenerator.java
@@ -18,7 +18,6 @@
 package ch.dvbern.ebegu.pdfgenerator;
 
 import java.io.OutputStream;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,21 +28,16 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import ch.dvbern.ebegu.entities.GemeindeStammdaten;
-import ch.dvbern.ebegu.entities.gemeindeantrag.FerienbetreuungAngabenAngebot;
-import ch.dvbern.ebegu.entities.gemeindeantrag.FerienbetreuungAngabenKostenEinnahmen;
-import ch.dvbern.ebegu.entities.gemeindeantrag.FerienbetreuungAngabenNutzung;
-import ch.dvbern.ebegu.entities.gemeindeantrag.FerienbetreuungAngabenStammdaten;
+import ch.dvbern.ebegu.entities.gemeindeantrag.LastenausgleichTagesschuleAngabenGemeinde;
 import ch.dvbern.ebegu.entities.gemeindeantrag.LastenausgleichTagesschuleAngabenGemeindeContainer;
 import ch.dvbern.ebegu.enums.Sprache;
 import ch.dvbern.ebegu.pdfgenerator.PdfGenerator.CustomGenerator;
 import ch.dvbern.ebegu.pdfgenerator.pdfTable.SimplePDFTable;
-import ch.dvbern.ebegu.util.Constants;
 import ch.dvbern.lib.invoicegenerator.errors.InvoiceGeneratorException;
 import com.lowagie.text.Document;
 import com.lowagie.text.Element;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfPTable;
-import org.apache.commons.lang.StringUtils;
 
 public class LATSReportPdfGenerator extends MandantPdfGenerator {
 
@@ -57,14 +51,19 @@ public class LATSReportPdfGenerator extends MandantPdfGenerator {
 	protected static final String ANGEBOT_ALLEN_OFFEN = "PdfGeneration_angebotAllenOffen";
 
 	protected static final String ABRECHNUNG = "PdfGeneration_abrechnung";
-	protected static final String BETREUUNGSSTUNDEN_OHNE_BESONDERE_ANFORDERUNGEN = "PdfGeneration_betreuungsstundenOhneAnforderungen";
-	protected static final String BETREUUNGSSTUNDEN_MIT_BESONDERE_ANFORDERUNGEN = "PdfGeneration_betreuungsstundenMitAnforderungen";
-	protected static final String BETREUUNGSSTUNDEN_VOLKSSCHULANGEBOT = "PdfGeneration_betreuungsstundenVolksschulangebot";
-	protected static final String LASTENAUSGLEICHBERECHTIGTE_BETREUUNGSSTUNDEN = "PdfGeneration_lastenasugleichsberechtigteBetreuungsstunden";
+	protected static final String BETREUUNGSSTUNDEN_OHNE_BESONDERE_ANFORDERUNGEN =
+			"PdfGeneration_betreuungsstundenOhneAnforderungen";
+	protected static final String BETREUUNGSSTUNDEN_MIT_BESONDERE_ANFORDERUNGEN =
+			"PdfGeneration_betreuungsstundenMitAnforderungen";
+	protected static final String BETREUUNGSSTUNDEN_VOLKSSCHULANGEBOT =
+			"PdfGeneration_betreuungsstundenVolksschulangebot";
+	protected static final String LASTENAUSGLEICHBERECHTIGTE_BETREUUNGSSTUNDEN =
+			"PdfGeneration_lastenasugleichsberechtigteBetreuungsstunden";
 	protected static final String ZU_NORMLOHNKOSTEN_HOCH = "PdfGeneration_zuNormlohnkostenHoch";
 	protected static final String ZU_NORMLOHNKOSTEN_TIEF = "PdfGeneration_zuNormlohnkostenTief";
 	protected static final String NORMLOHNKOSTEN_BETREUUNG = "PdfGeneration_normlohnkostenBetreuung";
-	protected static final String TATSACHLICHE_EINNAHMEN_ELTERNGEBUEHREN = "PdfGeneration_tatsachlicheEinnahmenElterngebuehren";
+	protected static final String TATSACHLICHE_EINNAHMEN_ELTERNGEBUEHREN =
+			"PdfGeneration_tatsachlicheEinnahmenElterngebuehren";
 
 	protected static final String LATS_BETRAG = "PdfGeneration_latsBetrag";
 	protected static final String ERSTE_RATE = "PdfGeneration_ersteRate";
@@ -86,7 +85,9 @@ public class LATSReportPdfGenerator extends MandantPdfGenerator {
 
 	protected static final String KONTROLLFRAGEN = "PdfGeneration_kontrollfragen";
 	protected static final String BETREUUNGSSTUNDEN_DOKUMENTIERT = "PdfGeneration_betreuungsstundenDokumentiert";
-	protected static final String ELTERNGEBUHREN_GEMAESS_TSVERORDNUNG = "PdfGeneration_elterngebuhrenGemaessTSVerordnung";
+	protected static final String BEMERKUNG = "PdfGeneration_bemerkung";
+	protected static final String ELTERNGEBUHREN_GEMAESS_TSVERORDNUNG =
+			"PdfGeneration_elterngebuhrenGemaessTSVerordnung";
 	protected static final String ELTERN_BELEGE = "PdfGeneration_elternBelege";
 	protected static final String ELTERN_MAXTARIF = "PdfGeneration_elternMaxtarif";
 	protected static final String HAELFTE_AUSGEBILDET = "PdfGeneration_haelfteAusgebildet";
@@ -100,7 +101,7 @@ public class LATSReportPdfGenerator extends MandantPdfGenerator {
 	private NoAdressPdfGenerator pdfGenerator;
 
 	@Nonnull
-	protected final LastenausgleichTagesschuleAngabenGemeindeContainer
+	private final LastenausgleichTagesschuleAngabenGemeindeContainer
 			lastenausgleichTagesschuleAngabenGemeindeContainer;
 
 	public LATSReportPdfGenerator(
@@ -157,12 +158,255 @@ public class LATSReportPdfGenerator extends MandantPdfGenerator {
 	protected CustomGenerator getCustomGenerator() {
 		return (generator, ctx) -> {
 			Document document = generator.getDocument();
+
+			document.add(this.createStatus());
+
+			Paragraph angabenGemeindeHeaeder = new Paragraph(translate(ANGABEN_GEMEINDE, mandant));
+			angabenGemeindeHeaeder.setSpacingAfter(SUB_HEADER_SPACING_AFTER);
+			document.add(angabenGemeindeHeaeder);
+			document.add(this.createTableAllgemeineAngaben());
+
+			Paragraph abrechnungHeader = new Paragraph(translate(ABRECHNUNG, mandant));
+			abrechnungHeader.setSpacingAfter(SUB_HEADER_SPACING_AFTER);
+			document.add(abrechnungHeader);
+			document.add(this.createTableAbrechnung());
+
+			Paragraph kostenbeteilugungHeader = new Paragraph(translate(KOSTENBETEILIGUNG_GEMEINDE, mandant));
+			kostenbeteilugungHeader.setSpacingAfter(SUB_HEADER_SPACING_AFTER);
+			document.add(kostenbeteilugungHeader);
+			document.add(this.createTableKostenbeteiligung());
+
+			Paragraph weitereKostenEtraegeHeader = new Paragraph(translate(WEITERE_KOSTEN_ETRAGE, mandant));
+			weitereKostenEtraegeHeader.setSpacingAfter(SUB_HEADER_SPACING_AFTER);
+			document.add(weitereKostenEtraegeHeader);
+			document.add(this.createTableWeitereKostenEtraege());
+
+			Paragraph kontrollfragenHeader = new Paragraph(translate(KONTROLLFRAGEN, mandant));
+			kontrollfragenHeader.setSpacingAfter(SUB_HEADER_SPACING_AFTER);
+			document.add(kontrollfragenHeader);
+			document.add(this.createTableKontrollfragen());
 		};
 	}
 
-	@Nullable
-	private Integer getIntValue(@Nullable BigDecimal value) {
-		return value == null ? null : value.intValue();
+	@Nonnull
+	private Element createStatus() {
+		Paragraph paragraph = new Paragraph(translate("LastenausgleichTagesschuleAngabenGemeindeStatus_"
+				+ this.lastenausgleichTagesschuleAngabenGemeindeContainer.getStatusString(), mandant));
+		paragraph.setSpacingAfter(TABLE_SPACING_AFTER);
+		return paragraph;
+	}
+
+	private Element createTableAllgemeineAngaben() {
+		LastenausgleichTagesschuleAngabenGemeinde angabenGemeinde =
+				(Objects.requireNonNull(lastenausgleichTagesschuleAngabenGemeindeContainer.isInBearbeitungGemeinde() ?
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenDeklaration() :
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenKorrektur()));
+
+		SimplePDFTable table = new SimplePDFTable(pdfGenerator.getConfiguration(), false);
+		table.addHeaderRow(translate(ALLGEMEINE_ANGABEN_GEMEINDE, mandant), "");
+		table.addRow(
+				translate(BEDARF_TS_ANGEBOT_ELTERN_ABGEKLAERT, mandant),
+				getBooleanAsString(angabenGemeinde.getBedarfBeiElternAbgeklaert()));
+		table.addRow(
+				translate(BESTEHT_ANGEBOT, mandant, getNextSchuljahrAsString()),
+				getBooleanAsString(angabenGemeinde.getAngebotFuerFerienbetreuungVorhanden()));
+		table.addRow(
+				translate(ANGEBOT_ALLEN_OFFEN, mandant),
+				getBooleanAsString(angabenGemeinde.getAngebotVerfuegbarFuerAlleSchulstufen()));
+
+		PdfPTable pdfPTable = table.createTable();
+		pdfPTable.setSpacingAfter(TABLE_SPACING_AFTER);
+
+		return pdfPTable;
+	}
+
+	private Element createTableAbrechnung() {
+		LastenausgleichTagesschuleAngabenGemeinde angabenGemeinde =
+				(Objects.requireNonNull(lastenausgleichTagesschuleAngabenGemeindeContainer.isInBearbeitungGemeinde() ?
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenDeklaration() :
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenKorrektur()));
+
+		SimplePDFTable table = new SimplePDFTable(pdfGenerator.getConfiguration(), false);
+		table.addHeaderRow(translate(ABRECHNUNG, mandant), "");
+		table.addRow(
+				translate(ALLE_TS_ANMELDUNGEN_IN_KIBON, mandant),
+				getBooleanAsString(lastenausgleichTagesschuleAngabenGemeindeContainer.getAlleAngabenInKibonErfasst()));
+		table.addRow(
+				translate(BETREUUNGSSTUNDEN_OHNE_BESONDERE_ANFORDERUNGEN, mandant, getSchuljahrAsString()),
+				angabenGemeinde.getGeleisteteBetreuungsstundenOhneBesondereBeduerfnisse());
+		table.addRow(
+				translate(BETREUUNGSSTUNDEN_MIT_BESONDERE_ANFORDERUNGEN, mandant, getSchuljahrAsString()),
+				angabenGemeinde.getGeleisteteBetreuungsstundenBesondereBeduerfnisse());
+		table.addRow(
+				translate(BETREUUNGSSTUNDEN_VOLKSSCHULANGEBOT, mandant, getSchuljahrBasisjahrAsString()),
+				angabenGemeinde.getGeleisteteBetreuungsstundenBesondereVolksschulangebot());
+		table.addRow(
+				translate(LASTENAUSGLEICHBERECHTIGTE_BETREUUNGSSTUNDEN, mandant),
+				angabenGemeinde.getLastenausgleichberechtigteBetreuungsstunden());
+		table.addRow(
+				translate(ZU_NORMLOHNKOSTEN_HOCH, mandant),
+				angabenGemeinde.getDavonStundenZuNormlohnMehrAls50ProzentAusgebildete());
+		table.addRow(
+				translate(ZU_NORMLOHNKOSTEN_TIEF, mandant),
+				angabenGemeinde.getDavonStundenZuNormlohnWenigerAls50ProzentAusgebildete());
+		table.addRow(
+				translate(NORMLOHNKOSTEN_BETREUUNG, mandant),
+				angabenGemeinde.getNormlohnkostenBetreuungBerechnet());
+		table.addRow(
+				translate(TATSACHLICHE_EINNAHMEN_ELTERNGEBUEHREN, mandant, getSchuljahrAsString()),
+				angabenGemeinde.getEinnahmenElterngebuehren());
+
+		table.addRow(
+				translate(LATS_BETRAG, mandant, getSchuljahrAsString()),
+				angabenGemeinde.getLastenausgleichsberechtigerBetrag());
+		table.addRow(
+				translate(ERSTE_RATE, mandant, getSchuljahrBasisjahrAsString()),
+				angabenGemeinde.getErsteRateAusbezahlt());
+		table.addRow(
+				translate(SCHLUSSZAHLUNG, mandant, getSchuljahrBasisjahrPlus1AsString()),
+				angabenGemeinde.getSchlusszahlung());
+
+		PdfPTable pdfPTable = table.createTable();
+		pdfPTable.setSpacingAfter(TABLE_SPACING_AFTER);
+
+		return pdfPTable;
+	}
+
+	private Element createTableKostenbeteiligung() {
+		LastenausgleichTagesschuleAngabenGemeinde angabenGemeinde =
+				(Objects.requireNonNull(lastenausgleichTagesschuleAngabenGemeindeContainer.isInBearbeitungGemeinde() ?
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenDeklaration() :
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenKorrektur()));
+
+		SimplePDFTable table = new SimplePDFTable(pdfGenerator.getConfiguration(), false);
+		table.addHeaderRow(translate(KOSTENBETEILIGUNG_GEMEINDE, mandant), "");
+		table.addRow(
+				translate(GESAMTKOSTEN_TS, mandant, getSchuljahrAsString()),
+				angabenGemeinde.getGesamtKostenTagesschule());
+		table.addRow(translate(EINNAHMEN_LASTENAUSGLEICH, mandant), "");
+		table.addRow(translate(EINNAHMEN_ELTERNGEBUEHREN, mandant), angabenGemeinde.getEinnahmenElterngebuehren());
+		table.addRow(translate(EINNAHMEN_VERPFLEGUNG, mandant), angabenGemeinde.getEinnnahmenVerpflegung());
+		table.addRow(translate(EINNAHMEN_SUBVENTIONEN, mandant), angabenGemeinde.getEinnahmenSubventionenDritter());
+		table.addRow(translate(KOSTENBEITRAG_GEMEINDE, mandant), angabenGemeinde.getKostenbeitragGemeinde());
+		table.addRow(translate(ERTRAGSUBERSCHUSS_GEMEINDE, mandant), angabenGemeinde.getUeberschussVerwendung());
+		table.addRow(
+				translate(ERWARTETER_KOSTENBEITRAG_GEMEINDE, mandant),
+				angabenGemeinde.getErwarteterKostenbeitragGemeinde());
+		table.addRow(
+				translate(UBERSCHUSS_VORJAHR, mandant, getPreviousSchuljahrAsString()),
+				getBooleanAsString(angabenGemeinde.getUeberschussErzielt()));
+
+		PdfPTable pdfPTable = table.createTable();
+		pdfPTable.setSpacingAfter(TABLE_SPACING_AFTER);
+
+		return pdfPTable;
+	}
+
+	private Element createTableWeitereKostenEtraege() {
+		LastenausgleichTagesschuleAngabenGemeinde angabenGemeinde =
+				(Objects.requireNonNull(lastenausgleichTagesschuleAngabenGemeindeContainer.isInBearbeitungGemeinde() ?
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenDeklaration() :
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenKorrektur()));
+
+		SimplePDFTable table = new SimplePDFTable(pdfGenerator.getConfiguration(), false);
+		table.addHeaderRow(translate(WEITERE_KOSTEN_ETRAGE, mandant), "");
+		table.addRow(
+				translate(BEMERKUNGEN_KOSTEN_ETRAGE, mandant),
+				angabenGemeinde.getBemerkungenWeitereKostenUndErtraege());
+
+		PdfPTable pdfPTable = table.createTable();
+		pdfPTable.setSpacingAfter(TABLE_SPACING_AFTER);
+
+		return pdfPTable;
+	}
+
+	private Element createTableKontrollfragen() {
+		LastenausgleichTagesschuleAngabenGemeinde angabenGemeinde =
+				(Objects.requireNonNull(lastenausgleichTagesschuleAngabenGemeindeContainer.isInBearbeitungGemeinde() ?
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenDeklaration() :
+						lastenausgleichTagesschuleAngabenGemeindeContainer.getAngabenKorrektur()));
+
+		SimplePDFTable table = new SimplePDFTable(pdfGenerator.getConfiguration(), false);
+		table.addHeaderRow(translate(KONTROLLFRAGEN, mandant), "");
+
+		table.addRow(
+				translate(BETREUUNGSSTUNDEN_DOKUMENTIERT, mandant),
+				getBooleanAsString(angabenGemeinde.getBetreuungsstundenDokumentiertUndUeberprueft()));
+		if (Boolean.FALSE.equals(angabenGemeinde.getBetreuungsstundenDokumentiertUndUeberprueft())) {
+			table.addRow(
+					translate(BEMERKUNG, mandant),
+					angabenGemeinde.getBetreuungsstundenDokumentiertUndUeberprueftBemerkung());
+		}
+
+		table.addRow(
+				translate(ELTERNGEBUHREN_GEMAESS_TSVERORDNUNG, mandant),
+				getBooleanAsString(angabenGemeinde.getElterngebuehrenGemaessVerordnungBerechnet()));
+		if (Boolean.FALSE.equals(angabenGemeinde.getElterngebuehrenGemaessVerordnungBerechnet())) {
+			table.addRow(
+					translate(BEMERKUNG, mandant),
+					angabenGemeinde.getElterngebuehrenGemaessVerordnungBerechnetBemerkung());
+		}
+
+		table.addRow(
+				translate(ELTERN_BELEGE, mandant),
+				getBooleanAsString(angabenGemeinde.getEinkommenElternBelegt()));
+		if (Boolean.FALSE.equals(angabenGemeinde.getEinkommenElternBelegt())) {
+			table.addRow(translate(BEMERKUNG, mandant), angabenGemeinde.getEinkommenElternBelegtBemerkung());
+		}
+
+		table.addRow(translate(ELTERN_MAXTARIF, mandant), getBooleanAsString(angabenGemeinde.getMaximalTarif()));
+		if (Boolean.FALSE.equals(angabenGemeinde.getMaximalTarif())) {
+			table.addRow(translate(BEMERKUNG, mandant), angabenGemeinde.getMaximalTarifBemerkung());
+		}
+
+		table.addRow(
+				translate(HAELFTE_AUSGEBILDET, mandant),
+				getBooleanAsString(angabenGemeinde.getMindestens50ProzentBetreuungszeitDurchAusgebildetesPersonal()));
+		if (Boolean.FALSE.equals(angabenGemeinde.getMindestens50ProzentBetreuungszeitDurchAusgebildetesPersonal())) {
+			table.addRow(
+					translate(BEMERKUNG, mandant),
+					angabenGemeinde.getMindestens50ProzentBetreuungszeitDurchAusgebildetesPersonalBemerkung());
+		}
+
+		table.addRow(
+				translate(AUSBILDUNGEN_ZERTIFIZIERT, mandant),
+				getBooleanAsString(angabenGemeinde.getAusbildungenMitarbeitendeBelegt()));
+		if (Boolean.FALSE.equals(angabenGemeinde.getAusbildungenMitarbeitendeBelegt())) {
+			table.addRow(translate(BEMERKUNG, mandant), angabenGemeinde.getAusbildungenMitarbeitendeBelegtBemerkung());
+		}
+
+		PdfPTable pdfPTable = table.createTable();
+		pdfPTable.setSpacingAfter(TABLE_SPACING_AFTER);
+
+		return pdfPTable;
+	}
+
+	private String getPreviousSchuljahrAsString() {
+		return this.lastenausgleichTagesschuleAngabenGemeindeContainer.getGesuchsperiode().getBasisJahr()
+				+ "/"
+				+ String.valueOf(this.lastenausgleichTagesschuleAngabenGemeindeContainer.getGesuchsperiode()
+				.getBasisJahrPlus1()).substring(2);
+	}
+
+	private String getNextSchuljahrAsString() {
+		return this.lastenausgleichTagesschuleAngabenGemeindeContainer.getGesuchsperiode().getBasisJahrPlus2()
+				+ "/"
+				+ String.valueOf(this.lastenausgleichTagesschuleAngabenGemeindeContainer.getGesuchsperiode()
+				.getBasisJahrPlus2() + 1).substring(2);
+	}
+
+	private String getSchuljahrAsString() {
+		return this.lastenausgleichTagesschuleAngabenGemeindeContainer.getGesuchsperiode().getGesuchsperiodeString();
+	}
+
+	private String getSchuljahrBasisjahrAsString() {
+		return String.valueOf(this.lastenausgleichTagesschuleAngabenGemeindeContainer.getGesuchsperiode()
+				.getBasisJahrPlus1());
+	}
+
+	private String getSchuljahrBasisjahrPlus1AsString() {
+		return String.valueOf(this.lastenausgleichTagesschuleAngabenGemeindeContainer.getGesuchsperiode()
+				.getBasisJahrPlus2());
 	}
 
 	@Nonnull
@@ -175,7 +419,6 @@ public class LATSReportPdfGenerator extends MandantPdfGenerator {
 		}
 		return translate("label_false", mandant);
 	}
-
 
 	@Nonnull
 	protected final List<String> getAbsenderAdresse() {
