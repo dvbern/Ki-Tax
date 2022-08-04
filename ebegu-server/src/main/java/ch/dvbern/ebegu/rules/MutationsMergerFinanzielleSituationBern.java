5408ed8e46f0548149697975f541c134735591d8
buan <anita.brunner@dvbern.ch>
2022-08-04 13:34:20 +0200

    KIBON-2351: pmd change nested if statements to early exit

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/MutationsMergerFinanzielleSituationBern.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/MutationsMergerFinanzielleSituationBern.java
@@ -39,29 +39,31 @@ public class MutationsMergerFinanzielleSituationBern extends AbstractMutationsMe
 		BigDecimal massgebendesEinkommenVorher = resultVorangehenderAbschnitt.getMassgebendesEinkommen();
 		if (massgebendesEinkommen.compareTo(massgebendesEinkommenVorher) <= 0) {
 			// Massgebendes Einkommen wird kleiner, der Anspruch also höher: Darf nicht rückwirkend sein!
-			if (!inputData.getParent().getGueltigkeit().getGueltigAb().isAfter(mutationsEingansdatum)) {
-				// Der Stichtag fuer diese Erhöhung ist noch nicht erreicht -> Wir arbeiten mit dem alten Wert!
-				// Sobald der Stichtag erreicht ist, müssen wir nichts mehr machen, da dieser Merger *nach* den Monatsabschnitten läuft
-				// Wir haben also nie Abschnitte, die über die Monatsgrenze hinausgehen
-				setFinSitDataFromResultToInput(inputData, resultVorangehenderAbschnitt);
+			if (inputData.getParent().getGueltigkeit().getGueltigAb().isAfter(mutationsEingansdatum)) {
+				return;
+			}
 
-				if (resultVorangehenderAbschnitt.getTsCalculationResultMitPaedagogischerBetreuung() != null) {
-					inputData.getTsInputMitBetreuung().setVerpflegungskostenVerguenstigt(
-						getValueOrZero(
-							resultVorangehenderAbschnitt.getTsCalculationResultMitPaedagogischerBetreuung().getVerpflegungskostenVerguenstigt()));
-				} else {
-					inputData.getTsInputMitBetreuung().setVerpflegungskostenVerguenstigt(BigDecimal.ZERO);
-				}
-				if (resultVorangehenderAbschnitt.getTsCalculationResultOhnePaedagogischerBetreuung() != null) {
-					inputData.getTsInputOhneBetreuung().setVerpflegungskostenVerguenstigt(
-						getValueOrZero(
-							resultVorangehenderAbschnitt.getTsCalculationResultOhnePaedagogischerBetreuung().getVerpflegungskostenVerguenstigt()));
-				} else {
-					inputData.getTsInputOhneBetreuung().setVerpflegungskostenVerguenstigt(BigDecimal.ZERO);
-				}
-				if (massgebendesEinkommen.compareTo(massgebendesEinkommenVorher) < 0) {
-					inputData.addBemerkung(MsgKey.ANSPRUCHSAENDERUNG_MSG, getLocale());
-				}
+			// Der Stichtag fuer diese Erhöhung ist noch nicht erreicht -> Wir arbeiten mit dem alten Wert!
+			// Sobald der Stichtag erreicht ist, müssen wir nichts mehr machen, da dieser Merger *nach* den Monatsabschnitten läuft
+			// Wir haben also nie Abschnitte, die über die Monatsgrenze hinausgehen
+			setFinSitDataFromResultToInput(inputData, resultVorangehenderAbschnitt);
+
+			if (resultVorangehenderAbschnitt.getTsCalculationResultMitPaedagogischerBetreuung() != null) {
+				inputData.getTsInputMitBetreuung().setVerpflegungskostenVerguenstigt(
+					getValueOrZero(
+						resultVorangehenderAbschnitt.getTsCalculationResultMitPaedagogischerBetreuung().getVerpflegungskostenVerguenstigt()));
+			} else {
+				inputData.getTsInputMitBetreuung().setVerpflegungskostenVerguenstigt(BigDecimal.ZERO);
+			}
+			if (resultVorangehenderAbschnitt.getTsCalculationResultOhnePaedagogischerBetreuung() != null) {
+				inputData.getTsInputOhneBetreuung().setVerpflegungskostenVerguenstigt(
+					getValueOrZero(
+						resultVorangehenderAbschnitt.getTsCalculationResultOhnePaedagogischerBetreuung().getVerpflegungskostenVerguenstigt()));
+			} else {
+				inputData.getTsInputOhneBetreuung().setVerpflegungskostenVerguenstigt(BigDecimal.ZERO);
+			}
+			if (massgebendesEinkommen.compareTo(massgebendesEinkommenVorher) < 0) {
+				inputData.addBemerkung(MsgKey.ANSPRUCHSAENDERUNG_MSG, getLocale());
 			}
 		}
 	}
