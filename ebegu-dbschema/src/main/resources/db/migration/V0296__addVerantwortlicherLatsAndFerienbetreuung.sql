0cf2afd907385c9cdb76c707d316ed434e983e0d
buan <anita.brunner@dvbern.ch>
2022-08-15 16:27:41 +0200

    KIBON-2519: Add verantworlicher to audit table

--- ebegu-dbschema/src/main/resources/db/migration/V0296__addVerantwortlicherLatsAndFerienbetreuung.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V0296__addVerantwortlicherLatsAndFerienbetreuung.sql
@@ -19,12 +19,17 @@
 ALTER TABLE lastenausgleich_tagesschule_angaben_gemeinde_container ADD COLUMN IF NOT EXISTS
 	verantwortlicher_id BINARY(16);
 
+ALTER TABLE lastenausgleich_tagesschule_angaben_gemeinde_container_aud  ADD COLUMN IF NOT EXISTS
+	verantwortlicher_id BINARY(16);
+
 ALTER TABLE lastenausgleich_tagesschule_angaben_gemeinde_container
 ADD CONSTRAINT FK_lats_angaben_gemeinde_container_verantwortlicher_id
 	FOREIGN KEY (verantwortlicher_id)
 		REFERENCES benutzer(id);
 
 ALTER TABLE ferienbetreuung_angaben_container ADD COLUMN IF NOT EXISTS
+	verantwortlicher_id BINARY(16);
+ALTER TABLE ferienbetreuung_angaben_container_aud ADD COLUMN IF NOT EXISTS
 	verantwortlicher_id BINARY(16);
 
 ALTER TABLE ferienbetreuung_angaben_container
