09f90b0fbe431751fe826ab21e030e2c1af0ba14
ENJA <janik.endtner@dvbern.ch>
2022-08-22 11:02:25 +0200

    KIBON-2518 add colors

--- ebegu-web/src/app/gemeinde-antraege/lastenausgleich-ts/antrag/tagesschulen-list/tagesschulen-list.component.ts
+++ ebegu-web/src/app/gemeinde-antraege/lastenausgleich-ts/antrag/tagesschulen-list/tagesschulen-list.component.ts
@@ -119,8 +119,8 @@ export class TagesschulenListComponent implements OnInit {
                                 return '';
                             }
                             return isOk ?
-                                '<i class="fa fa-check padding-left-60"></i>' :
-                                '<i class="fa fa-close padding-left-60"></i>';
+                                '<i class="fa fa-check padding-left-60 green"></i>' :
+                                '<i class="fa fa-close padding-left-60 red"></i>';
                         },
                     },
                 ];
