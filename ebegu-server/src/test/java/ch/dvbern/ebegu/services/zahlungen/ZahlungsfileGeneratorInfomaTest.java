c397515a6bd6f4f7572ff34881d50609a7bcc255
ENJA <janik.endtner@dvbern.ch>
2022-09-13 16:44:05 +0200

    KIBON-2654 fix test

--- ebegu-server/src/test/java/ch/dvbern/ebegu/services/zahlungen/ZahlungsfileGeneratorInfomaTest.java
+++ ebegu-server/src/test/java/ch/dvbern/ebegu/services/zahlungen/ZahlungsfileGeneratorInfomaTest.java
@@ -57,11 +57,11 @@ class ZahlungsfileGeneratorInfomaTest {
 
 		final String actualZahlung = InfomaStammdatenZahlung.with(zahlung, 200001, Locale.GERMAN);
 		final String externeId = "21000001.00211_08_31";
-		final String expectedZahlung = "1|2|BGR200001|" + externeId + "|31.08.2022||2|419081||||Kita Bruennen - Zahlungslauf August 2022|1|215|||||||||||||||||-423.25||31.08.2022|||||||||||||||||||||||||||||||010|||||Kita Bruennen, Betreuungsgutscheine August 2022|||\n";
+		final String expectedZahlung = "1|2|BGR200001|" + externeId + "|31.08.2022||2|419081||||Kita Brünnen - Zahlungslauf August 2022|1|215|||||||||||||||||-423.25||31.08.2022|||||||||||||||||||||||||||||||010|||||Kita Brünnen, Betreuungsgutscheine August 2022|||\n";
 		Assertions.assertEquals(expectedZahlung, actualZahlung);
 
 		final String actualFinanzbuchhaltung = InfomaStammdatenFinanzbuchhaltung.with(zahlung, 200001, Locale.GERMAN);
-		final String expectedFinanzbuchhaltung = "1|2|BGR200001|" + externeId + "|31.08.2022||0|3637.010||||Kita Bruennen - Zahlungslauf August 2022|1|215|||2158303||||||||||||||423.25|||||||||||||||||||||||||||||||||RB IBAN|||||Kita Bruennen, Betreuungsgutscheine August 2022|||\n";
+		final String expectedFinanzbuchhaltung = "1|2|BGR200001|" + externeId + "|31.08.2022||0|3637.010||||Kita Brünnen - Zahlungslauf August 2022|1|215|||2158303||||||||||||||423.25|||||||||||||||||||||||||||||||||RB IBAN|||||Kita Brünnen, Betreuungsgutscheine August 2022|||\n";
 		Assertions.assertEquals(expectedFinanzbuchhaltung, actualFinanzbuchhaltung);
 	}
 }
