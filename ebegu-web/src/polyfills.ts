6d2848a6ec0cfaea262c3aae4e3264a0eb40943e
Kafader Simon <simon.kafader@dvbern.ch>
2022-09-14 08:21:13 +0200

    KIBON-2613: update to angular 13

--- ebegu-web/src/polyfills.ts
+++ ebegu-web/src/polyfills.ts
@@ -21,9 +21,6 @@ import 'core-js/es5';
 import 'core-js/es7/array';
 import 'core-js/es7/object';
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 /** IE10 and IE11 requires the following for the Reflect API. */
 
 import 'core-js/es6/reflect';
@@ -33,12 +30,6 @@ import 'core-js/es6/reflect';
 (window as any).__Zone_enable_cross_context_check = true;
 // tslint:disable-next-line:no-import-side-effect
 import 'zone.js'; // Included with Angular CLI.
-
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
