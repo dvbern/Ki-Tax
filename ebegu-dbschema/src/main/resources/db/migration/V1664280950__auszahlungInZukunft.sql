c10e1626fcb074766272713e2d36c49f465309f0
buan <anita.brunner@dvbern.ch>
2022-10-07 15:08:37 +0200

    KIBON-2584: remove schema from sql script

--- ebegu-dbschema/src/main/resources/db/migration/V1664280950__auszahlungInZukunft.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V1664280950__auszahlungInZukunft.sql
@@ -15,7 +15,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-UPDATE ebegu.application_property SET value = 'false' WHERE name = 'ANZAHL_MONATE_AUSZAHLEN_IN_ZUKUNFT' and value = '0';
-UPDATE ebegu.application_property SET value = 'true' WHERE name = 'ANZAHL_MONATE_AUSZAHLEN_IN_ZUKUNFT' and value = '1';
+UPDATE application_property SET value = 'false' WHERE name = 'ANZAHL_MONATE_AUSZAHLEN_IN_ZUKUNFT' and value = '0';
+UPDATE application_property SET value = 'true' WHERE name = 'ANZAHL_MONATE_AUSZAHLEN_IN_ZUKUNFT' and value = '1';
 UPDATE application_property SET name = 'CHECKBOX_AUSZAHLEN_IN_ZUKUNFT' where name = 'ANZAHL_MONATE_AUSZAHLEN_IN_ZUKUNFT';
 
