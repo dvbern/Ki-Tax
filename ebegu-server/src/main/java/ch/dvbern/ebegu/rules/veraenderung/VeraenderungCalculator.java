e20bc65cde02528c36b27229793a0a04c104a3f8
Kafader Simon <simon.kafader@dvbern.ch>
2022-11-03 16:52:30 +0100

    KIBON-2687: implement ignorable calculations

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/veraenderung/VeraenderungCalculator.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/veraenderung/VeraenderungCalculator.java
@@ -3,7 +3,6 @@ package ch.dvbern.ebegu.rules.veraenderung;
 import java.math.BigDecimal;
 import java.util.List;
 
-import javax.annotation.Nonnull;
 import javax.validation.constraints.NotNull;
 
 import ch.dvbern.ebegu.entities.Verfuegung;
@@ -22,4 +21,33 @@ public abstract class VeraenderungCalculator {
 	public abstract BigDecimal calculateVeraenderung(
 		@NotNull List<VerfuegungZeitabschnitt> zeitabschnitte,
 		@NotNull Verfuegung vorgaengerVerfuegung);
+
+	public boolean calculateIgnorable(
+			List<VerfuegungZeitabschnitt> zeitabschnitteAktuell,
+			Verfuegung verfuegung,
+			BigDecimal veraenderung) {
+		if (veraenderung.compareTo(BigDecimal.ZERO) <= 0) {
+			return false;
+		}
+
+		if (zeitabschnitteAktuell.size() != verfuegung.getZeitabschnitte().size()) {
+			return false;
+		}
+
+		for (int i = 0; i < zeitabschnitteAktuell.size(); i++) {
+			VerfuegungZeitabschnitt currentZeitabschnitt = zeitabschnitteAktuell.get(0);
+			VerfuegungZeitabschnitt verfuegungZeitabschnitt = verfuegung.getZeitabschnitte().get(0);
+
+			if (!currentZeitabschnitt.getGueltigkeit().equals(verfuegungZeitabschnitt.getGueltigkeit())) {
+				return false;
+			}
+
+			if (!currentZeitabschnitt.getRelevantBgCalculationResult().differsIgnorableFrom(verfuegungZeitabschnitt.getRelevantBgCalculationResult())) {
+				return false;
+			}
+		}
+
+
+		return true;
+	}
 }
