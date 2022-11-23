3c9fdb0fb8d5de96fe4f21ee9fe0d298949b3360
BUAN <anita.brunner@dvbern.ch>
2022-11-23 10:55:40 +0100

    KIBON-2647: add visitor for AnspruchBeschaeftigungAbhaengigkeitTyp

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/enums/AnspruchBeschaeftigungAbhaengigkeitTyp.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/enums/AnspruchBeschaeftigungAbhaengigkeitTyp.java
@@ -1,12 +1,29 @@
 package ch.dvbern.ebegu.enums;
 
 import ch.dvbern.ebegu.entities.Einstellung;
+import ch.dvbern.ebegu.util.AnspruchBeschaeftigungAbhangigkeitTypVisitor;
+import ch.dvbern.ebegu.util.mandant.MandantVisitor;
 
 public enum AnspruchBeschaeftigungAbhaengigkeitTyp {
 
-	ABHAENGING,
-	UNABHAENGING,
-	MINIMUM;
+	ABHAENGING {
+		@Override
+		public <T> T accept(AnspruchBeschaeftigungAbhangigkeitTypVisitor<T> visitor) {
+			return visitor.visitAbhaengig();
+		}
+	},
+	UNABHAENGING {
+		@Override
+		public <T> T accept(AnspruchBeschaeftigungAbhangigkeitTypVisitor<T> visitor) {
+			return visitor.visitUnabhaengig();
+		}
+	},
+	MINIMUM {
+		@Override
+		public <T> T accept(AnspruchBeschaeftigungAbhangigkeitTypVisitor<T> visitor) {
+			return visitor.visitMinimum();
+		}
+	};
 
 	public boolean isAnspruchUnabhaengig() {
 		return this == UNABHAENGING;
@@ -15,5 +32,7 @@ public enum AnspruchBeschaeftigungAbhaengigkeitTyp {
 	public static AnspruchBeschaeftigungAbhaengigkeitTyp getEnumValue(Einstellung einstellung) {
 		return AnspruchBeschaeftigungAbhaengigkeitTyp.valueOf(einstellung.getValue());
 	}
+
+	public abstract <T> T accept(AnspruchBeschaeftigungAbhangigkeitTypVisitor<T> visitor);
 
 }
