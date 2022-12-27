a15d22f9fc579a3943ea942464a17e39477c806a
ENJA <janik.endtner@dvbern.ch>
2022-12-27 08:42:49 +0100

    KIBON-2751 ferieninsel anmeldungen ignorieren

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/services/BetreuungService.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/services/BetreuungService.java
@@ -243,7 +243,7 @@ public interface BetreuungService {
 	 Setzt die Schulamt-Anmeldung auf SCHULAMT_MUTATION_IGNORIERT
 	 */
 	@Nonnull
-	AnmeldungTagesschule anmeldungMutationIgnorieren(@Valid @Nonnull AnmeldungTagesschule anmeldung);
+	AbstractAnmeldung anmeldungMutationIgnorieren(@Valid @Nonnull AbstractAnmeldung anmeldung);
 
 	@Nonnull
 	AbstractAnmeldung anmeldungSchulamtStornieren(@Valid @Nonnull AbstractAnmeldung anmeldung);
