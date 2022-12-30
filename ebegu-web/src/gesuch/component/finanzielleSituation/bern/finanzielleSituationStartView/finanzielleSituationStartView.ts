96ca6a0d257d208165579cc869277fd878a143d3
ENJA <janik.endtner@dvbern.ch>
2022-12-30 13:16:13 +0100

    direct-to-dev KIBON-2747 demoFeature für finSit rückwirkend anpassen

--- ebegu-web/src/gesuch/component/finanzielleSituation/bern/finanzielleSituationStartView/finanzielleSituationStartView.ts
+++ ebegu-web/src/gesuch/component/finanzielleSituation/bern/finanzielleSituationStartView/finanzielleSituationStartView.ts
@@ -1,16 +1,18 @@
 /*
- * Ki-Tax: System for the management of external childcare subsidies
- * Copyright (C) 2017 City of Bern Switzerland
+ * Copyright (C) 2022 DV Bern AG, Switzerland
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
+ *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 import {IComponentOptions} from 'angular';
@@ -30,7 +32,6 @@ import {TSFinanzielleSituation} from '../../../../../models/TSFinanzielleSituati
 import {TSFinanzModel} from '../../../../../models/TSFinanzModel';
 import {TSGesuch} from '../../../../../models/TSGesuch';
 import {TSLand} from '../../../../../models/types/TSLand';
-import {DateUtil} from '../../../../../utils/DateUtil';
 import {EbeguRestUtil} from '../../../../../utils/EbeguRestUtil';
 import {EbeguUtil} from '../../../../../utils/EbeguUtil';
 import {TSRoleUtil} from '../../../../../utils/TSRoleUtil';
@@ -80,6 +81,7 @@ export class FinanzielleSituationStartViewController extends AbstractFinSitBernV
     private triedSavingWithoutForm: boolean = false;
 
     private isAlwaysShowAuszahlungsdatenActivated: boolean = false;
+    private isMutationIgnorierenActivated: boolean = false;
 
     public constructor(
         gesuchModelManager: GesuchModelManager,
@@ -127,6 +129,8 @@ export class FinanzielleSituationStartViewController extends AbstractFinSitBernV
 
         demoFeatureRS.isDemoFeatureAllowed(TSDemoFeature.ALLWAYS_SHOW_ZAHLUNGSDATEN_ON_FINSIT_BERN)
             .then(isAllowed => this.isAlwaysShowAuszahlungsdatenActivated = isAllowed);
+        demoFeatureRS.isDemoFeatureAllowed(TSDemoFeature.VERAENDERUNG_BEI_MUTATION)
+            .then(isAllowed => this.isMutationIgnorierenActivated = isAllowed);
     }
 
     public showSteuerveranlagung(): boolean {
@@ -444,6 +448,9 @@ export class FinanzielleSituationStartViewController extends AbstractFinSitBernV
 
     public showFinanzielleSituationRueckwirkendAnpassen() {
         if (!this.gesuchModelManager.getGesuch().isMutation()) {
+            return false;
+        }
+        if (!this.isMutationIgnorierenActivated) {
             return false;
         }
 
