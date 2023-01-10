b207acf480f8e866587b75e99f9fa0d0c80085b8
ENJA <janik.endtner@dvbern.ch>
2023-01-10 17:26:26 +0100

    KIBON-2803 dv-basisjahr component umbenennen und label als input. überall ersetzen

--- ebegu-web/src/gesuch/component/dv-eingabe-hint/dv-eingabe-hint.component.ts
+++ ebegu-web/src/gesuch/component/dv-eingabe-hint/dv-eingabe-hint.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 DV Bern AG, Switzerland
+ * Copyright (C) 2023 DV Bern AG, Switzerland
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -8,36 +8,32 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import {
-    ChangeDetectionStrategy,
-    Component,
-    Input
-} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {EbeguUtil} from '../../../utils/EbeguUtil';
 
 /**
  * Zeigt einen Hinweis, wie hoch die Eingabe im Basisjahr war.
  */
 @Component({
-    selector: 'dv-eingaben-basisjahr',
-    templateUrl: './dv-eingabe-basisjahr.component.html',
-    styleUrls: ['./dv-eingabe-basisjahr.component.less'],
+    selector: 'dv-eingabe-hint',
+    templateUrl: './dv-eingabe-hint.component.html',
+    styleUrls: ['./dv-eingabe-hint.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DvEingabeBasisjahrComponent {
+export class DvEingabeHintComponent {
     @Input() public neuerWert: number;
-    @Input() public wertBasisjahr: number;
-    @Input() public basisjahr: number;
+    @Input() public vergleichswert: number;
+    @Input() public label: string;
 
     public isVisible(): boolean {
-        return EbeguUtil.isNotNullOrUndefined(this.wertBasisjahr) && (this.wertBasisjahr > 0 ||
-            (this.wertBasisjahr !== this.neuerWert && this.neuerWert > 0));
+        return EbeguUtil.isNotNullOrUndefined(this.vergleichswert) && (this.vergleichswert > 0 ||
+            (this.vergleichswert !== this.neuerWert && this.neuerWert > 0));
     }
 }
