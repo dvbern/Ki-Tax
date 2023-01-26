952e0340daa554c596f468ec3eae94d0c8c8bb65
Michael Spreng <michael.spreng@dvbern.ch>
2023-01-26 16:08:55 +0100

    KIBON-2765_Familiensituationbeendet_AbschnittRule Test

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/FamiliensituationBeendetAbschnittRule.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/FamiliensituationBeendetAbschnittRule.java
@@ -5,6 +5,7 @@ import java.time.temporal.TemporalAdjusters;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 import javax.annotation.Nonnull;
 
@@ -15,21 +16,17 @@ import ch.dvbern.ebegu.entities.VerfuegungZeitabschnitt;
 import ch.dvbern.ebegu.enums.BetreuungsangebotTyp;
 import ch.dvbern.ebegu.types.DateRange;
 import com.google.common.collect.ImmutableList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.KITA;
 import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.TAGESFAMILIEN;
 import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.TAGESSCHULE;
 
 public class FamiliensituationBeendetAbschnittRule extends AbstractAbschnittRule {
-	private static final Logger LOG = LoggerFactory.getLogger(FamiliensituationBeendetAbschnittRule.class);
 	public static final int ZERO = 0;
 
 	protected FamiliensituationBeendetAbschnittRule(
 		@Nonnull DateRange validityPeriod,
 		@Nonnull Locale locale) {
-		//FIXME: was ist das hier
 		super(RuleKey.FAMILIENSITUATION, RuleType.REDUKTIONSREGEL, RuleValidity.ASIV, validityPeriod, locale);
 	}
 
@@ -38,10 +35,13 @@ public class FamiliensituationBeendetAbschnittRule extends AbstractAbschnittRule
 	List<VerfuegungZeitabschnitt> createVerfuegungsZeitabschnitte(@Nonnull AbstractPlatz platz) {
 		Gesuch gesuch = platz.extractGesuch();
 		Familiensituation familiensituation = platz.extractGesuch().extractFamiliensituation();
-		LocalDate familiensituationAenderungPer = familiensituation.getAenderungPer();
 
-		LocalDate firstDayOfNextMonth = familiensituationAenderungPer.with(TemporalAdjusters.firstDayOfNextMonth());
+		LocalDate familiensituationAenderungPer = Objects.requireNonNull(familiensituation).getAenderungPer();
+		if (null == familiensituationAenderungPer) {
+			return new LinkedList<>();
+		}
 		LocalDate gueltigBis = gesuch.getGesuchsperiode().getGueltigkeit().getGueltigBis();
+		LocalDate firstDayOfNextMonth = familiensituationAenderungPer.with(TemporalAdjusters.firstDayOfNextMonth());
 
 		VerfuegungZeitabschnitt neuerZeitabschnittOhnePartner =
 			createZeitabschnittWithinValidityPeriodOfRule(new DateRange(firstDayOfNextMonth, gueltigBis));
