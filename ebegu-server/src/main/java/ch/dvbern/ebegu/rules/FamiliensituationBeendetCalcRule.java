affc573793c54072e363b080c615bb9d39131507
Michael Spreng <michael.spreng@dvbern.ch>
2023-02-15 13:23:37 +0100

    KIBON-2765 FamiliensituationBeendetCalcRule Log / Nullcheck

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/FamiliensituationBeendetCalcRule.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/FamiliensituationBeendetCalcRule.java
@@ -12,15 +12,12 @@ import ch.dvbern.ebegu.enums.BetreuungsangebotTyp;
 import ch.dvbern.ebegu.enums.MsgKey;
 import ch.dvbern.ebegu.types.DateRange;
 import com.google.common.collect.ImmutableList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.KITA;
 import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.TAGESFAMILIEN;
 import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.TAGESSCHULE;
 
 public class FamiliensituationBeendetCalcRule extends AbstractCalcRule {
-	private static final Logger LOG = LoggerFactory.getLogger(FamiliensituationBeendetCalcRule.class);
 	public static final int ZERO = 0;
 
 	protected FamiliensituationBeendetCalcRule(
@@ -39,7 +36,7 @@ public class FamiliensituationBeendetCalcRule extends AbstractCalcRule {
 		}
 		Gesuch gesuch = platz.extractGesuch();
 		String gesuchstellerPartner =
-			(gesuch.getGesuchsteller1() != null) ? gesuch.getGesuchsteller2().extractFullName() : "";
+			(gesuch.getGesuchsteller2() != null) ? gesuch.getGesuchsteller2().extractFullName() : "";
 		inputData.setAnspruchspensumProzent(ZERO);
 		inputData.setAnspruchspensumRest(ZERO);
 		inputData.addBemerkung(MsgKey.PARTNER_NOT_IDENTISCH_MIT_VORGESUCH, getLocale(), gesuchstellerPartner);
