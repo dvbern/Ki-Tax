605c56c4ecfcf361e036242a3e5296eeb73c3043
ENJA <janik.endtner@dvbern.ch>
2023-02-21 10:13:21 +0100

    KIBON-2738 review

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/FamilienabzugAbschnittRule.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/FamilienabzugAbschnittRule.java
@@ -1,16 +1,18 @@
 /*
- * Ki-Tax: System for the management of external childcare subsidies
- * Copyright (C) 2017 City of Bern Switzerland
+ * Copyright (C) 2023 DV Bern AG, Switzerland
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
+ *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package ch.dvbern.ebegu.rules;
@@ -274,6 +276,9 @@ public class FamilienabzugAbschnittRule extends AbstractAbschnittRule {
 	}
 
 	private int calculateFKJVAnzahlPersonen(double beruecksichtigungAbzug, boolean isKinderAbzugTypeVersion2) {
+		// in der FKJV Periode 22/23 hatten wir einen Fehler, der die Kinder immer mitzählte, auch wenn es für ein Kind keinen Kinderabzug gab.
+		// dies wird auf die Periode 23/24 geändert. Kinder ohne Kinderabzug zählen nicht zur Familiengrösse und Kinder für die der
+		// halbe oder der ganze Pauschalabzug abgezogen werden kann, zählen ganz zur Familiengrösse
 		if (!isKinderAbzugTypeVersion2) {
 			return 1;
 		}
