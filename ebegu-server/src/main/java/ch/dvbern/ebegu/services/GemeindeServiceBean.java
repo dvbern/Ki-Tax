18bcc217aaf925e9ca852136f3b021ce6f5373bf
ENJA <janik.endtner@dvbern.ch>
2023-04-28 10:45:01 +0200

    direct-to-dev KIBON-2724 events für appenzeller gemeinden temporär nicht exportieren

--- ebegu-server/src/main/java/ch/dvbern/ebegu/services/GemeindeServiceBean.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/services/GemeindeServiceBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 DV Bern AG, Switzerland
+ * Copyright (C) 2023 DV Bern AG, Switzerland
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -8,11 +8,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package ch.dvbern.ebegu.services;
@@ -71,6 +71,7 @@ import ch.dvbern.ebegu.outbox.ExportedEvent;
 import ch.dvbern.ebegu.outbox.gemeinde.GemeindeEventConverter;
 import ch.dvbern.ebegu.persistence.CriteriaQueryHelper;
 import ch.dvbern.ebegu.util.Constants;
+import ch.dvbern.ebegu.util.mandant.MandantIdentifier;
 import ch.dvbern.lib.cdipersistence.Persistence;
 
 import static java.util.Objects.requireNonNull;
@@ -735,6 +736,8 @@ public class GemeindeServiceBean extends AbstractBaseService implements Gemeinde
 
 	@Override
 	public void fireGemeindeChangedEvent(@Nonnull Gemeinde gemeinde) {
+		if (gemeinde.getMandant().getMandantIdentifier() != MandantIdentifier.APPENZELL_AUSSERRHODEN) {
 			event.fire(gemeindeEventConverter.of(gemeinde));
+		}
 	}
 }
