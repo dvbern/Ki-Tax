b60055eafdfd01c6e36ab60af2b6709007e779a4
buan <anita.brunner@dvbern.ch>
2023-05-23 13:44:27 +0200

    KIBON-2975: Move MergeZeitabschnitte Methode to SuperClass and Extend it from AbschlussRules

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/AbstractEbeguRule.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/AbstractEbeguRule.java
@@ -20,15 +20,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
 
 import javax.annotation.Nonnull;
 import javax.validation.Valid;
@@ -49,7 +45,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  * This defines a Rule that has a unique Name given by RuleKey. The Rule is valid for a specified validityPeriod and
  * is of a given type
  */
-public abstract class AbstractEbeguRule implements Rule {
+public abstract class AbstractEbeguRule extends AbstractRule implements Rule  {
 
 	private RuleValidity ruleValidity;
 
@@ -321,65 +317,6 @@ public abstract class AbstractEbeguRule implements Rule {
 		alles.addAll(bestehendeEntities);
 		alles.addAll(neueEntities);
 		return mergeZeitabschnitte(alles);
-	}
-
-	/**
-	 * Erstellt aus der übergebenen Liste von VerfuegungsZeitabschnitten eine neue Liste, die keine Überschneidungen mehr
-	 * enthält. Überschneiden sich zwei Entitäten in der Ursprungsliste, so werden daraus drei Zeiträume erstellt:
-	 * <pre>
-	 * |------------------------|
-	 * 40
-	 * 	           |-------------------------------------|
-	 * 			   60
-	 * ergibt:
-	 * |-----------|------------|------------------------|
-	 * 40          100                60
-	 * </pre>
-	 */
-	@Nonnull
-	protected List<VerfuegungZeitabschnitt> mergeZeitabschnitte(@Nonnull List<VerfuegungZeitabschnitt> entitiesToMerge) {
-		List<VerfuegungZeitabschnitt> result = new ArrayList<>();
-		Set<LocalDate> setOfPotentialZeitraumGrenzen = createSetOfPotentialZeitraumGrenzen(entitiesToMerge);
-		if (setOfPotentialZeitraumGrenzen.isEmpty()) {
-			return result;
-		}
-		Iterator<LocalDate> iterator = setOfPotentialZeitraumGrenzen.iterator();
-		LocalDate datumVon = iterator.next();
-		while (iterator.hasNext()) {
-			LocalDate datumBis = iterator.next().minusDays(1);   //wir haben bei den Bis Daten jeweils einen Tag hinzugefuegt
-			VerfuegungZeitabschnitt mergedZeitabschnitt = new VerfuegungZeitabschnitt(new DateRange(datumVon, datumBis));
-			// Alle Zeitabschnitte suchen, die mit  diesem Range ueberlappen
-			boolean foundOverlapping = false;
-			for (VerfuegungZeitabschnitt verfuegungZeitabschnitt : entitiesToMerge) {
-				Optional<DateRange> optionalOverlap = verfuegungZeitabschnitt.getGueltigkeit().getOverlap(mergedZeitabschnitt.getGueltigkeit());
-				if (optionalOverlap.isPresent()) {
-					mergedZeitabschnitt.add(verfuegungZeitabschnitt); //Zeitabschnitt hinzumergen
-					foundOverlapping = true;
-				}
-			}
-			if (foundOverlapping) {
-				result.add(mergedZeitabschnitt);
-			}
-			datumVon = datumBis.plusDays(1); //naechstes vondatum
-		}
-		return result;
-	}
-
-	/**
-	 * Sammelt alle Start und Enddaten aus der Liste der Zeitabschnitte zusammen.
-	 * Dabei ist es so, dass das bis Datum der Zeitabschnitte inklusiv - inklusiv ist. Wir moechten aber die Daten jeweils
-	 * inklusv - exklusive enddatum. Daher wird zum endtaum jeweils ein Tag hinzugezaehlt
-	 *
-	 * @param entitiesUnmerged liste der Abschnitte
-	 * @return Liste aller Dates die potentiell als Zeitraumgrenze dienen werden
-	 */
-	private Set<LocalDate> createSetOfPotentialZeitraumGrenzen(@Nonnull List<VerfuegungZeitabschnitt> entitiesUnmerged) {
-		Set<LocalDate> setOfDates = new TreeSet<>();
-		for (VerfuegungZeitabschnitt verfuegungZeitabschnitt : entitiesUnmerged) {
-			setOfDates.add(verfuegungZeitabschnitt.getGueltigkeit().getGueltigAb());
-			setOfDates.add(verfuegungZeitabschnitt.getGueltigkeit().getGueltigBis().plusDays(1));
-		}
-		return setOfDates;
 	}
 
 	@Override
