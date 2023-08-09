154e775044c0ac2a8384f7b28e8db7d09f422230
Michael Spreng <michael.spreng@dvbern.ch>
2023-08-09 10:35:28 +0200

    KIBON-3044 Anpassung Zahlungen Statistik

--- ebegu-shared/src/main/java/ch/dvbern/ebegu/enums/reporting/MergeFieldZahlungen.java
+++ ebegu-shared/src/main/java/ch/dvbern/ebegu/enums/reporting/MergeFieldZahlungen.java
@@ -16,55 +16,52 @@
  */
 package ch.dvbern.ebegu.enums.reporting;
 
-import javax.annotation.Nonnull;
-
 import ch.dvbern.oss.lib.excelmerger.mergefields.MergeField;
 import ch.dvbern.oss.lib.excelmerger.mergefields.MergeFieldProvider;
 import ch.dvbern.oss.lib.excelmerger.mergefields.RepeatRowMergeField;
 import ch.dvbern.oss.lib.excelmerger.mergefields.SimpleMergeField;
 
-import static ch.dvbern.oss.lib.excelmerger.converters.StandardConverters.BIGDECIMAL_CONVERTER;
-import static ch.dvbern.oss.lib.excelmerger.converters.StandardConverters.BOOLEAN_X_CONVERTER;
-import static ch.dvbern.oss.lib.excelmerger.converters.StandardConverters.DATETIME_CONVERTER;
-import static ch.dvbern.oss.lib.excelmerger.converters.StandardConverters.DATE_CONVERTER;
-import static ch.dvbern.oss.lib.excelmerger.converters.StandardConverters.STRING_CONVERTER;
+import javax.annotation.Nonnull;
+
+import static ch.dvbern.oss.lib.excelmerger.converters.StandardConverters.*;
 
 public enum MergeFieldZahlungen implements MergeFieldProvider {
 
-	periodeParam(new SimpleMergeField<>("periodeParam", STRING_CONVERTER)),
-	gemeindeParam(new SimpleMergeField<>("gemeindeParam", STRING_CONVERTER)),
-	institutionParam(new SimpleMergeField<>("institutionParam", STRING_CONVERTER)),
-	timestampParam(new SimpleMergeField<>("timestampParam", DATETIME_CONVERTER)),
+    periodeParam(new SimpleMergeField<>("periodeParam", STRING_CONVERTER)),
+    gemeindeParam(new SimpleMergeField<>("gemeindeParam", STRING_CONVERTER)),
+    institutionParam(new SimpleMergeField<>("institutionParam", STRING_CONVERTER)),
+    timestampParam(new SimpleMergeField<>("timestampParam", DATETIME_CONVERTER)),
 
 
-	repeatRow(new RepeatRowMergeField("repeatRow")),
-	zahlungslaufTitle(new SimpleMergeField<>("zahlungslaufTitle", STRING_CONVERTER)),
-	gemeinde(new SimpleMergeField<>("gemeinde", STRING_CONVERTER)),
-	institution(new SimpleMergeField<>("institution", STRING_CONVERTER)),
-	timestampZahlungslauf(new SimpleMergeField<>("timestampZahlungslauf", DATETIME_CONVERTER)),
-	kindVorname(new SimpleMergeField<>("kindVorname", STRING_CONVERTER)),
-	kindNachname(new SimpleMergeField<>("kindNachname", STRING_CONVERTER)),
-	referenznummer(new SimpleMergeField<>("referenznummer", STRING_CONVERTER)),
-	zeitabschnittVon(new SimpleMergeField<>("zeitabschnittVon", DATE_CONVERTER)),
-	zeitabschnittBis(new SimpleMergeField<>("zeitabschnittBis", DATE_CONVERTER)),
-	bgPensum(new SimpleMergeField<>("bgPensum", BIGDECIMAL_CONVERTER)),
-	betrag(new SimpleMergeField<>("betrag", BIGDECIMAL_CONVERTER)),
-	korrektur(new SimpleMergeField<>("korrektur", BOOLEAN_X_CONVERTER)),
-	ignorieren(new SimpleMergeField<>("ignorieren", BOOLEAN_X_CONVERTER)),
-	ibanEltern(new SimpleMergeField<>("ibanEltern", STRING_CONVERTER)),
-	kontoEltern(new SimpleMergeField<>("kontoEltern", STRING_CONVERTER));
+    repeatRow(new RepeatRowMergeField("repeatRow")),
+    zahlungslaufTitle(new SimpleMergeField<>("zahlungslaufTitle", STRING_CONVERTER)),
+    faelligkeitsDatum(new SimpleMergeField<>("faellikeitsDatum", DATE_CONVERTER)),
+    gemeinde(new SimpleMergeField<>("gemeinde", STRING_CONVERTER)),
+    institution(new SimpleMergeField<>("institution", STRING_CONVERTER)),
+    timestampZahlungslauf(new SimpleMergeField<>("timestampZahlungslauf", DATETIME_CONVERTER)),
+    kindVorname(new SimpleMergeField<>("kindVorname", STRING_CONVERTER)),
+    kindNachname(new SimpleMergeField<>("kindNachname", STRING_CONVERTER)),
+    referenznummer(new SimpleMergeField<>("referenznummer", STRING_CONVERTER)),
+    zeitabschnittVon(new SimpleMergeField<>("zeitabschnittVon", DATE_CONVERTER)),
+    zeitabschnittBis(new SimpleMergeField<>("zeitabschnittBis", DATE_CONVERTER)),
+    bgPensum(new SimpleMergeField<>("bgPensum", BIGDECIMAL_CONVERTER)),
+    betrag(new SimpleMergeField<>("betrag", BIGDECIMAL_CONVERTER)),
+    korrektur(new SimpleMergeField<>("korrektur", BOOLEAN_X_CONVERTER)),
+    ignorieren(new SimpleMergeField<>("ignorieren", BOOLEAN_X_CONVERTER)),
+    ibanEltern(new SimpleMergeField<>("ibanEltern", STRING_CONVERTER)),
+    kontoEltern(new SimpleMergeField<>("kontoEltern", STRING_CONVERTER));
 
-	@Nonnull
-	private final MergeField<?> mergeField;
+    @Nonnull
+    private final MergeField<?> mergeField;
 
-	<V> MergeFieldZahlungen(@Nonnull MergeField<V> mergeField) {
-		this.mergeField = mergeField;
-	}
+    <V> MergeFieldZahlungen(@Nonnull MergeField<V> mergeField) {
+        this.mergeField = mergeField;
+    }
 
-	@Override
-	@Nonnull
-	public <V> MergeField<V> getMergeField() {
-		//noinspection unchecked
-		return (MergeField<V>) mergeField;
-	}
+    @Override
+    @Nonnull
+    public <V> MergeField<V> getMergeField() {
+        //noinspection unchecked
+        return (MergeField<V>) mergeField;
+    }
 }
