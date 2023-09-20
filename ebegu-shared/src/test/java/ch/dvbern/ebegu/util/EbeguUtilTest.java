e3ecf56bfb890ba016d6f96b68bfb969841a97ea
nefr <frederic.nell@dvbern.ch>
2023-09-20 13:02:11 +0200

    KIBON-3030: Erlaeuterung mit 0 Anspruch sollte beim Appenzell trotzdem mitgeschickt werden

--- ebegu-shared/src/test/java/ch/dvbern/ebegu/util/EbeguUtilTest.java
+++ ebegu-shared/src/test/java/ch/dvbern/ebegu/util/EbeguUtilTest.java
@@ -16,10 +16,21 @@
 package ch.dvbern.ebegu.util;
 
 import java.time.LocalDate;
+import java.util.Set;
+import java.util.TreeSet;
 
+import ch.dvbern.ebegu.entities.Betreuung;
+import ch.dvbern.ebegu.entities.Dossier;
+import ch.dvbern.ebegu.entities.Fall;
 import ch.dvbern.ebegu.entities.Familiensituation;
 import ch.dvbern.ebegu.entities.FamiliensituationContainer;
+import ch.dvbern.ebegu.entities.Gesuch;
+import ch.dvbern.ebegu.entities.KindContainer;
+import ch.dvbern.ebegu.entities.Mandant;
+import ch.dvbern.ebegu.enums.AntragStatus;
+import ch.dvbern.ebegu.enums.Betreuungsstatus;
 import ch.dvbern.ebegu.enums.EnumFamilienstatus;
+import ch.dvbern.ebegu.util.mandant.MandantIdentifier;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -103,5 +114,38 @@ public class EbeguUtilTest {
 		String filename = EbeguUtil.toFilename("Kita Beundenweg/Crèche Oeuches.pdf");
 		Assert.assertNotNull(filename);
 		Assert.assertEquals("Kita_Beundenweg_Crèche_Oeuches.pdf", filename);
+	}
+
+	@Test
+	public void isErlaeuterungenZurVerfuegungMit0AnspruchRequiredTest() {
+		Gesuch gesuch = prepareGesuchForErlaeuterungenZurVerguegungTests(MandantIdentifier.BERN);
+		// 0 Anspruch sollte keine Erlaeterung sein:
+		Assert.assertFalse(EbeguUtil.isErlaeuterungenZurVerfuegungRequired(gesuch));
+	}
+	@Test
+	public void isErlaeuterungenZurVerfuegungMit0AnspruchRequiredFuerAppenzellTest() {
+		Gesuch gesuch = prepareGesuchForErlaeuterungenZurVerguegungTests(MandantIdentifier.APPENZELL_AUSSERRHODEN);
+		// 0 Anspruch aber beim Appenzell ist einer Ausnahme so es muss immer sein:
+		Assert.assertTrue(EbeguUtil.isErlaeuterungenZurVerfuegungRequired(gesuch));
+	}
+
+	private Gesuch prepareGesuchForErlaeuterungenZurVerguegungTests(MandantIdentifier mandantIdentifier) {
+		Mandant mandant = new Mandant();
+		mandant.setMandantIdentifier(mandantIdentifier);
+		Fall fall = new Fall();
+		fall.setMandant(mandant);
+		Dossier dossier = new Dossier();
+		dossier.setFall(fall);
+		Gesuch gesuch = new Gesuch();
+		gesuch.setDossier(dossier);
+		gesuch.setStatus(AntragStatus.VERFUEGT);
+		Betreuung betreuung = new Betreuung();
+		betreuung.setBetreuungsstatus(Betreuungsstatus.VERFUEGT);
+		Set<Betreuung> betreuungen = new TreeSet<>();
+		betreuungen.add(betreuung);
+		KindContainer kindContainer = new KindContainer();
+		kindContainer.setBetreuungen(betreuungen);
+		gesuch.addKindContainer(kindContainer);
+		return gesuch;
 	}
 }
