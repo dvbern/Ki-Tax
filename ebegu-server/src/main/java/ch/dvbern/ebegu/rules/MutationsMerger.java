550793a76f68fa02a6b2bf46dca0701664fc86b5
buan <anita.brunner@dvbern.ch>
2023-12-11 11:29:57 +0100

    Revert "KIBON-3205: Move handleAnpassungAnspruch Methode from MutationsMergerFinSit to MutationsMerger"
    This reverts commit d08e600c

--- ebegu-server/src/main/java/ch/dvbern/ebegu/rules/MutationsMerger.java
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rules/MutationsMerger.java
@@ -15,8 +15,21 @@
 
 package ch.dvbern.ebegu.rules;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import ch.dvbern.ebegu.dto.BGCalculationInput;
-import ch.dvbern.ebegu.entities.*;
+import ch.dvbern.ebegu.entities.AbstractPlatz;
+import ch.dvbern.ebegu.entities.BGCalculationResult;
+import ch.dvbern.ebegu.entities.Betreuung;
+import ch.dvbern.ebegu.entities.Verfuegung;
+import ch.dvbern.ebegu.entities.VerfuegungZeitabschnitt;
 import ch.dvbern.ebegu.enums.BetreuungsangebotTyp;
 import ch.dvbern.ebegu.enums.MsgKey;
 import ch.dvbern.ebegu.enums.ZahlungslaufTyp;
@@ -25,15 +38,9 @@ import com.google.common.collect.ImmutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Objects;
-
-import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.*;
+import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.KITA;
+import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.TAGESFAMILIEN;
+import static ch.dvbern.ebegu.enums.BetreuungsangebotTyp.TAGESSCHULE;
 
 /**
  * Sonderregel das Ergenis der aktuellen Berechnung mit der Vorhergehenden merged.
@@ -158,7 +165,6 @@ public final class MutationsMerger extends AbstractAbschlussRule {
 		handleFinanzielleSituation(inputAktuel, resultVorgaenger, platz, mutationsEingansdatum);
 		handleAnpassungErweiterteBeduerfnisse(inputAktuel, resultVorgaenger, mutationsEingansdatum);
 		handleEinreichfrist(inputAktuel, resultVorgaenger, platz, mutationsEingansdatum);
-		handleAnpassungAnspruch(inputAktuel, resultVorgaenger, mutationsEingansdatum);
 	}
 
 	private void handleFinanzielleSituation(
@@ -238,48 +244,5 @@ public final class MutationsMerger extends AbstractAbschlussRule {
 
 		LOG.error("Vorgaengerzeitabschnitt fuer Mutation konnte nicht gefunden werden {}", stichtag);
 		return null;
-	}
-
-	private void handleAnpassungAnspruch(
-		@Nonnull BGCalculationInput inputData,
-		@Nullable BGCalculationResult resultVorangehenderAbschnitt,
-		@Nonnull LocalDate mutationsEingansdatum
-	) {
-		DateRange gueltigkeit = inputData.getParent().getGueltigkeit();
-
-		//Meldung rechtzeitig: In diesem Fall wird der Anspruch zusammen mit dem Ereigniseintritt des Arbeitspensums angepasst. -> keine Aenderungen
-		if (!isMeldungZuSpaet(gueltigkeit, mutationsEingansdatum)) {
-			return;
-		}
-
-		final int anspruchberechtigtesPensum = inputData.getAnspruchspensumProzent();
-		final int anspruchAufVorgaengerVerfuegung = resultVorangehenderAbschnitt == null
-			? 0
-			: resultVorangehenderAbschnitt.getAnspruchspensumProzent();
-
-		if (anspruchberechtigtesPensum > anspruchAufVorgaengerVerfuegung) {
-			//Anspruch wird erhöht
-			//Meldung nicht Rechtzeitig: Der Anspruch kann sich erst auf den Folgemonat des Eingangsdatum erhöhen
-			inputData.setAnspruchspensumProzent(anspruchAufVorgaengerVerfuegung);
-			inputData.setRueckwirkendReduziertesPensumRest(anspruchberechtigtesPensum - anspruchAufVorgaengerVerfuegung);
-			//Wenn der Anspruch auf dem Vorgänger 0 ist, weil das Erstgesuch zu spät eingereicht wurde
-			//soll die Bemerkung bezüglich der Erhöhung nicht angezeigt werden, da es sich um keine Erhöhung handelt
-			if(!isAnspruchZeroBecauseVorgaengerZuSpaet(resultVorangehenderAbschnitt)) {
-				inputData.addBemerkung(MsgKey.ANSPRUCHSAENDERUNG_MSG, locale);
-			}
-
-		} else if (anspruchberechtigtesPensum < anspruchAufVorgaengerVerfuegung) {
-			//Meldung nicht Rechtzeitig: Reduktionen des Anspruchs sind auch rückwirkend erlaubt -> keine Aenderungen
-			inputData.addBemerkung(MsgKey.REDUCKTION_RUECKWIRKEND_MSG, locale);
-		}
-	}
-
-	private boolean isAnspruchZeroBecauseVorgaengerZuSpaet(BGCalculationResult resultVorangehenderAbschnitt) {
-		if(resultVorangehenderAbschnitt == null) {
-			return false;
-		}
-
-		boolean anspruchsPensumZero = resultVorangehenderAbschnitt.getAnspruchspensumProzent() == 0;
-		return anspruchsPensumZero && resultVorangehenderAbschnitt.isZuSpaetEingereicht();
 	}
 }
