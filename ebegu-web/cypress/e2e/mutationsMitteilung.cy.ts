2820e2163ac01a4e464fee13ef018774104c67b1
Kafader Simon <simon.kafader@dvbern.ch>
2023-12-20 16:02:41 +0100

    KIBON-3218: adapt test after merge

--- ebegu-web/cypress/e2e/mutationsMitteilung.cy.ts
+++ ebegu-web/cypress/e2e/mutationsMitteilung.cy.ts
@@ -7,8 +7,9 @@ describe('Kibon - generate Testfälle [Gemeinde Sachbearbeiter]', () => {
     const monatlicheKosten = '1000';
     const startdatum = '01.04.2023';
     const enddatum = '30.06.2023';
+    let fallnummer: string;
 
-    it('should create a prefilled new Testfall Antrag and mutationsmeldung', () => {
+    before(() => {
         cy.login(adminUser);
         cy.visit('/#/faelle');
         cy.getByData('page-title').contains('Alle Fälle');
@@ -20,12 +21,21 @@ describe('Kibon - generate Testfälle [Gemeinde Sachbearbeiter]', () => {
         cy.getByData('periode.2022/23').click();
         cy.getByData('creationType.verfuegt').find('label').click();
         cy.getByData('testfall-1').click();
-        cy.get('[data-test="dialog-link"]', { timeout: 20000 }).click();
+        cy.intercept('GET', '**/dossier/id//**').as('opengesuch');
+        cy.get('[data-test="dialog-link"]', {timeout: Cypress.config('defaultCommandTimeout') * 10}).click();
+        cy.wait('@opengesuch');
+        cy.getByData('fallnummer').invoke('text').then(value => {
+            fallnummer = value;
+            cy.getByData('fallnummer').should('contain.text', fallnummer);
+        });
+    });
+
+    it('should create a prefilled new Testfall Antrag and mutationsmeldung', () => {
         cy.login(userSB);
         cy.visit('/#/faelle');
-        cy.getByData('antrag-entry#0').click();
+        cy.getByData('antrag-entry#' + fallnummer).click();
         cy.getByData('sidenav.BETREUUNG').click();
-        cy.getByData('betreuung#0').click();
+        cy.getByData('container.betreuung#0').click();
         cy.getByData('mutationsmeldung-erstellen').click();
         cy.getByData('betreuungspensum-0').clear().type(monatlichesPensum);
         cy.getByData('monatliche-betreuungskosten#0').clear().type(monatlicheKosten);
@@ -40,7 +50,7 @@ describe('Kibon - generate Testfälle [Gemeinde Sachbearbeiter]', () => {
     it('should accept the Mutationsmeldung', () => {
         cy.login(adminUser);
         cy.visit('/#/faelle');
-        cy.getByData('antrag-entry#0').click();
+        cy.getByData('antrag-entry#' + fallnummer).click();
         cy.getByData('toolbar-mitteilungen').click();
         cy.getByData('container.mitteilung#0').click();
         cy.getByData('container.mitteilung#0', 'navigation-button').click();
@@ -52,9 +62,9 @@ describe('Kibon - generate Testfälle [Gemeinde Sachbearbeiter]', () => {
     it('should check if the Mutationsmeldung was accepted', () => {
         cy.login(adminUser);
         cy.visit('/#/faelle');
-        cy.getByData('antrag-entry#0').click();
+        cy.getByData('antrag-entry#' + fallnummer).first().click();
         cy.getByData('sidenav.BETREUUNG').click();
-        cy.getByData('betreuung#1').click();
+        cy.getByData('container.betreuung#1').click();
         cy.getByData('betreuungspensum-0').should('have.value', monatlichesPensum);
         cy.getByData('monatliche-betreuungskosten#0').should('have.value', monatlicheKosten);
         cy.getByData('betreuung-datum-ab#0').find("input").should('have.value', startdatum);
