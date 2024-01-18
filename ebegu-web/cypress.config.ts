c83a6053fd8e64af7a01297a9c23e38bb4aa1b51
Simon Kafader <simon.kafader@dvbern.ch>
2024-01-18 15:51:08 +0000

    ci(KIBON-3283): add cypress reporter and video generation

--- ebegu-web/cypress.config.ts
+++ ebegu-web/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 import * as dvTasks from './cypress/support/tasks';
+import * as fs from 'fs';
 
 const baseUrl = process.env.baseURL ?? 'http://local-be.kibon.ch:4200/';
 
@@ -13,6 +14,20 @@ export default defineConfig({
             on('task', {
                 ...dvTasks,
             });
+            on('after:spec',
+                (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+                  if (results && results.video) {
+                    // Do we have failures for any retry attempts?
+                    const failures = results.tests.some((test) =>
+                      test.attempts.some((attempt) => attempt.state === 'failed')
+                    )
+                    if (!failures) {
+                      // delete the video if the spec passed and no tests retried
+                      fs.unlinkSync(results.video)
+                    }
+                  }
+                }
+              )
         },
         projectId: 'ebegu-web',
         defaultCommandTimeout: 8000,
@@ -22,7 +37,12 @@ export default defineConfig({
         baseUrl,
         fixturesFolder: './cypress/fixtures',
         experimentalRunAllSpecs: true,
-        requestTimeout: 10000
+        requestTimeout: 10000,
+        reporter: 'junit',
+        reporterOptions: {
+            mochaFile: './cypress/results/test-result-[hash].xml'
+        },
+        video: true
     },
     scrollBehavior: 'nearest',
 });
