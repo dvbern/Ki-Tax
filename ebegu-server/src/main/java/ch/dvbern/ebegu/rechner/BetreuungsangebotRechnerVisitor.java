1bec4dcf0d74b8c2b0e0eacbadb6bcd771804f29
buan <anita.brunner@dvbern.ch>
2024-02-21 08:58:10 +0100

    KIBON-3353: add MittagstischRechner and Visitor for BetreuungsangebotTypRechner

---
+++ ebegu-server/src/main/java/ch/dvbern/ebegu/rechner/BetreuungsangebotRechnerVisitor.java
@@ -0,0 +1,50 @@
+package ch.dvbern.ebegu.rechner;
+
+import ch.dvbern.ebegu.entities.Mandant;
+import ch.dvbern.ebegu.enums.BetreuungsangebotTyp;
+import ch.dvbern.ebegu.rechner.rules.RechnerRule;
+import ch.dvbern.ebegu.util.BetreuungsangebotTypVisitor;
+import com.sun.istack.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class BetreuungsangebotRechnerVisitor implements BetreuungsangebotTypVisitor<AbstractRechner> {
+
+	private final Mandant mandant;
+	private final List<RechnerRule> rechnerRulesForGemeinde;
+
+	public BetreuungsangebotRechnerVisitor(Mandant mandant, List<RechnerRule> rechnerRulesForGemeinde) {
+		this.mandant = mandant;
+		this.rechnerRulesForGemeinde = rechnerRulesForGemeinde;
+	}
+
+	public AbstractRechner getRechnerForBetreuungsTyp(@NotNull BetreuungsangebotTyp betreuungsangebotTyp) {
+		return betreuungsangebotTyp.accept(this);
+	}
+	@Override
+	public AbstractRechner visitKita() {
+		return new KitaRechnerVisitor(rechnerRulesForGemeinde).getKitaRechnerForMandant(mandant);
+	}
+
+	@Override
+	public AbstractRechner visitTagesfamilien() {
+		return new TageselternRechnerVisitor(rechnerRulesForGemeinde).getTageselternRechnerForMandant(mandant);
+	}
+
+	@Override
+	public AbstractRechner visitMittagtisch() {
+		return new MittagstischRechner();
+	}
+
+	@Override
+	public AbstractRechner visitTagesschule() {
+		return new TagesschuleRechnerVisitor(rechnerRulesForGemeinde).getTagesschuleRechnerForMandant(mandant);
+	}
+
+	@Nullable
+	@Override
+	public AbstractRechner visitFerieninsel() {
+		return null;
+	}
+}
