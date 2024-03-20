7d5589d67e6195ddb64d18e00479fdcab58d3c04
Kafader Simon <simon.kafader@dvbern.ch>
2024-03-20 14:35:17 +0100

    feat(KIBON-3421): extend auszahlungsdaten component to optionally support address
     - set default of using address to false to keep current usage same

--- ebegu-web/src/gesuch/auszahlungsdaten/auszahlungsdaten.component.ts
+++ ebegu-web/src/gesuch/auszahlungsdaten/auszahlungsdaten.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter} from '@angular/core';
+import {ListResourceRS} from '../../app/core/service/listResourceRS.rest';
+import {TSAdresse} from '../../models/TSAdresse';
 import {TSZahlungsinformationen} from '../../models/TSZahlungsinformationen';
+import {TSLand} from '../../models/types/TSLand';
+import {EbeguUtil} from '../../utils/EbeguUtil';
 import {GesuchModelManager} from '../service/gesuchModelManager';
 
 @Component({
@@ -15,15 +19,24 @@ export class AuszahlungsdatenComponent implements OnInit {
     @Input()
     public auszahlungsdatenGS: TSZahlungsinformationen;
 
+    @Input()
+    public showAddress = false;
+
     @Output()
     public readonly auszahlungsdatenChange = new EventEmitter<TSZahlungsinformationen>();
 
+    public laenderList: TSLand[];
+
     public constructor(
-        private readonly gesuchsmodelManager: GesuchModelManager
+        private readonly gesuchsmodelManager: GesuchModelManager,
+        private readonly listResourceRS: ListResourceRS,
     ) {
     }
 
     public ngOnInit(): void {
+        this.listResourceRS.getLaenderList().then((laenderList: TSLand[]) => {
+            this.laenderList = laenderList;
+        });
     }
 
     public isReadOnly(): boolean {
@@ -32,6 +45,16 @@ export class AuszahlungsdatenComponent implements OnInit {
 
     public isKorrekturModusOrFreigegeben(): boolean {
         return this.gesuchsmodelManager.isKorrekturModusJugendamt();
+    }
+
+    public abweichendeZahlungsadresseChanged(): void {
+        if (!this.auszahlungsdaten.abweichendeZahlungsadresse) {
+            this.auszahlungsdaten.zahlungsadresse = null;
+            return;
+        }
+        if (EbeguUtil.isNullOrUndefined(this.auszahlungsdaten.zahlungsadresse)) {
+            this.auszahlungsdaten.zahlungsadresse = new TSAdresse();
+        }
     }
 
 }
