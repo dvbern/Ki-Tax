99ad90e1f3ec87b3388f4e0808ceae9b73a56023
Kafader Simon <simon.kafader@dvbern.ch>
2024-03-26 08:43:08 +0100

    feat(KIBON-3375): set einstellung ABHAENGIGKEIT_ANSPRUCH_BESCHAEFTIGUNGPENSUM to ABHAENGIG in schwyz

---
+++ ebegu-dbschema/src/main/resources/db/migration/V1711437508__setSchwyzEinstellungErwerbspensumAbhaengig.sql
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2024 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+SET @mandant_schwyz_id = (SELECT id FROM mandant WHERE mandant_identifier = 'SCHWYZ');
+
+UPDATE einstellung
+SET value = 'ABHAENGING'
+WHERE gesuchsperiode_id IN (SELECT id FROM gesuchsperiode WHERE mandant_id = @mandant_schwyz_id) AND
+	  ebegu.einstellung.einstellung_key = 'ABHAENGIGKEIT_ANSPRUCH_BESCHAEFTIGUNGPENSUM';
