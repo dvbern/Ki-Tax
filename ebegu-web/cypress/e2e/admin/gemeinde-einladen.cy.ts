31de43fc687f7d16f4b449ce7a89a5222fed477e
Kafader Simon <simon.kafader@dvbern.ch>
2024-03-26 15:56:32 +0100

    test(KIBON-3290): reproduce issue with test
     - create test and pos for gemeinde einladen and ausfuellen as mandant

---
+++ ebegu-web/cypress/e2e/admin/gemeinde-einladen.cy.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2024 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {AddGemeindePO, GemeindeListPO, MainNavigationPO} from '@dv-e2e/page-objects';
+import {getUser} from '@dv-e2e/types';
+import {EditGemeindePO} from '../../page-objects/admin/edit-gemeinde.po';
+
+describe('Einladung einer Gemeinde und Ausfüllen der Stammdaten durch Mandant', () => {
+    const userMandant = getUser('[2-SB-Kanton-Bern] Benno Röthlisberger');
+    let invitedGemeinde;
+
+    before(() => {
+        cy.login(userMandant);
+        cy.visit('/#/');
+    });
+
+    it('should be possible to invite gemeinde as sb mandant', () => {
+        MainNavigationPO.getMenuButton().click();
+        MainNavigationPO.getGemeindenLink().click();
+
+        GemeindeListPO.getGemeindeHinzufuegenButton().click();
+
+        AddGemeindePO.getGemeindeSelection().find('option').eq(1).then(firstOption => {
+            invitedGemeinde = firstOption.text().trim();
+            AddGemeindePO.getGemeindeSelection().select(invitedGemeinde);
+
+            AddGemeindePO.getAdminMail().type(`admin-${invitedGemeinde}@mailbucket.dvbern.ch`);
+
+            cy.waitForRequest('POST', '**/gemeinde*', () => {
+                AddGemeindePO.getEinladungSendenButton().click();
+            });
+
+            GemeindeListPO.getSearchField().type(invitedGemeinde);
+            GemeindeListPO.getSearchItem(0).click();
+
+            EditGemeindePO.getEditButton().click();
+            EditGemeindePO.fillGemeindeStammdaten('withValid', invitedGemeinde);
+            cy.waitForRequest('PUT', '**/gemeinde/stammdaten', () => {
+                EditGemeindePO.getSaveButton().click();
+            });
+
+        });
+    })
+});
---
+++ ebegu-web/cypress/page-objects/admin/edit-gemeinde.po.ts
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2024 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// !! -- PAGE OBJECTS -- !!
+import {GemeindeFixture} from '@dv-e2e/fixtures';
+
+const getAnschrift = () => {
+	return cy.getByData('anschrift');
+};
+
+const getEmail = () => {
+	return cy.getByData('email');
+};
+
+const getStrasse = () => {
+	return cy.getByData('strasse');
+};
+
+const getHausnummer = () => {
+	return cy.getByData('hausnummer');
+};
+
+const getPLZ = () => {
+	return cy.getByData('plz');
+};
+
+const getOrt = () => {
+	return cy.getByData('ort');
+};
+
+const getTelefon = () => {
+	return cy.getByData('telefon');
+};
+
+const getStandardVerantwortliche = () => {
+	return cy.getByData('standardverantwortliche');
+};
+
+const getEditButton = () => {
+	return cy.getByData('container.edit', 'navigation-button');
+};
+
+const getSaveButton = () => {
+	return cy.getByData('container.save', 'navigation-button');
+};
+
+// !! -- PAGE ACTIONS -- !!
+
+const fillGemeindeStammdaten = (dataset: keyof typeof GemeindeFixture, gemeinde: string) => {
+  GemeindeFixture[dataset]((data) => {
+      const stammdaten = data.stammdaten;
+      getAnschrift().type(stammdaten.anschrift);
+      getEmail().type(`gemeinde-${gemeinde}@mailbucket.dvbern.ch`);
+      getStrasse().type(stammdaten.strasse);
+      getHausnummer().type(stammdaten.hausnummer);
+      getPLZ().type(stammdaten.plz);
+      getTelefon().type(stammdaten.telefon);
+      getOrt().type(gemeinde);
+      getStandardVerantwortliche().click();
+      cy.get('mat-option').first().click()
+  })
+};
+
+export const EditGemeindePO = {
+    // PAGE OBJECTS
+    getEditButton,
+    getSaveButton,
+    // PAGE ACTIONS
+    fillGemeindeStammdaten
+};
