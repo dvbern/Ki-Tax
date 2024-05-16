e3288fe1839732679a02ce2a5bf1658d7769f703
Kafader Simon <simon.kafader@dvbern.ch>
2024-05-16 17:15:41 +0200

    refactor(KIBON-3523): remove unnecessary einstellung setup for famsit schwyz
     - schwyz has no konkubinat famsit

--- ebegu-web/src/gesuch/component/familiensituation/familiensituation-schwyz/familiensituation-schwyz.component.ts
+++ ebegu-web/src/gesuch/component/familiensituation/familiensituation-schwyz/familiensituation-schwyz.component.ts
@@ -1,19 +1,13 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
-import {EinstellungRS} from '../../../../admin/service/einstellungRS.rest';
 import {ErrorService} from '../../../../app/core/errors/service/ErrorService';
-import {LogFactory} from '../../../../app/core/logging/LogFactory';
 import {AuthServiceRS} from '../../../../authentication/service/AuthServiceRS.rest';
-import {TSEinstellungKey} from '../../../../models/enums/TSEinstellungKey';
 import {TSFamilienstatus} from '../../../../models/enums/TSFamilienstatus';
-import {TSEinstellung} from '../../../../models/TSEinstellung';
 import {FamiliensituationRS} from '../../../service/familiensituationRS.service';
 import {GesuchModelManager} from '../../../service/gesuchModelManager';
 import {WizardStepManager} from '../../../service/wizardStepManager';
 import {AbstractFamiliensitutaionView} from '../AbstractFamiliensitutaionView';
 import {TSGesuchstellerKardinalitaet} from '../../../../models/enums/TSGesuchstellerKardinalitaet';
-
-const LOG = LogFactory.createLog('FamiliensituationSchwyzComponent');
 
 @Component({
     selector: 'dv-familiensituation-schwyz',
@@ -28,22 +22,10 @@ export class FamiliensituationSchwyzComponent extends AbstractFamiliensitutaionV
         protected readonly wizardStepManager: WizardStepManager,
         protected readonly familiensituationRS: FamiliensituationRS,
         protected readonly authService: AuthServiceRS,
-        private readonly einstellungRS: EinstellungRS,
         private readonly translate: TranslateService,
     ) {
         super(gesuchModelManager, errorService, wizardStepManager, familiensituationRS, authService);
         this.getFamiliensituation().familienstatus = TSFamilienstatus.SCHWYZ;
-    }
-
-    public ngOnInit(): void {
-        this.einstellungRS.getAllEinstellungenBySystemCached(
-            this.gesuchModelManager.getGesuchsperiode().id,
-        ).subscribe((response: TSEinstellung[]) => {
-            response.filter(r => r.key === TSEinstellungKey.MINIMALDAUER_KONKUBINAT)
-                .forEach(value => {
-                    this.getFamiliensituation().minDauerKonkubinat = Number(value.value);
-                });
-        }, error => LOG.error(error));
     }
 
     protected async confirm(onResult: (arg: any) => void): Promise<void> {
