14836d799a1d570e78d96f619d308f13640474e0
Björn Mosler <bjoern.mosler@dvbern.ch>
2024-05-23 15:23:58 +0200

    KIBON-3329: add GERES client and Personensuche for Schwyz.

--- ebegu-ws/src/main/java/ch/dvbern/ebegu/ws/ewk/GeresDummyClient.java
+++ ebegu-ws/src/main/java/ch/dvbern/ebegu/ws/ewk/GeresDummyClient.java
@@ -15,59 +15,67 @@
 
 package ch.dvbern.ebegu.ws.ewk;
 
-import ch.dvbern.ebegu.cdi.Dummy;
-import ch.dvbern.ebegu.cdi.Geres;
-import ch.dvbern.ebegu.dto.personensuche.EWKAdresse;
-import ch.dvbern.ebegu.dto.personensuche.EWKPerson;
-import ch.dvbern.ebegu.dto.personensuche.EWKResultat;
-import ch.dvbern.ebegu.enums.Geschlecht;
-import ch.dvbern.ebegu.errors.PersonenSucheServiceBusinessException;
-import ch.dvbern.ebegu.errors.PersonenSucheServiceException;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
-import javax.annotation.Nonnull;
-import javax.enterprise.context.Dependent;
 import java.security.SecureRandom;
 import java.time.LocalDate;
 
-/**
- * Dummy Implementation des EWK-Services
- */
-@Dummy
-@Geres
-@Dependent
-@SuppressFBWarnings
-public class GeresWebServiceDummy implements IEWKWebService {
+import javax.annotation.Nonnull;
+
+import ch.dvbern.ebegu.dto.personensuche.EWKAdresse;
+import ch.dvbern.ebegu.dto.personensuche.EWKBeziehung;
+import ch.dvbern.ebegu.dto.personensuche.EWKPerson;
+import ch.dvbern.ebegu.dto.personensuche.EWKResultat;
+import ch.dvbern.ebegu.entities.Gesuchsteller;
+import ch.dvbern.ebegu.enums.Geschlecht;
+import ch.dvbern.ebegu.errors.PersonenSucheServiceBusinessException;
+import ch.dvbern.ebegu.errors.PersonenSucheServiceException;
+
+public class GeresDummyClient implements GeresClient {
+
+	private final SecureRandom secureRandom = new SecureRandom();
 
 	@Nonnull
 	@Override
-	public EWKResultat suchePersonMitFallbackOhneVorname(@Nonnull String name, @Nonnull String vorname, @Nonnull LocalDate geburtsdatum, @Nonnull Geschlecht geschlecht, long bfsNummer) throws PersonenSucheServiceException {
+	public EWKResultat suchePersonMitFallbackOhneVorname(@Nonnull String name, @Nonnull String vorname, @Nonnull LocalDate geburtsdatum, @Nonnull Geschlecht geschlecht, Long bfsNummer) {
 		return suchePerson(name, vorname, geburtsdatum, geschlecht, bfsNummer);
 	}
 
 	@Nonnull
 	@Override
-	public EWKResultat suchePersonMitFallbackOhneVorname(@Nonnull String name, @Nonnull String vorname, @Nonnull LocalDate geburtsdatum, @Nonnull Geschlecht geschlecht) throws PersonenSucheServiceException {
+	public EWKResultat suchePersonMitFallbackOhneVorname(@Nonnull String name, @Nonnull String vorname, @Nonnull LocalDate geburtsdatum, @Nonnull Geschlecht geschlecht) {
 		return suchePerson(name, vorname, geburtsdatum, geschlecht, null);
 	}
 
 	private EWKResultat suchePerson(String name, String vorname, LocalDate geburtsdatum, Geschlecht geschlecht, Long bfsNummer) {
-		// todo homa review kibon-955 transform data from xml instead of fixed object
 		EWKResultat ewkResultat = new EWKResultat();
 		EWKPerson person = new EWKPerson();
-		person.setPersonID(String.valueOf(new SecureRandom().nextInt()));
+		person.setPersonID(String.valueOf(secureRandom.nextInt(1000)));
 		person.setNachname(name == null ? "Muster" : name + ("-Muster"));
 		person.setVorname(vorname == null ? "Max" : vorname);
-		person.setGeburtsdatum(geburtsdatum == null ? LocalDate.now() : geburtsdatum);
-		person.setGeschlecht(geschlecht == null ? geschlecht : Geschlecht.MAENNLICH);
+		person.setGeburtsdatum(geburtsdatum == null ? LocalDate.now().minusYears(secureRandom.nextInt(50)) : geburtsdatum);
+		person.setGeschlecht(geschlecht == null ? Geschlecht.MAENNLICH : geschlecht);
 		EWKAdresse adresse = new EWKAdresse();
-		adresse.setWohnungsId(Long.valueOf(2));
-		adresse.setGebaeudeId(Long.valueOf(2));
+		adresse.setWohnungsId(2L);
+		adresse.setGebaeudeId(2L);
 		adresse.setPostleitzahl("3006");
 		adresse.setStrasse("Musterstrasse");
 		adresse.setOrt("Bern");
 		adresse.setGebiet("Bern ( " + (bfsNummer != null ? bfsNummer : "") + ')');
 		person.setAdresse(adresse);
+		person.setGesuchsteller(secureRandom.nextInt(100) % 3 == 0);
+		person.setHaushalt(secureRandom.nextInt(100) % 2 == 0);
+		person.setNichtGefunden(secureRandom.nextInt(100) % 5 == 0);
+		EWKBeziehung e = new EWKBeziehung();
+		if (secureRandom.nextInt(100) % 3 == 0) {
+
+			e.setAdresse(adresse);
+		}
+		e.setNachname("Muster");
+		e.setVorname("Vater");
+		int i = secureRandom.nextInt(11) + 1;
+		e.setBeziehungstyp("EWK_BEZIEHUNG_" + i);
+		if (secureRandom.nextInt(100) % 2 == 0) {
+			person.getBeziehungen().add(e);
+		}
 		ewkResultat.getPersonen().add(person);
 		return ewkResultat;
 	}
@@ -75,8 +83,17 @@ public class GeresWebServiceDummy implements IEWKWebService {
 	@Nonnull
 	@Override
 	public EWKResultat suchePersonenInHaushalt(Long wohnungsId, Long gebaeudeId) throws PersonenSucheServiceException, PersonenSucheServiceBusinessException {
-		return suchePerson("Muster", "Max", LocalDate.now(), Geschlecht.WEIBLICH, 351L);
+		return suchePerson("Chambre", "Max", LocalDate.parse("2021-12-11"), Geschlecht.WEIBLICH, 351L);
 	}
 
+	@Nonnull
+	@Override
+	public EWKPerson suchePersonMitAhvNummer(Gesuchsteller gesuchsteller) {
+		return suchePerson(gesuchsteller.getNachname(), gesuchsteller.getVorname(), LocalDate.now(), Geschlecht.MAENNLICH, null).getPersonen().get(0);
+	}
 
+	@Override
+	public String test() throws PersonenSucheServiceException {
+		return "TestResponse by GeresDummyClient";
+	}
 }
