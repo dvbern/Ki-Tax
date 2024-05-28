1d4ea2baadc51da752a4b094070856fe0351347f
aese <severin.aeschlimann@dvbern.ch>
2024-05-28 13:06:18 +0200

    KIBON-3498: Anpassung Einkommensjahr Solothurn

--- ebegu-web/src/gesuch/component/finanzielleSituation/solothurn/AbstractFinSitsolothurnView.ts
+++ ebegu-web/src/gesuch/component/finanzielleSituation/solothurn/AbstractFinSitsolothurnView.ts
@@ -105,6 +105,10 @@ export abstract class AbstractFinSitsolothurnView extends AbstractGesuchViewX<TS
         return currentYear;
     }
 
+    public getPreviousYear(): number | string {
+        return this.getBasisjahr();
+    }
+
     public abstract isGemeinsam(): boolean;
 
     public abstract getAntragstellerNummer(): number;
