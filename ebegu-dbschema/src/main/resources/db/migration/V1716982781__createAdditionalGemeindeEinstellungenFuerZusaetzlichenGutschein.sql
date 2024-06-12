19fa91ab9516b649e891daa2dfaff7ceb5ba4aba
Kafader Simon <simon.kafader@dvbern.ch>
2024-06-12 09:30:09 +0200

    feat(KIBON-3335): remove unneeded min linear zusaetzlicher einstellung betrag einstellungen

--- ebegu-dbschema/src/main/resources/db/migration/V1716982781__createAdditionalGemeindeEinstellungenFuerZusaetzlichenGutschein.sql
+++ ebegu-dbschema/src/main/resources/db/migration/V1716982781__createAdditionalGemeindeEinstellungenFuerZusaetzlichenGutschein.sql
@@ -32,24 +32,6 @@ FROM (SELECT UNHEX(REPLACE(UUID(), '-', '')) as id,
 			 gp.mandant_id as mandant_id
 	  from gesuchsperiode as gp) as tmp;
 
--- GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_LINEAR_KITA_MIN
-INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
-						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
-SELECT *
-FROM (SELECT UNHEX(REPLACE(UUID(), '-', '')) as id,
-			 '2020-01-01 00:00:00' as timestamp_erstellt,
-			 '2020-01-01 00:00:00' as timestamp_mutiert,
-			 'flyway' as user_erstellt,
-			 'flyway' as user_mutiert,
-			 0 as version,
-			 'GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_LINEAR_KITA_MIN' as einstellung_key,
-			 '0' as value,
-			 NULL as gemeinde_id,
-			 gp.id as gesuchsperiode_id,
-			 gp.mandant_id as mandant_id
-	  from gesuchsperiode as gp) as tmp;
-
-
 -- GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_LINEAR_KITA_MAX
 INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
 						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
@@ -61,24 +43,6 @@ FROM (SELECT UNHEX(REPLACE(UUID(), '-', '')) as id,
 			 'flyway' as user_mutiert,
 			 0 as version,
 			 'GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_LINEAR_KITA_MAX' as einstellung_key,
-			 '0' as value,
-			 NULL as gemeinde_id,
-			 gp.id as gesuchsperiode_id,
-			 gp.mandant_id as mandant_id
-	  from gesuchsperiode as gp) as tmp;
-
-
--- GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_LINEAR_TFO_MIN
-INSERT INTO einstellung (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
-						 einstellung_key, value, gemeinde_id, gesuchsperiode_id, mandant_id)
-SELECT *
-FROM (SELECT UNHEX(REPLACE(UUID(), '-', '')) as id,
-			 '2020-01-01 00:00:00' as timestamp_erstellt,
-			 '2020-01-01 00:00:00' as timestamp_mutiert,
-			 'flyway' as user_erstellt,
-			 'flyway' as user_mutiert,
-			 0 as version,
-			 'GEMEINDE_ZUSAETZLICHER_GUTSCHEIN_LINEAR_TFO_MIN' as einstellung_key,
 			 '0' as value,
 			 NULL as gemeinde_id,
 			 gp.id as gesuchsperiode_id,
